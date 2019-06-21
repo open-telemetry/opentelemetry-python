e7b5279c5a827b679f3ded2e1f5d8874ad1c5c7b
Chris Kleinknecht <libc@google.com>
2019-06-21 14:32:28 -0700

    Add sphinx docs (#12)

---
+++ docs/conf.py
@@ -0,0 +1,68 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../opentelemetry-api/'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'OpenTelemetry'
+copyright = '2019, OpenTelemetry Authors'
+author = 'OpenTelemetry Authors'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    # API doc generation
+    'sphinx.ext.autodoc',
+    # Support for google-style docstrings
+    'sphinx.ext.napoleon',
+    # Infer types from hints instead of docstrings
+    'sphinx_autodoc_typehints',
+    # Add links to source from generated docs
+    'sphinx.ext.viewcode',
+    # Link to other sphinx docs
+    'sphinx.ext.intersphinx',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+    'member-order': 'bysource'
+}
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []
---
+++ docs/index.rst
@@ -0,0 +1,26 @@
+.. OpenTelemetry documentation master file, created by
+   sphinx-quickstart on Mon Jun  3 22:48:38 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+OpenTelemetry API
+=================
+
+Welcome to OpenTelemetry's API documentation!
+
+This documentation describes the ``opentelemetry-api`` package, which provides
+abstract types for OpenTelemetry implementations.
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   opentelemetry.trace
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
