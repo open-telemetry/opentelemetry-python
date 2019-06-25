6598479c4bba24efd7c13dca96a98181e19ed79c
Reiley Yang <reyang@microsoft.com>
2019-06-25 10:45:53 -0700

    find namespace packages

--- opentelemetry-api/setup.py
+++ opentelemetry-api/setup.py
@@ -44,15 +44,7 @@ setuptools.setup(
     ],
     extras_require={},
     license="Apache-2.0",
-    packages=[
-        "opentelemetry.context",
-        "opentelemetry.distributedcontext",
-        "opentelemetry.internal",
-        "opentelemetry.logs",
-        "opentelemetry.metrics",
-        "opentelemetry.resources",
-        "opentelemetry.trace",
-    ],
+    packages=setuptools.find_namespace_packages(include=["opentelemetry.*"]),
     url="https://github.com/open-telemetry/opentelemetry-python/tree/master/opentelemetry-api",
     zip_safe=False,
 )
