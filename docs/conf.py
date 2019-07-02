2fe0300ae7e5aa3d5b47dcfca95c2aff67220b5a
Chris Kleinknecht <libc@google.com>
2019-07-02 14:04:50 -0700

    Update sphinx conf for package move (#39)

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../opentelemetry-api/'))
+sys.path.insert(0, os.path.abspath('../opentelemetry-api/src/'))
 
 
 # -- Project information -----------------------------------------------------
