26289fb155c8b287e2ccc990bafa14325b73b6d3
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-07-28 21:52:36 -0700

    Adding the Resource API (#61)
    Design Decisions include:
    
    The API package should be minimal, and example implementations should
    live in SDK when possible.
    
    The precedent that exists is to name both the interface and the
    implementation in the sdk the same. This avoids the generally
    unhelpful "Default" prefix.
    
    opentelemetry-python is standardizing on Google style docstrings. As such
    resolving formatting that is inconsistent with the style guilde.

---
+++ opentelemetry-sdk/tests/resources/test_init.py
@@ -0,0 +1,33 @@
+import unittest
+from opentelemetry.sdk import resources
+
+
+class TestResources(unittest.TestCase):
+    def test_resource_merge(self):
+        left = resources.Resource({"service": "ui"})
+        right = resources.Resource({"host": "service-host"})
+        self.assertEqual(
+            left.merge(right),
+            resources.Resource({
+                "service": "ui",
+                "host": "service-host"
+            }))
+
+    def test_resource_merge_empty_string(self):
+        """Verify Resource.merge behavior with the empty string.
+
+        Labels from the source Resource take precedence, with
+        the exception of the empty string.
+
+        """
+        left = resources.Resource({"service": "ui", "host": ""})
+        right = resources.Resource({
+            "host": "service-host",
+            "service": "not-ui"
+        })
+        self.assertEqual(
+            left.merge(right),
+            resources.Resource({
+                "service": "ui",
+                "host": "service-host"
+            }))
