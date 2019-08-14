ee756c876e27dac11dac7324ba258f01b5e11e5b
Alban Crequy <alban@kinvolk.io>
2019-08-14 12:23:43 +0200

    Update opentelemetry-api/src/opentelemetry/trace/__init__.py
    Co-Authored-By: Chris Kleinknecht <libc@google.com>

--- opentelemetry-api/src/opentelemetry/trace/__init__.py
+++ opentelemetry-api/src/opentelemetry/trace/__init__.py
@@ -109,7 +109,7 @@ class Span:
                       ) -> None:
         """Sets an Attribute.
 
-        Sets a single Attribute with the key and value passed as arguments.
+        Sets a single `Attribute` with the key and value passed as arguments.
         """
 
     def add_event(self: 'Span',
