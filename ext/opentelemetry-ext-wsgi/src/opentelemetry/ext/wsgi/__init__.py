17afba58e9745f25dfa5445ee2e11816388b7221
Allan Feldman <6374032+a-feld@users.noreply.github.com>
2019-08-19 11:10:15 -0700

    Implement WSGI middleware integration. (#84)

---
+++ ext/opentelemetry-ext-wsgi/src/opentelemetry/ext/wsgi/__init__.py
@@ -0,0 +1,105 @@
+# Copyright 2019, OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+The opentelemetry-ext-wsgi package provides a WSGI middleware that can be used
+on any WSGI framework (such as Django / Flask) to track requests timing through
+OpenTelemetry.
+"""
+
+import functools
+import wsgiref.util as wsgiref_util
+
+from opentelemetry import trace
+from opentelemetry.ext.wsgi.version import __version__  # noqa
+
+
+class OpenTelemetryMiddleware:
+    """The WSGI application middleware.
+
+    This class is used to create and annotate spans for requests to a WSGI
+    application.
+
+    Args:
+        wsgi: The WSGI application callable.
+    """
+
+    def __init__(
+            self,
+            wsgi,
+            propagators=None,
+    ):
+        self.wsgi = wsgi
+
+        # TODO: implement context propagation
+        self.propagators = propagators
+
+    @staticmethod
+    def _add_request_attributes(span, environ):
+        span.set_attribute("component", "http")
+        span.set_attribute("http.method", environ["REQUEST_METHOD"])
+
+        host = environ.get("HTTP_HOST") or environ["SERVER_NAME"]
+        span.set_attribute("http.host", host)
+
+        url = (
+            environ.get("REQUEST_URI") or
+            environ.get("RAW_URI") or
+            wsgiref_util.request_uri(environ, include_query=False)
+        )
+        span.set_attribute("http.url", url)
+
+    @staticmethod
+    def _add_response_attributes(span, status):
+        status_code, status_text = status.split(" ", 1)
+        span.set_attribute("http.status_text", status_text)
+
+        try:
+            status_code = int(status_code)
+        except ValueError:
+            pass
+        else:
+            span.set_attribute("http.status_code", status_code)
+
+    @classmethod
+    def _create_start_response(cls, span, start_response):
+        @functools.wraps(start_response)
+        def _start_response(status, response_headers, *args, **kwargs):
+            cls._add_response_attributes(span, status)
+            return start_response(status, response_headers, *args, **kwargs)
+
+        return _start_response
+
+    def __call__(self, environ, start_response):
+        """The WSGI application
+
+        Args:
+            environ: A WSGI environment.
+            start_response: The WSGI start_response callable.
+        """
+
+        tracer = trace.tracer()
+        path_info = environ["PATH_INFO"] or "/"
+
+        with tracer.start_span(path_info) as span:
+            self._add_request_attributes(span, environ)
+            start_response = self._create_start_response(span, start_response)
+
+            iterable = self.wsgi(environ, start_response)
+            try:
+                for yielded in iterable:
+                    yield yielded
+            finally:
+                if hasattr(iterable, 'close'):
+                    iterable.close()
---
+++ ext/opentelemetry-ext-wsgi/tests/test_wsgi_middleware.py
@@ -0,0 +1,195 @@
+# Copyright 2019, OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import io
+import sys
+import unittest
+import unittest.mock as mock
+import wsgiref.util as wsgiref_util
+
+from opentelemetry import trace as trace_api
+from opentelemetry.ext.wsgi import OpenTelemetryMiddleware
+
+
+class Response:
+    def __init__(self):
+        self.iter = iter([b"*"])
+        self.close_calls = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.iter)
+
+    def close(self):
+        self.close_calls += 1
+
+
+def simple_wsgi(environ, start_response):
+    assert isinstance(environ, dict)
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    return [b"*"]
+
+
+def create_iter_wsgi(response):
+    def iter_wsgi(environ, start_response):
+        assert isinstance(environ, dict)
+        start_response("200 OK", [("Content-Type", "text/plain")])
+        return response
+    return iter_wsgi
+
+
+def error_wsgi(environ, start_response):
+    assert isinstance(environ, dict)
+    try:
+        raise ValueError
+    except ValueError:
+        exc_info = sys.exc_info()
+    start_response("200 OK", [("Content-Type", "text/plain")], exc_info)
+    exc_info = None
+    return [b"*"]
+
+
+class TestWsgiApplication(unittest.TestCase):
+    def setUp(self):
+        tracer = trace_api.tracer()
+        self.span_context_manager = mock.MagicMock()
+        self.span_context_manager.__enter__.return_value = \
+            mock.create_autospec(
+                trace_api.Span, spec_set=True
+            )
+        self.patcher = mock.patch.object(
+            tracer,
+            "start_span",
+            autospec=True,
+            spec_set=True,
+            return_value=self.span_context_manager,
+        )
+        self.start_span = self.patcher.start()
+
+        self.write_buffer = io.BytesIO()
+        self.write = self.write_buffer.write
+
+        self.environ = {}
+        wsgiref_util.setup_testing_defaults(self.environ)
+
+        self.status = None
+        self.response_headers = None
+        self.exc_info = None
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def start_response(self, status, response_headers, exc_info=None):
+        # The span should have started already
+        self.span_context_manager.__enter__.assert_called()
+
+        self.status = status
+        self.response_headers = response_headers
+        self.exc_info = exc_info
+        return self.write
+
+    def validate_response(self, response, error=None):
+        while True:
+            try:
+                value = next(response)
+                self.span_context_manager.__exit__.assert_not_called()
+                self.assertEqual(value, b"*")
+            except StopIteration:
+                self.span_context_manager.__exit__.assert_called()
+                break
+
+        self.assertEqual(self.status, "200 OK")
+        self.assertEqual(
+            self.response_headers,
+            [("Content-Type", "text/plain")]
+        )
+        if error:
+            self.assertIs(self.exc_info[0], error)
+            self.assertIsInstance(self.exc_info[1], error)
+            self.assertIsNotNone(self.exc_info[2])
+        else:
+            self.assertIsNone(self.exc_info)
+
+        # Verify that start_span has been called
+        self.start_span.assert_called_once_with("/")
+
+    def test_basic_wsgi_call(self):
+        app = OpenTelemetryMiddleware(simple_wsgi)
+        response = app(self.environ, self.start_response)
+        self.validate_response(response)
+
+    def test_wsgi_iterable(self):
+        original_response = Response()
+        iter_wsgi = create_iter_wsgi(original_response)
+        app = OpenTelemetryMiddleware(iter_wsgi)
+        response = app(self.environ, self.start_response)
+        # Verify that start_response has not been called yet
+        self.assertIsNone(self.status)
+        self.validate_response(response)
+
+        # Verify that close has been called exactly once
+        assert original_response.close_calls == 1
+
+    def test_wsgi_exc_info(self):
+        app = OpenTelemetryMiddleware(error_wsgi)
+        response = app(self.environ, self.start_response)
+        self.validate_response(response, error=ValueError)
+
+
+class TestWsgiAttributes(unittest.TestCase):
+    def setUp(self):
+        self.environ = {}
+        wsgiref_util.setup_testing_defaults(self.environ)
+        self.span = mock.create_autospec(trace_api.Span, spec_set=True)
+
+    def test_request_attributes(self):
+        OpenTelemetryMiddleware._add_request_attributes(  # noqa pylint: disable=protected-access
+            self.span,
+            self.environ,
+        )
+        expected = (
+            mock.call("component", "http"),
+            mock.call("http.method", "GET"),
+            mock.call("http.host", "127.0.0.1"),
+            mock.call("http.url", "http://127.0.0.1/"),
+        )
+        self.assertEqual(self.span.set_attribute.call_count, len(expected))
+        self.span.set_attribute.assert_has_calls(expected, any_order=True)
+
+    def test_response_attributes(self):
+        OpenTelemetryMiddleware._add_response_attributes(  # noqa pylint: disable=protected-access
+            self.span, "404 Not Found",
+        )
+        expected = (
+            mock.call("http.status_code", 404),
+            mock.call("http.status_text", "Not Found"),
+        )
+        self.assertEqual(self.span.set_attribute.call_count, len(expected))
+        self.span.set_attribute.assert_has_calls(expected, any_order=True)
+
+    def test_response_attributes_invalid_status_code(self):
+        OpenTelemetryMiddleware._add_response_attributes(  # noqa pylint: disable=protected-access
+            self.span, "Invalid Status Code",
+        )
+        self.assertEqual(self.span.set_attribute.call_count, 1)
+        self.span.set_attribute.assert_called_with(
+            "http.status_text",
+            "Status Code",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
