dc1fdeb89ad88c6b395ddff61b23d669eb6fd8fb
Allan Feldman <afeldman@newrelic.com>
2019-08-20 17:00:44 -0700

    Update SDK to use context.

--- opentelemetry-sdk/src/opentelemetry/sdk/distributedcontext/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/distributedcontext/__init__.py
@@ -13,56 +13,70 @@
 # limitations under the License.
 
 from contextlib import contextmanager
-import contextvars
 import typing
 
+from opentelemetry.context import Context
 from opentelemetry import distributedcontext as dctx_api
-
-_CURRENT_DISTRIBUTEDCONTEXT_CV = contextvars.ContextVar(
-    'distributed_context',
-    default=None,
-)
-
-
-class EntryMetadata(dctx_api.EntryMetadata):
-    pass
-
-
-class EntryKey(dctx_api.EntryKey):
-    pass
-
-
-class EntryValue(dctx_api.EntryValue):
-    pass
-
-
-class Entry(dctx_api.Entry):
-    pass
 
 
 class DistributedContext(dict, dctx_api.DistributedContext):
-    def get_entries(self) -> typing.Iterable[Entry]:
+    """A container for distributed context entries"""
+
+    def get_entries(self) -> typing.Iterable[dctx_api.Entry]:
+        """Returns an immutable iterator to entries."""
         return self.values()
 
-    def get_entry_value(self, key: EntryKey) -> typing.Optional[EntryValue]:
+    def get_entry_value(
+        self, key: dctx_api.EntryKey
+    ) -> typing.Optional[dctx_api.EntryValue]:
+        """Returns the entry associated with a key or None
+
+        Args:
+            key: the key with which to perform a lookup
+        """
         return self.get(key)
 
 
 class DistributedContextManager(dctx_api.DistributedContextManager):
-    def __init__(self,
-                 cv: 'contextvars.ContextVar' = _CURRENT_DISTRIBUTEDCONTEXT_CV,
-                 ) -> None:
-        self._cv = cv
+    """See `opentelemetry.distributedcontext.DistributedContextManager`
+
+    Args:
+        name: The name of the context manager
+    """
+
+    def __init__(self, name: str = "") -> None:
+        if name:
+            slot_name = "DistributedContext.{}".format(name)
+        else:
+            slot_name = "DistributedContext"
+
+        self._current_context = Context.register_slot(slot_name)
 
     def get_current_context(self) -> typing.Optional[DistributedContext]:
-        return self._cv.get(default=None)
+        """Gets the current DistributedContext.
+
+        Returns:
+            A DistributedContext instance representing the current context.
+        """
+        return self._current_context.get()
 
     @contextmanager
     def use_context(
         self, context: DistributedContext
     ) -> typing.Iterator[DistributedContext]:
-        token = self._cv.set(context)
+        """Context manager for controlling a DistributedContext lifetime.
+
+        Set the context as the active DistributedContext.
+
+        On exiting, the context manager will restore the parent
+        DistributedContext.
+
+        Args:
+            context: A DistributedContext instance to make current.
+        """
+        snapshot = self._current_context.get()
+        self._current_context.set(context)
         try:
             yield context
         finally:
-            self._cv.reset(token)
+            self._current_context.set(snapshot)
