9fc87b451f3dc9247661c3c5f2674b7797014786
Christian Neumüller <christian.neumueller@dynatrace.com>
2019-08-22 13:59:35 +0200

    requests test: Properly clean up after ourselves.

--- ext/opentelemetry-ext-http-requests/tests/test_requests_integration.py
+++ ext/opentelemetry-ext-http-requests/tests/test_requests_integration.py
@@ -22,42 +22,41 @@ class TestRequestsIntegration(unittest.TestCase):
             self.span_attrs[key] = value
 
         self.span.set_attribute = setspanattr
-        self.patcher = mock.patch.object(
+        self.start_span_patcher = mock.patch.object(
             self.tracer,
             "start_span",
             autospec=True,
             spec_set=True,
             return_value=self.span_context_manager
         )
-        self.start_span = self.patcher.start()
+        self.start_span = self.start_span_patcher.start()
 
         mocked_response = requests.models.Response()
         mocked_response.status_code = 200
         mocked_response.reason = "Roger that!"
-        send = mock.patch.object(
+        self.send_patcher = mock.patch.object(
             requests.Session, "send", autospec=True, spec_set=True,
             return_value=mocked_response)
-        self.send = send.start()
+        self.send = self.send_patcher.start()
+
+        opentelemetry.ext.http_requests.enable(self.tracer)
 
     def tearDown(self):
-        self.patcher.stop()
+        opentelemetry.ext.http_requests.disable()
+        self.send_patcher.stop()
+        self.start_span_patcher.stop()
 
     def test_basic(self):
-        try:
-
-            opentelemetry.ext.http_requests.enable(self.tracer)
-            url = "https://www.example.org/foo/bar?x=y#top"
-            _response = requests.get(url=url)
-            self.send.assert_called()
-            self.tracer.start_span.assert_called_with("/foo/bar")
-            self.span_context_manager.__enter__.assert_called()
-            self.span_context_manager.__exit__.assert_called()
-            self.assertEqual(self.span_attrs, {
-                "component": "http",
-                "http.method": "GET",
-                "http.url": url,
-                "http.status_code": 200,
-                "http.status_text": "Roger that!"
-            })
-        finally:
-            opentelemetry.ext.http_requests.disable()
+        url = "https://www.example.org/foo/bar?x=y#top"
+        _response = requests.get(url=url)
+        self.send.assert_called()
+        self.tracer.start_span.assert_called_with("/foo/bar")
+        self.span_context_manager.__enter__.assert_called()
+        self.span_context_manager.__exit__.assert_called()
+        self.assertEqual(self.span_attrs, {
+            "component": "http",
+            "http.method": "GET",
+            "http.url": url,
+            "http.status_code": 200,
+            "http.status_text": "Roger that!"
+        })
