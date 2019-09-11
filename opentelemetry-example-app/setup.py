64856c8ddaae90f9b8cd003e84a3ed505b42c3e6
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-09-10 21:50:28 -0700

    modifying version of example-app down to 0.1.dev0

--- opentelemetry-example-app/setup.py
+++ opentelemetry-example-app/setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="opentelemetry-example-app",
-    version="0.0.1alpha0",
+    version="0.1.dev0",
     author="OpenTelemetry Authors",
     author_email="cncf-opentelemetry-contributors@lists.cncf.io",
     classifiers=[
