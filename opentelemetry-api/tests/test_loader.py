21c1c4abd51652477068e3a02c60a4fe644eea7d
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-09-10 22:14:26 -0700

    Introducing an example app instrumented with opentelemetry (#129)
    Creating an example app that showcases how an application integrates with opentelemetry.

--- opentelemetry-api/tests/test_loader.py
+++ opentelemetry-api/tests/test_loader.py
@@ -12,13 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from importlib import reload
 import os
 import sys
 import unittest
+from importlib import reload
 
-from opentelemetry import loader
-from opentelemetry import trace
+from opentelemetry import loader, trace
 
 DUMMY_TRACER = None
 
--- opentelemetry-sdk/tests/trace/test_trace.py
+++ opentelemetry-sdk/tests/trace/test_trace.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest import mock
 import unittest
+from unittest import mock
 
 from opentelemetry import trace as trace_api
 from opentelemetry.sdk import trace
