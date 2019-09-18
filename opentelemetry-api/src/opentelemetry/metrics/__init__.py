2290df735e11f55bb222c6e0add00237596958da
Leighton Chen <lechen@microsoft.com>
2019-09-18 16:43:44 -0700

    Metrics API RFC 0003 cont'd (#136)
    * Create functions
    
    Comments for Meter
    
    More comments
    
    Add more comments
    
    Fix typos
    
    * fix lint
    
    * Fix lint
    
    * fix typing
    
    * Remove options, constructors, seperate labels
    
    * Consistent naming for float and int
    
    * Abstract time series
    
    * Use ABC
    
    * Fix typo
    
    * Fix docs
    
    * seperate measure classes
    
    * Add examples
    
    * fix lint
    
    * Update to RFC 0003
    
    * Add spancontext, measurebatch
    
    * Fix docs
    
    * Fix comments
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * skip examples
    
    * white space
    
    * fix spacing
    
    * fix imports
    
    * fix imports
    
    * LabelValues to str
    
    * Black formatting
    
    * fix isort
    
    * Remove aggregation
    
    * Fix names
    
    * Remove aggregation from docs
    
    * Fix lint
    
    * metric changes
    
    * Typing
    
    * Fix lint
    
    * Fix lint
    
    * Add space
    
    * Fix lint
    
    * fix comments
    
    * address comments
    
    * fix comments

--- opentelemetry-api/src/opentelemetry/metrics/__init__.py
+++ opentelemetry-api/src/opentelemetry/metrics/__init__.py
@@ -26,9 +26,9 @@ See the `metrics api`_ spec for terminology and context clarification.
 
 
 """
-
 from abc import ABC, abstractmethod
-from typing import List
+from enum import Enum
+from typing import List, Union
 
 from opentelemetry.metrics.time_series import (
     CounterTimeSeries,
@@ -37,10 +37,8 @@ from opentelemetry.metrics.time_series import (
 )
 from opentelemetry.trace import SpanContext
 
-LabelKeys = List["LabelKey"]
-LabelValues = List[str]
 
-
+# pylint: disable=unused-argument
 class Meter:
     """An interface to allow the recording of metrics.
 
@@ -49,160 +47,118 @@ class Meter:
     for the exported metric are deferred.
     """
 
-    def create_float_counter(
-        self,
-        name: str,
-        description: str,
-        unit: str,
-        label_keys: LabelKeys,
-        span_context: SpanContext = None,
-    ) -> "FloatCounter":
-        """Creates a counter type metric that contains float values.
+    # TODO: RecordBatch
 
-        Args:
-            name: The name of the counter.
-            description: Human readable description of the metric.
-            unit: Unit of the metric values.
-            label_keys: list of keys for the labels with dynamic values.
-                Order of the list is important as the same order MUST be used
-                on recording when suppling values for these labels.
-            span_context: The `SpanContext` that identifies the `Span`
-                that the metric is associated with.
 
-        Returns: A new `FloatCounter`
-        """
+class ValueType(Enum):
+    FLOAT = 0
+    INT = 1
 
-    def create_int_counter(
-        self,
-        name: str,
-        description: str,
-        unit: str,
-        label_keys: LabelKeys,
-        span_context: SpanContext = None,
-    ) -> "IntCounter":
-        """Creates a counter type metric that contains int values.
 
-        Args:
-            name: The name of the counter.
-            description: Human readable description of the metric.
-            unit: Unit of the metric values.
-            label_keys: list of keys for the labels with dynamic values.
-                Order of the list is important as the same order MUST be used
-                on recording when suppling values for these labels.
-            span_context: The `SpanContext` that identifies the `Span`
-                that the metric is associated with.
+def create_counter(
+    name: str,
+    description: str,
+    unit: str,
+    value_type: "ValueType",
+    is_bidirectional: bool = False,
+    label_keys: List[str] = None,
+    span_context: SpanContext = None,
+) -> Union["FloatCounter", "IntCounter"]:
+    """Creates a counter metric with type value_type.
 
-        Returns:
-            A new `IntCounter`
-        """
+    By default, counter values can only go up (unidirectional). The API
+    should reject negative inputs to unidirectional counter metrics.
+    Counter metrics have a bidirectional option to allow for negative
+    inputs.
 
-    def create_float_gauge(
-        self,
-        name: str,
-        description: str,
-        unit: str,
-        label_keys: LabelKeys,
-        span_context: SpanContext = None,
-    ) -> "FloatGauge":
-        """Creates a gauge type metric that contains float values.
+    Args:
+        name: The name of the counter.
+        description: Human readable description of the metric.
+        unit: Unit of the metric values.
+        value_type: The type of values being recorded by the metric.
+        is_bidirectional: Set to true to allow negative inputs.
+        label_keys: list of keys for the labels with dynamic values.
+            Order of the list is important as the same order must be used
+            on recording when suppling values for these labels.
+        span_context: The `SpanContext` that identifies the `Span`
+            that the metric is associated with.
 
-        Args:
-            name: The name of the counter.
-            description: Human readable description of the metric.
-            unit: Unit of the metric values.
-            label_keys: list of keys for the labels with dynamic values.
-                Order of the list is important as the same order MUST be used
-                on recording when suppling values for these labels.
-            span_context: The `SpanContext` that identifies the `Span`
-                that the metric is associated with.
+    Returns: A new counter metric for values of the given value_type.
+    """
 
-        Returns:
-            A new `FloatGauge`
-        """
 
-    def create_int_gauge(
-        self,
-        name: str,
-        description: str,
-        unit: str,
-        label_keys: LabelKeys,
-        span_context: SpanContext = None,
-    ) -> "IntGauge":
-        """Creates a gauge type metric that contains int values.
+def create_gauge(
+    name: str,
+    description: str,
+    unit: str,
+    value_type: "ValueType",
+    is_unidirectional: bool = False,
+    label_keys: List[str] = None,
+    span_context: SpanContext = None,
+) -> Union["FloatGauge", "IntGauge"]:
+    """Creates a gauge metric with type value_type.
 
-        Args:
-            name: The name of the counter.
-            description: Human readable description of the metric.
-            unit: Unit of the metric values.
-            label_keys: list of keys for the labels with dynamic values.
-                Order of the list is important as the same order MUST be used
-                on recording when suppling values for these labels.
-            span_context: The `SpanContext` that identifies the `Span`
-                that the metric is associated with.
+    By default, gauge values can go both up and down (bidirectional). The API
+    allows for an optional unidirectional flag, in which when set will reject
+    descending update values.
 
-        Returns:
-            A new `IntGauge`
-        """
+    Args:
+        name: The name of the gauge.
+        description: Human readable description of the metric.
+        unit: Unit of the metric values.
+        value_type: The type of values being recorded by the metric.
+        is_unidirectional: Set to true to reject negative inputs.
+        label_keys: list of keys for the labels with dynamic values.
+            Order of the list is important as the same order must be used
+            on recording when suppling values for these labels.
+        span_context: The `SpanContext` that identifies the `Span`
+            that the metric is associated with.
 
-    def create_int_measure(
-        self,
-        name: str,
-        description: str,
-        unit: str,
-        label_keys: LabelKeys,
-        span_context: SpanContext = None,
-    ) -> "IntMeasure":
-        """Creates a measure used to record raw int values.
+    Returns: A new gauge metric for values of the given value_type.
+    """
 
-        Args:
-            name: The name of the measure.
-            description: Human readable description of this measure.
-            unit: Unit of the measure values.
-            label_keys: list of keys for the labels with dynamic values.
-                Order of the list is important as the same order MUST be used
-                on recording when suppling values for these labels.
-            span_context: The `SpanContext` that identifies the `Span`
-                that the metric is associated with.
 
-        Returns:
-            A new `IntMeasure`
-        """
+def create_measure(
+    name: str,
+    description: str,
+    unit: str,
+    value_type: "ValueType",
+    is_non_negative: bool = False,
+    label_keys: List[str] = None,
+    span_context: SpanContext = None,
+) -> Union["FloatMeasure", "IntMeasure"]:
+    """Creates a measure metric with type value_type.
 
-    def create_float_measure(
-        self,
-        name: str,
-        description: str,
-        unit: str,
-        label_keys: LabelKeys,
-        span_context: SpanContext = None,
-    ) -> "FloatMeasure":
-        """Creates a Measure used to record raw float values.
+    Measure metrics represent raw statistics that are recorded. As an option,
+    measure metrics can be declared as non-negative. The API will reject
+    negative metric events for non-negative measures.
 
-        Args:
-            name: the name of the measure
-            description: Human readable description of this measure.
-            unit: Unit of the measure values.
-            label_keys: list of keys for the labels with dynamic values.
-                Order of the list is important as the same order MUST be used
-                on recording when suppling values for these labels.
-            span_context: The `SpanContext` that identifies the `Span`
-                that the metric is associated with.
+    Args:
+        name: The name of the measure.
+        description: Human readable description of the metric.
+        unit: Unit of the metric values.
+        value_type: The type of values being recorded by the metric.
+        is_non_negative: Set to true to reject negative inputs.
+        label_keys: list of keys for the labels with dynamic values.
+            Order of the list is important as the same order must be used
+            on recording when suppling values for these labels.
+        span_context: The `SpanContext` that identifies the `Span`
+            that the metric is associated with.
 
-        Returns:
-            A new `FloatMeasure`
-        """
+    Returns: A new measure metric for values of the given value_type.
+    """
 
 
 class Metric(ABC):
     """Base class for various types of metrics.
 
     Metric class that inherit from this class are specialized with the type of
-    time series that the metric holds. Metric is constructed from the meter.
+    time series that the metric holds.
     """
 
     @abstractmethod
-    def get_or_create_time_series(self, label_values: LabelValues) -> "object":
-        """Gets and returns a timeseries, a container for a cumulative value.
+    def get_or_create_time_series(self, label_values: List[str]) -> "object":
+        """Gets a timeseries, used for repeated-use of metrics instruments.
 
         If the provided label values are not already associated with this
         metric, a new timeseries is returned, otherwise it returns the existing
@@ -215,7 +171,7 @@ class Metric(ABC):
                 with the return timeseries.
         """
 
-    def remove_time_series(self, label_values: LabelValues) -> None:
+    def remove_time_series(self, label_values: List[str]) -> None:
         """Removes the timeseries from the Metric, if present.
 
         The timeseries with matching label values will be removed.
@@ -229,89 +185,54 @@ class Metric(ABC):
 
 
 class FloatCounter(Metric):
-    """A counter type metric that holds float values.
-
-    Cumulative values can go up or stay the same, but can never go down.
-    Cumulative values cannot be negative.
-    """
+    """A counter type metric that holds float values."""
 
     def get_or_create_time_series(
-        self, label_values: LabelValues
+        self, label_values: List[str]
     ) -> "CounterTimeSeries":
-        """Gets a `CounterTimeSeries` with a cumulative float value."""
+        """Gets a `CounterTimeSeries` with a float value."""
 
 
 class IntCounter(Metric):
-    """A counter type metric that holds int values.
-
-    Cumulative values can go up or stay the same, but can never go down.
-    Cumulative values cannot be negative.
-    """
+    """A counter type metric that holds int values."""
 
     def get_or_create_time_series(
-        self, label_values: LabelValues
+        self, label_values: List[str]
     ) -> "CounterTimeSeries":
-        """Gets a `CounterTimeSeries` with a cumulative int value."""
+        """Gets a `CounterTimeSeries` with an int value."""
 
 
 class FloatGauge(Metric):
-    """A gauge type metric that holds float values.
-
-    Cumulative value can go both up and down. Values can be negative.
-    """
+    """A gauge type metric that holds float values."""
 
     def get_or_create_time_series(
-        self, label_values: LabelValues
+        self, label_values: List[str]
     ) -> "GaugeTimeSeries":
-        """Gets a `GaugeTimeSeries` with a cumulative float value."""
+        """Gets a `GaugeTimeSeries` with a float value."""
 
 
 class IntGauge(Metric):
-    """A gauge type metric that holds int values.
-
-    Cumulative value can go both up and down. Values can be negative.
-    """
+    """A gauge type metric that holds int values."""
 
     def get_or_create_time_series(
-        self, label_values: LabelValues
+        self, label_values: List[str]
     ) -> "GaugeTimeSeries":
-        """Gets a `GaugeTimeSeries` with a cumulative int value."""
+        """Gets a `GaugeTimeSeries` with an int value."""
 
 
 class FloatMeasure(Metric):
-    """A measure type metric that holds float values.
-
-    Measure metrics represent raw statistics that are recorded.
-    """
+    """A measure type metric that holds float values."""
 
     def get_or_create_time_series(
-        self, label_values: LabelValues
+        self, label_values: List[str]
     ) -> "MeasureTimeSeries":
-        """Gets a `MeasureTimeSeries` with a cumulated float value."""
+        """Gets a `MeasureTimeSeries` with a float value."""
 
 
 class IntMeasure(Metric):
-    """A measure type metric that holds int values.
-
-    Measure metrics represent raw statistics that are recorded.
-    """
+    """A measure type metric that holds int values."""
 
     def get_or_create_time_series(
-        self, label_values: LabelValues
+        self, label_values: List[str]
     ) -> "MeasureTimeSeries":
-        """Gets a `MeasureTimeSeries` with a cumulated int value."""
-
-
-class LabelKey:
-    """The label keys associated with the metric.
-
-    :type key: str
-    :param key: the key for the label
-
-    :type description: str
-    :param description: description of the label
-    """
-
-    def __init__(self, key: str, description: str) -> None:
-        self.key = key
-        self.description = description
+        """Gets a `MeasureTimeSeries` with an int value."""
