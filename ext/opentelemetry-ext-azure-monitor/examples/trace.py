b657c2740c2ffc640a47c7ca274f0bed52b1a068
Reiley Yang <reyang@microsoft.com>
2019-09-24 20:12:56 -0700

    Skeleton for azure monitor exporters (#151)

---
+++ ext/opentelemetry-ext-azure-monitor/examples/trace.py
@@ -0,0 +1,13 @@
+from opentelemetry import trace
+from opentelemetry.ext.azure_monitor import AzureMonitorSpanExporter
+from opentelemetry.sdk.trace import Tracer
+from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
+
+trace.set_preferred_tracer_implementation(lambda T: Tracer())
+tracer = trace.tracer()
+tracer.add_span_processor(
+    SimpleExportSpanProcessor(AzureMonitorSpanExporter())
+)
+
+with tracer.start_span("hello") as span:
+    print("Hello, World!")
