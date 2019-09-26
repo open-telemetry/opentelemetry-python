8620712c88cc258797869188c2d35286e728d7b8
Reiley Yang <reyang@microsoft.com>
2019-09-25 21:47:12 -0700

    cosmetic update

--- ext/opentelemetry-ext-azure-monitor/src/opentelemetry/ext/azure_monitor/protocol.py
+++ ext/opentelemetry-ext-azure-monitor/src/opentelemetry/ext/azure_monitor/protocol.py
@@ -194,7 +194,5 @@ class Request(BaseObject):
         self.ver = self.ver
         self.id = self.id  # noqa pylint: disable=invalid-name
         self.duration = self.duration
-        self.responseCode = (
-            self.responseCode
-        )  # noqa pylint: disable=invalid-name
+        self.responseCode = self.responseCode  # noqa pylint: disable=invalid-name
         self.success = self.success
