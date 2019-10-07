c94a576c4bc8c1bd7b8959bf84b456ab95b8cbab
Christian Neumüller <christian.neumueller@dynatrace.com>
2019-10-07 20:07:49 +0200

    Move util.time_ns to API. (#205)

---
+++ opentelemetry-api/src/opentelemetry/util/__init__.py
@@ -0,0 +1,12 @@
+import time
+
+# Since we want API users to be able to provide timestamps,
+# this needs to be in the API.
+
+try:
+    time_ns = time.time_ns
+# Python versions < 3.7
+except AttributeError:
+
+    def time_ns() -> int:
+        return int(time.time() * 1e9)
--- opentelemetry-sdk/src/opentelemetry/sdk/util.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/util.py
@@ -14,7 +14,6 @@
 
 import datetime
 import threading
-import time
 from collections import OrderedDict, deque
 
 try:
@@ -25,14 +24,6 @@ except ImportError:
     # pylint: disable=no-name-in-module,ungrouped-imports
     from collections import MutableMapping
     from collections import Sequence
-
-try:
-    time_ns = time.time_ns
-# Python versions < 3.7
-except AttributeError:
-
-    def time_ns():
-        return int(time.time() * 1e9)
 
 
 def ns_to_iso_str(nanoseconds):
