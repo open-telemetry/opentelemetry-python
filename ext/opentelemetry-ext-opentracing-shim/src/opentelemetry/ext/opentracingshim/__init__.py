8a7c1a7cfa8c62c0e43c99cc65f295e1dc5a3ffc
Johannes Liebermann <johannes@kinvolk.io>
2019-10-10 20:45:03 +0200

    Don't override active_span on TracerWrapper
    If the user passes a custom ScopeManager, we want the TracerWrapper
    to call `active_span` on the custom ScopeManager.

--- ext/opentelemetry-ext-opentracing-shim/src/opentelemetry/ext/opentracingshim/__init__.py
+++ ext/opentelemetry-ext-opentracing-shim/src/opentelemetry/ext/opentracingshim/__init__.py
@@ -120,7 +120,10 @@ class ScopeManagerWrapper(opentracing.ScopeManager):
 
     @property
     def active(self):
-        span = self._tracer.get_current_span()
+        span = self._tracer.unwrap().get_current_span()
+        if span is None:
+            return None
+
         wrapped_span = SpanWrapper(self._tracer, span.get_context(), span)
         return ScopeWrapper(self, wrapped_span)
 
@@ -139,13 +142,6 @@ class TracerWrapper(opentracing.Tracer):
         """Returns the wrapped OpenTelemetry `Tracer` object."""
 
         return self._otel_tracer
-
-    @property
-    def active_span(self):
-        span = self._otel_tracer.get_current_span()
-        if span is None:
-            return None
-        return SpanWrapper(self, span.get_context(), span)
 
     @contextmanager
     def start_active_span(
