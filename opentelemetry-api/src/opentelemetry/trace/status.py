ce3552b9478aea68cbd2602d348b62f0c9954f93
Chris Kleinknecht <libc@google.com>
2019-10-16 14:36:03 -0700

    Fix mangled comment wrapping

--- opentelemetry-api/src/opentelemetry/trace/status.py
+++ opentelemetry-api/src/opentelemetry/trace/status.py
@@ -68,11 +68,11 @@ class StatusCanonicalCode(enum.Enum):
     """The caller does not have permission to execute the specified operation.
 
     PERMISSION_DENIED must not be used for rejections caused by exhausting some
-    resource (use RESOURCE_EXHAUSTED instead for those
-    errors).PERMISSION_DENIED must not be used if the caller can not be
-    identified (use UNAUTHENTICATED instead for those errors). This error code
-    does not imply the request is valid or the requested entity exists or
-    satisfies other pre-conditions.
+    resource (use RESOURCE_EXHAUSTED instead for those errors).
+    PERMISSION_DENIED must not be used if the caller can not be identified (use
+    UNAUTHENTICATED instead for those errors). This error code does not imply
+    the request is valid or the requested entity exists or satisfies other
+    pre-conditions.
     """
 
     RESOURCE_EXHAUSTED = 8
