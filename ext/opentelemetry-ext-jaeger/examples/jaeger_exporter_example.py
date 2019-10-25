30568defe7a8a17a52e17ac5056162e6c2699a31
Chris Kleinknecht <libc@google.com>
2019-10-25 12:55:27 -0700

    Make start_as_current_span default

--- ext/opentelemetry-ext-jaeger/examples/jaeger_exporter_example.py
+++ ext/opentelemetry-ext-jaeger/examples/jaeger_exporter_example.py
@@ -29,16 +29,16 @@ span_processor = BatchExportSpanProcessor(jaeger_exporter)
 tracer.add_span_processor(span_processor)
 
 # create some spans for testing
-with tracer.start_span("foo") as foo:
+with tracer.start_as_current_span("foo") as foo:
     time.sleep(0.1)
     foo.set_attribute("my_atribbute", True)
     foo.add_event("event in foo", {"name": "foo1"})
-    with tracer.start_span("bar") as bar:
+    with tracer.start_as_current_span("bar") as bar:
         time.sleep(0.2)
         bar.set_attribute("speed", 100.0)
         bar.add_link(foo.get_context())
 
-        with tracer.start_span("baz") as baz:
+        with tracer.start_as_current_span("baz") as baz:
             time.sleep(0.3)
             baz.set_attribute("name", "mauricio")
 
