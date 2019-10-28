922fe71873fa5fe2becb92db7a201664554ef9e6
Chris Kleinknecht <libc@google.com>
2019-10-28 16:34:08 -0700

    Testing publish action

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 # This script builds wheels for the API, SDK, and extension packages in the
 # dist/ dir, to be uploaded to PyPI.
 
-# set -ev
+set -ev
 
 # Get the latest versions of packaging tools
 python3 -m pip install --upgrade pip setuptools wheel
@@ -13,12 +13,19 @@ BASEDIR=$(dirname $(readlink -f $(dirname $0)))
 (
   cd $BASEDIR
   mkdir -p dist
+
+  # Clean old artifacts
   rm -rf dist/*
 
- for d in opentelemetry-api/ opentelemetry-sdk/ ext/*/ ; do
-   (
-     cd "$d"
-     python3 setup.py --verbose bdist_wheel --dist-dir "$BASEDIR/dist/"
-   )
- done
+  # Clean old artifacts
+  for d in opentelemetry-api/ opentelemetry-sdk/ ext/*/ ; do
+    (
+      cd "$d"
+      echo "pwd"
+      echo $(pwd)
+      python3 setup.py bdist_wheel --dist-dir "$BASEDIR/dist/"
+    )
+  done
+  echo "built packages: "
+  ls -lah "$BASEDIR/dist/"
 )
