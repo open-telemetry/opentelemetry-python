74cd9ea2676734f07c8ee01fa03d246bbc800b31
Chris Kleinknecht <libc@google.com>
2019-10-28 17:57:25 -0700

    Reblacken

--- opentelemetry-api/src/opentelemetry/trace/__init__.py
+++ opentelemetry-api/src/opentelemetry/trace/__init__.py
@@ -589,7 +589,7 @@ def tracer() -> Tracer:
 
 
 def set_preferred_tracer_implementation(
-    factory: ImplementationFactory
+    factory: ImplementationFactory,
 ) -> None:
     """Set the factory to be used to create the tracer.
 
