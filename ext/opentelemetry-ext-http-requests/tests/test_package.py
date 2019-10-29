84fced09f3c0d39f135b329b165028627ae74c3f
Chris Kleinknecht <libc@google.com>
2019-10-29 13:12:56 -0700

    Check that version numbers are normalized

---
+++ ext/opentelemetry-ext-http-requests/tests/test_package.py
@@ -0,0 +1,28 @@
+# Copyright 2019, OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import pkg_resources
+
+from opentelemetry.ext.http_requests import version
+
+
+class TestVersion(unittest.TestCase):
+    def test_version(self):
+        """Check that the version number is normalized."""
+        self.assertEqual(
+            version.__version__,
+            str(pkg_resources.parse_version(version.__version__)),
+        )
