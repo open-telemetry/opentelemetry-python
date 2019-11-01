a1762b40103f46beb3029d2be6cc9b89d0054c23
Leighton <lechen@microsoft.com>
2019-11-01 13:57:15 -0700

    fix import

--- opentelemetry-sdk/tests/metrics/export/test_export.py
+++ opentelemetry-sdk/tests/metrics/export/test_export.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import mock
 import unittest
+from unittest import mock
 
 from opentelemetry.sdk import metrics
 from opentelemetry.sdk.metrics.export import ConsoleMetricsExporter
