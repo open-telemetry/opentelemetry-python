bbb583d92d9a7eaef634cbe845276dab5211638f
Alex Boten <aboten@lightstep.com>
2019-12-10 09:49:47 -0800

    adding Baggage API
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- opentelemetry-api/src/opentelemetry/baggage/__init__.py
+++ opentelemetry-api/src/opentelemetry/baggage/__init__.py
@@ -1,2 +1,76 @@
-class Baggage:
-    """"""
+# Copyright 2019, OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from opentelemetry.context import Context
+from opentelemetry.context.propagation import (
+    HTTPExtractor,
+    HTTPInjector,
+    INVALID_HTTP_EXTRACTOR,
+    INVALID_HTTP_INJECTOR,
+)
+
+
+EMPTY_VALUE = ""
+INVALID_CONTEXT = Context()
+
+
+class BaggageManager:
+    """ TODO """
+
+    def set_value(self, ctx: Context, key: str, value: str) -> Context:
+        """
+        Sets a value on a Context
+
+        Args:
+            ctx: Context to modify
+            key: Key of the value to set
+            value: Value used to update the context
+
+        Return:
+            Context: Updated context
+        """
+        # pylint: disable=unused-argument
+        return ctx
+
+    def value(self, ctx: Context, key: str) -> str:
+        """
+        Gets a value on a Context
+
+        Args:
+            ctx: Context to query
+            key: Key of the value to get
+
+        Return:
+            str: Value of the key
+        """
+        # pylint: disable=unused-argument
+        return EMPTY_VALUE
+
+    def remove_value(self, ctx: Context, key: str) -> Context:
+        """ TODO """
+        # pylint: disable=unused-argument
+        return INVALID_CONTEXT
+
+    def clear(self, ctx: Context) -> Context:
+        """ TODO """
+        # pylint: disable=unused-argument
+        return INVALID_CONTEXT
+
+    def http_injector(self) -> HTTPInjector:
+        """ TODO """
+        return INVALID_HTTP_INJECTOR
+
+    def http_extractor(self) -> HTTPExtractor:
+        """ TODO """
+        return INVALID_HTTP_EXTRACTOR
---
+++ opentelemetry-api/src/opentelemetry/baggage/propagation/__init__.py
@@ -0,0 +1,47 @@
+# Copyright 2019, OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from opentelemetry.context import Context
+from opentelemetry.context.propagation import Carrier, Getter, Setter
+
+
+class ContextKeys:
+    """ TODO """
+
+    @classmethod
+    def span_context_key(cls):
+        """ TODO """
+        return "baggage"
+
+
+class BaggageExtractor:
+    """ TODO """
+
+    def extract(
+        self, ctx: Context, carrier: Carrier, getter: Getter
+    ) -> Context:
+        """ TODO """
+        # pylint: disable=unused-argument
+        return ctx
+
+
+class BaggageInjector:
+    """ TODO """
+
+    def inject(
+        self, ctx: Context, carrier: Carrier, setter: Setter
+    ) -> Context:
+        """ TODO """
+        # pylint: disable=unused-argument
+        return ctx
