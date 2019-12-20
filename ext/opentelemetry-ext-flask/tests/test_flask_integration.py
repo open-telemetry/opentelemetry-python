3bfa984f0e945a61f8bce06ee867b80a943d3f16
Alex Boten <aboten@lightstep.com>
2019-12-20 09:55:50 -0800

    refactoring test
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- ext/opentelemetry-ext-flask/tests/test_flask_integration.py
+++ ext/opentelemetry-ext-flask/tests/test_flask_integration.py
@@ -23,6 +23,24 @@ from opentelemetry import trace as trace_api
 from opentelemetry.ext.testutil.wsgitestutil import WsgiTestBase
 
 
+def expected_attributes(override_attributes):
+    default_attributes = {
+        "component": "http",
+        "http.method": "GET",
+        "http.server_name": "localhost",
+        "http.scheme": "http",
+        "host.port": 80,
+        "http.host": "localhost",
+        "http.target": "/",
+        "http.flavor": "1.1",
+        "http.status_text": "OK",
+        "http.status_code": 200,
+    }
+    for key, val in override_attributes.items():
+        default_attributes[key] = val
+    return default_attributes
+
+
 class TestFlaskIntegration(WsgiTestBase):
     def setUp(self):
         super().setUp()
@@ -40,19 +58,12 @@ class TestFlaskIntegration(WsgiTestBase):
         self.client = Client(self.app, BaseResponse)
 
     def test_simple(self):
-        expected_attrs = {
-            "component": "http",
-            "http.method": "GET",
-            "http.server_name": "localhost",
-            "http.scheme": "http",
-            "host.port": 80,
-            "http.host": "localhost",
-            "http.target": "/hello/123",
-            "http.flavor": "1.1",
-            "http.route": "/hello/<int:helloid>",
-            "http.status_text": "OK",
-            "http.status_code": 200,
-        }
+        expected_attrs = expected_attributes(
+            {
+                "http.target": "/hello/123",
+                "http.route": "/hello/<int:helloid>",
+            }
+        )
         resp = self.client.get("/hello/123")
         self.assertEqual(200, resp.status_code)
         self.assertEqual([b"Hello: 123"], list(resp.response))
@@ -63,18 +74,15 @@ class TestFlaskIntegration(WsgiTestBase):
         self.assertEqual(span_list[0].attributes, expected_attrs)
 
     def test_404(self):
-        expected_attrs = {
-            "component": "http",
-            "http.method": "POST",
-            "http.server_name": "localhost",
-            "http.scheme": "http",
-            "host.port": 80,
-            "http.host": "localhost",
-            "http.target": "/bye",
-            "http.flavor": "1.1",
-            "http.status_text": "NOT FOUND",
-            "http.status_code": 404,
-        }
+        expected_attrs = expected_attributes(
+            {
+                "http.method": "POST",
+                "http.target": "/bye",
+                "http.status_text": "NOT FOUND",
+                "http.status_code": 404,
+            }
+        )
+
         resp = self.client.post("/bye")
         self.assertEqual(404, resp.status_code)
         resp.close()
@@ -85,19 +93,14 @@ class TestFlaskIntegration(WsgiTestBase):
         self.assertEqual(span_list[0].attributes, expected_attrs)
 
     def test_internal_error(self):
-        expected_attrs = {
-            "component": "http",
-            "http.method": "GET",
-            "http.server_name": "localhost",
-            "http.scheme": "http",
-            "host.port": 80,
-            "http.host": "localhost",
-            "http.target": "/hello/500",
-            "http.flavor": "1.1",
-            "http.route": "/hello/<int:helloid>",
-            "http.status_text": "INTERNAL SERVER ERROR",
-            "http.status_code": 500,
-        }
+        expected_attrs = expected_attributes(
+            {
+                "http.target": "/hello/500",
+                "http.route": "/hello/<int:helloid>",
+                "http.status_text": "INTERNAL SERVER ERROR",
+                "http.status_code": 500,
+            }
+        )
         resp = self.client.get("/hello/500")
         self.assertEqual(500, resp.status_code)
         resp.close()
