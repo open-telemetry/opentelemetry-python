1a0217f8fbac5c1cc1724e8156929c886c19ab44
Alex Boten <aboten@lightstep.com>
2020-02-06 10:37:12 -0800

    filling in asyncio test
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- opentelemetry-sdk/tests/context/test_asyncio.py
+++ opentelemetry-sdk/tests/context/test_asyncio.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import asyncio
 import unittest
 
 from opentelemetry import context
@@ -22,19 +23,35 @@ from opentelemetry.sdk.trace.export.in_memory_span_exporter import (
     InMemorySpanExporter,
 )
 
+_SPAN_NAMES = [
+    "test_span1",
+    "test_span2",
+    "test_span3",
+    "test_span4",
+    "test_span5",
+]
+
+
+def stop_loop_when(loop, cond_func, timeout=5.0):
+    """
+    Registers a periodic callback that stops the loop when cond_func() == True.
+    Compatible with both Tornado and asyncio.
+    """
+    if cond_func() or timeout <= 0.0:
+        loop.stop()
+        return
+
+    timeout -= 0.1
+    loop.call_later(0.1, stop_loop_when, loop, cond_func, timeout)
+
 
 class TestAsyncio(unittest.TestCase):
-    span_names = [
-        "test_span1",
-        "test_span2",
-        "test_span3",
-        "test_span4",
-        "test_span5",
-    ]
-
-    def do_work(self, name="default"):
+    async def task(self, name):
         with self.tracer.start_as_current_span(name):
             context.set_value("say-something", "bar")
+
+    def submit_another_task(self, name):
+        self.loop.create_task(self.task(name))
 
     def setUp(self):
         self.previous_context = context.get_current()
@@ -44,8 +61,41 @@ class TestAsyncio(unittest.TestCase):
         self.memory_exporter = InMemorySpanExporter()
         span_processor = export.SimpleExportSpanProcessor(self.memory_exporter)
         self.tracer_source.add_span_processor(span_processor)
+        self.loop = asyncio.get_event_loop()
 
     def tearDown(self):
         context.set_current(self.previous_context)
 
-    # FIXME: add an actual test
+    def test_with_asyncio(self):
+        with self.tracer.start_as_current_span("asyncio_test"):
+            for name in _SPAN_NAMES:
+                self.submit_another_task(name)
+
+        stop_loop_when(
+            self.loop,
+            lambda: len(self.memory_exporter.get_finished_spans()) >= 6,
+            timeout=5.0,
+        )
+        self.loop.run_forever()
+        span_list = self.memory_exporter.get_finished_spans()
+        span_names_list = [span.name for span in span_list]
+        expected = [
+            "test_span1",
+            "test_span2",
+            "test_span3",
+            "test_span4",
+            "test_span5",
+            "asyncio_test",
+        ]
+        self.assertCountEqual(span_names_list, expected)
+        span_names_list.sort()
+        expected.sort()
+        self.assertListEqual(span_names_list, expected)
+        expected_parent = next(
+            span for span in span_list if span.name == "asyncio_test"
+        )
+        # FIXME
+        for span in span_list:
+            if span is expected_parent:
+                continue
+            self.assertEqual(span.parent, expected_parent)
