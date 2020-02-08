782e9b1e91bee1d8454ef777a4a4093ddfb5e731
Emil Madsen <sovende@gmail.com>
2020-02-08 16:10:52 +0100

    Fix ext/testutil

---
+++ tests/util/src/opentelemetry/test/asgitestutil.py
@@ -0,0 +1,58 @@
+import asyncio
+from asgiref.testing import ApplicationCommunicator
+from opentelemetry.ext.testutil.spantestutil import SpanTestBase
+
+
+def setup_testing_defaults(scope):
+    scope.update({
+        'client': ('127.0.0.1', 32767),
+        'headers': [],
+        'http_version': '1.0',
+        'method': 'GET',
+        'path': '/',
+        'query_string': b'',
+        'scheme': 'http',
+        'server': ('127.0.0.1', 80),
+        'type': 'http'
+    })
+
+
+class AsgiTestBase(SpanTestBase):
+    def setUp(self):
+        super().setUp()
+
+        self.scope = {}
+        setup_testing_defaults(self.scope)
+        self.communicator = None
+
+    def tearDown(self):
+        if self.communicator:
+            asyncio.get_event_loop().run_until_complete(
+                self.communicator.wait()
+            )
+
+    def seed_app(self, app):
+        self.communicator = ApplicationCommunicator(app, self.scope)
+
+    def send_input(self, payload):
+        asyncio.get_event_loop().run_until_complete(
+            self.communicator.send_input(payload)
+        )
+        
+    def send_default_request(self):
+        self.send_input({'type': 'http.request', 'body': b''})
+
+    def get_output(self):
+        output = asyncio.get_event_loop().run_until_complete(
+            self.communicator.receive_output(0)
+        )
+        return output
+
+    def get_all_output(self):
+        outputs = []
+        while True:
+            try:
+                outputs.append(self.get_output())
+            except asyncio.TimeoutError as e:
+                break
+        return outputs
--- tests/util/src/opentelemetry/test/spantestutil.py
+++ tests/util/src/opentelemetry/test/spantestutil.py
@@ -1,9 +1,8 @@
-import io
-import wsgiref.util as wsgiref_util
-from opentelemetry.ext.testutil.spantestutil import SpanTestBase
+import unittest
+from importlib import reload
 
 from opentelemetry import trace as trace_api
-from opentelemetry.sdk.trace import TracerProvider, export
+from opentelemetry.sdk.trace import TracerSource, export
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import (
     InMemorySpanExporter,
 )
@@ -11,16 +10,22 @@ from opentelemetry.sdk.trace.export.in_memory_span_exporter import (
 _MEMORY_EXPORTER = None
 
 
-class WsgiTestBase(unittest.TestCase):
+class SpanTestBase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         global _MEMORY_EXPORTER  # pylint:disable=global-statement
-        trace_api.set_tracer_provider(TracerProvider())
-        tracer_provider = trace_api.get_tracer_provider()
+        trace_api.set_preferred_tracer_source_implementation(
+            lambda T: TracerSource()
+        )
+        tracer_source = trace_api.tracer_source()
         _MEMORY_EXPORTER = InMemorySpanExporter()
         span_processor = export.SimpleExportSpanProcessor(_MEMORY_EXPORTER)
-        tracer_provider.add_span_processor(span_processor)
+        tracer_source.add_span_processor(span_processor)
 
     @classmethod
     def tearDownClass(cls):
         reload(trace_api)
+
+    def setUp(self):
+        self.memory_exporter = _MEMORY_EXPORTER
+        self.memory_exporter.clear()
