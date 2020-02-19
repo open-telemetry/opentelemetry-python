674658af408caf21e550abdd35d0d6db5d681924
Alex Boten <aboten@lightstep.com>
2020-02-19 13:48:39 -0800

    fix lint, catch AttributeError as well
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- opentelemetry-api/src/opentelemetry/context/contextvars_context.py
+++ opentelemetry-api/src/opentelemetry/context/contextvars_context.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from contextvars import ContextVar, Token
+from contextvars import ContextVar
 from sys import version_info
 
 from opentelemetry.context.context import Context, RuntimeContext
