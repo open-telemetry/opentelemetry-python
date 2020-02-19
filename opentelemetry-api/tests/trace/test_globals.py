252892fa347694fda83735420b521706b844e1fa
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2020-02-19 09:35:56 -0800

    switching assertEquals to assertIs

--- opentelemetry-api/tests/trace/test_globals.py
+++ opentelemetry-api/tests/trace/test_globals.py
@@ -38,4 +38,4 @@ class TestGlobals(unittest.TestCase):
         """trace.get_tracer should proxy to the global tracer source."""
         from_global_api = trace.get_tracer("foo")
         from_tracer_api = trace.tracer_source().get_tracer("foo")
-        self.assertEqual(from_global_api, from_tracer_api)
+        self.assertIs(from_global_api, from_tracer_api)
