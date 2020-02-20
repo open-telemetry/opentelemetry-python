b4e77482321278fb870e9a56c59ec8ec1b1f701c
Chris Kleinknecht <libc@google.com>
2020-02-20 10:38:29 -0800

    Build wheels from source dists

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,25 +6,32 @@
 set -ev
 
 # Get the latest versions of packaging tools
-python3 -m pip install --user --upgrade pip setuptools wheel
+python3 -m pip install --upgrade pip setuptools wheel
 
 BASEDIR=$(dirname $(readlink -f $(dirname $0)))
+DISTDIR=dist
 
 (
   cd $BASEDIR
-  mkdir -p dist
-  rm -rf dist/*
+  mkdir -p $DISTDIR
+  rm -rf $DISTDIR/*
 
  for d in opentelemetry-api/ opentelemetry-sdk/ ext/*/ ; do
    (
      echo "building $d"
      cd "$d"
-     # some ext directories (such as docker tests)
-     # are not intended to be packaged. Verify the
-     # intent by looking for a setup.py
+     # Some ext directories (such as docker tests) are not intended to be
+     # packaged. Verify the intent by looking for a setup.py.
      if [ -f setup.py ]; then
-      python3 setup.py --verbose bdist_wheel --dist-dir "$BASEDIR/dist/" sdist --dist-dir "$BASEDIR/dist/"
+      python3 setup.py sdist --dist-dir "$BASEDIR/dist/" clean --all
      fi
    )
  done
+ # Build a wheel for each source distribution
+ (
+   cd $DISTDIR
+   for x in *.tar.gz ; do
+     pip wheel --no-deps $x
+   done
+ )
 )
