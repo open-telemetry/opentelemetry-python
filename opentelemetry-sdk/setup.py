01ad01143717d1bcd1c131b07f78d3a242d832ef
Chris Kleinknecht <libc@google.com>
2020-03-02 14:05:58 -0800

    Bump versions to 0.4a1

--- opentelemetry-sdk/setup.py
+++ opentelemetry-sdk/setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
     include_package_data=True,
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
-    install_requires=["opentelemetry-api==0.4a0"],
+    install_requires=["opentelemetry-api==0.4a1"],
     extras_require={},
     license="Apache-2.0",
     package_dir={"": "src"},
