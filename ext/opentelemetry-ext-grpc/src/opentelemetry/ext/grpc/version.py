38bb4ef6b854495dd9d44d87dec45820a0e7d73b
Chris Kleinknecht <libc@google.com>
2020-03-10 15:13:09 -0700

    Move examples, add boilerplate

--- ext/opentelemetry-ext-grpc/src/opentelemetry/ext/grpc/version.py
+++ ext/opentelemetry-ext-grpc/src/opentelemetry/ext/grpc/version.py
@@ -12,16 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
-import setuptools
-
-BASE_DIR = os.path.dirname(__file__)
-VERSION_FILENAME = os.path.join(
-    BASE_DIR, "src", "opentelemetry", "ext", "grpc", "version.py"
-)
-PACKAGE_INFO = {}
-with open(VERSION_FILENAME) as f:
-    exec(f.read(), PACKAGE_INFO)
-
-setuptools.setup(version=PACKAGE_INFO["__version__"])
+__version__ = "0.5.dev0"
