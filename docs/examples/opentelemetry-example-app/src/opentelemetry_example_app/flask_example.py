2a1353f72b10d5a6c582eb2b18c72e9982218fef
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2020-03-16 14:19:47 -0700

    Bugfix: Fixing example app test (#495)

--- docs/examples/opentelemetry-example-app/src/opentelemetry_example_app/flask_example.py
+++ docs/examples/opentelemetry-example-app/src/opentelemetry_example_app/flask_example.py
@@ -23,8 +23,10 @@ import opentelemetry.ext.http_requests
 from opentelemetry import trace
 from opentelemetry.ext.flask import instrument_app
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter
-from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleExportSpanProcessor,
+)
 
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(
@@ -42,6 +44,3 @@ def hello():
     with tracer.start_as_current_span("example-request"):
         requests.get("http://www.example.com")
     return "hello"
-
-
-app.run(debug=True)
