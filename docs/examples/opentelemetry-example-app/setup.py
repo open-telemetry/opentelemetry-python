2d0dcfd0c8d4bc124935da6e980607d3be9ccd2b
Chris Kleinknecht <libc@google.com>
2020-03-16 15:53:33 -0700

    s/0.5b0/0.6.dev0/

--- docs/examples/opentelemetry-example-app/setup.py
+++ docs/examples/opentelemetry-example-app/setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="opentelemetry-example-app",
-    version="0.5b0",
+    version="0.6.dev0",
     author="OpenTelemetry Authors",
     author_email="cncf-opentelemetry-contributors@lists.cncf.io",
     classifiers=[
--- ext/opentelemetry-ext-testutil/src/opentelemetry/ext/testutil/version.py
+++ ext/opentelemetry-ext-testutil/src/opentelemetry/ext/testutil/version.py
@@ -1,1 +1,1 @@
-__version__ = "0.5b0"
+__version__ = "0.6.dev0"
