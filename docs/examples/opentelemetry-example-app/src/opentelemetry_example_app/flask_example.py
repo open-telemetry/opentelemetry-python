5b48b83ba1cfc4b0615599293c0719a08d26d36c
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2020-03-16 13:31:20 -0700

    fixing lint

--- docs/examples/opentelemetry-example-app/src/opentelemetry_example_app/flask_example.py
+++ docs/examples/opentelemetry-example-app/src/opentelemetry_example_app/flask_example.py
@@ -23,8 +23,10 @@ import opentelemetry.ext.http_requests
 from opentelemetry import trace
 from opentelemetry.ext.flask import instrument_app
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter
-from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleExportSpanProcessor,
+)
 
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(
