90448d3a581d75c607fedce51b8166732d3e9bb7
Mauricio Vásquez <mauricio@kinvolk.io>
2020-03-18 15:15:15 -0500

    read missing check on status code

--- ext/opentelemetry-ext-jaeger/src/opentelemetry/ext/jaeger/__init__.py
+++ ext/opentelemetry-ext-jaeger/src/opentelemetry/ext/jaeger/__init__.py
@@ -362,3 +362,11 @@ class Collector:
     def submit(self, batch):
         batch.write(self.protocol)
         self.http_transport.flush()
+        code = self.http_transport.code
+        msg = self.http_transport.message
+        if code >= 300 or code < 200:
+            logger.error(
+                "Traces cannot be uploaded; HTTP status code: %s, message: %s",
+                code,
+                msg,
+            )
