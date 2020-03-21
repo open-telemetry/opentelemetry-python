3eb4232d17bc2e3eb3cbdd94967bfd0ae5548fa8
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2020-03-21 12:02:50 +0100

    Add script to generate AUTHORS
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

---
+++ scripts/generate-authors.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env fish
+set -e
+
+{
+	cat <<- 'EOH'
+		# This file lists all individuals having contributed content to the repository.
+		# For how it is generated, see `scripts/generate-authors.sh`.
+	EOH
+	echo
+
+    git log --format='%aN <%aE>' | awk '
+    {
+        pos = index($0, "<");
+        name = substr($0, 0, pos - 2);
+        email = substr($0, pos + 1, length($0) - pos - 1);
+        names[name]++;
+        emails[email]++;
+        if (names[name] == 1 && emails[email] == 1) {
+            print $0;
+        }
+    }
+    ' | env LC_ALL=C.UTF-8 sort -uf
+} > AUTHORS
