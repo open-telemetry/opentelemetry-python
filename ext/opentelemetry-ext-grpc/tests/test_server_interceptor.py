38456040dd8e79a86e7c7d635d3bca412acbb4fd
Chris Kleinknecht <libc@google.com>
2020-03-24 17:10:38 -0700

    Update server tests

--- ext/opentelemetry-ext-grpc/tests/test_server_interceptor.py
+++ ext/opentelemetry-ext-grpc/tests/test_server_interceptor.py
@@ -17,11 +17,14 @@ import unittest
 from concurrent import futures
 from contextlib import contextmanager
 from unittest import mock
+from unittest.mock import patch
 
 import grpc
 
-from opentelemetry import trace
+from opentelemetry import context, trace
+from opentelemetry.context.threadlocal_context import ThreadLocalRuntimeContext
 from opentelemetry.ext.grpc import server_interceptor
+from opentelemetry.ext.grpc.grpcext import intercept_server
 from opentelemetry.sdk import trace as trace_sdk
 
 
@@ -37,6 +40,22 @@ class UnaryUnaryMethodHandler(grpc.RpcMethodHandler):
         self.stream_stream = None
 
 
+# lifted from
+# https://github.com/grpc/grpc/blob/694f491e06b3503952ca5053c98493cf48af43c1/src/python/grpcio_tests/tests/unit/_interceptor_test.py
+
+
+_SERIALIZE_REQUEST = lambda bytestring: bytestring * 2
+_DESERIALIZE_REQUEST = lambda bytestring: bytestring[len(bytestring) // 2 :]
+_SERIALIZE_RESPONSE = lambda bytestring: bytestring * 3
+_DESERIALIZE_RESPONSE = lambda bytestring: bytestring[: len(bytestring) // 3]
+
+
+_UNARY_UNARY = "/test/UnaryUnary"
+_UNARY_STREAM = "/test/UnaryStream"
+_STREAM_UNARY = "/test/StreamUnary"
+_STREAM_STREAM = "/test/StreamStream"
+
+
 class UnaryUnaryRpcHandler(grpc.GenericRpcHandler):
     def __init__(self, handler):
         self._unary_unary_handler = handler
@@ -45,7 +64,144 @@ class UnaryUnaryRpcHandler(grpc.GenericRpcHandler):
         return UnaryUnaryMethodHandler(self._unary_unary_handler)
 
 
+class _Handler(object):
+    def __init__(self, control):
+        self._control = control
+
+    def handle_unary_unary(self, request, servicer_context):
+        self._control.control()
+        if servicer_context is not None:
+            servicer_context.set_trailing_metadata((("testkey", "testvalue"),))
+        if request == _EXCEPTION_REQUEST:
+            raise _ApplicationErrorStandin()
+        return request
+
+    def handle_unary_stream(self, request, servicer_context):
+        if request == _EXCEPTION_REQUEST:
+            raise _ApplicationErrorStandin()
+        for _ in range(test_constants.STREAM_LENGTH):
+            self._control.control()
+            yield request
+        self._control.control()
+        if servicer_context is not None:
+            servicer_context.set_trailing_metadata((("testkey", "testvalue"),))
+
+    def handle_stream_unary(self, request_iterator, servicer_context):
+        if servicer_context is not None:
+            servicer_context.invocation_metadata()
+        self._control.control()
+        response_elements = []
+        for request in request_iterator:
+            self._control.control()
+            response_elements.append(request)
+        self._control.control()
+        if servicer_context is not None:
+            servicer_context.set_trailing_metadata((("testkey", "testvalue"),))
+        if _EXCEPTION_REQUEST in response_elements:
+            raise _ApplicationErrorStandin()
+        return b"".join(response_elements)
+
+    def handle_stream_stream(self, request_iterator, servicer_context):
+        self._control.control()
+        if servicer_context is not None:
+            servicer_context.set_trailing_metadata((("testkey", "testvalue"),))
+        for request in request_iterator:
+            if request == _EXCEPTION_REQUEST:
+                raise _ApplicationErrorStandin()
+            self._control.control()
+            yield request
+        self._control.control()
+
+
+class _MethodHandler(grpc.RpcMethodHandler):
+    def __init__(
+        self,
+        request_streaming,
+        response_streaming,
+        request_deserializer,
+        response_serializer,
+        unary_unary,
+        unary_stream,
+        stream_unary,
+        stream_stream,
+    ):
+        self.request_streaming = request_streaming
+        self.response_streaming = response_streaming
+        self.request_deserializer = request_deserializer
+        self.response_serializer = response_serializer
+        self.unary_unary = unary_unary
+        self.unary_stream = unary_stream
+        self.stream_unary = stream_unary
+        self.stream_stream = stream_stream
+
+
+class _GenericHandler(grpc.GenericRpcHandler):
+    def __init__(self, handler):
+        self._handler = handler
+
+    def service(self, handler_call_details):
+        if handler_call_details.method == _UNARY_UNARY:
+            return _MethodHandler(
+                False,
+                False,
+                None,
+                None,
+                self._handler.handle_unary_unary,
+                None,
+                None,
+                None,
+            )
+        elif handler_call_details.method == _UNARY_STREAM:
+            return _MethodHandler(
+                False,
+                True,
+                _DESERIALIZE_REQUEST,
+                _SERIALIZE_RESPONSE,
+                None,
+                self._handler.handle_unary_stream,
+                None,
+                None,
+            )
+        elif handler_call_details.method == _STREAM_UNARY:
+            return _MethodHandler(
+                True,
+                False,
+                _DESERIALIZE_REQUEST,
+                _SERIALIZE_RESPONSE,
+                None,
+                None,
+                self._handler.handle_stream_unary,
+                None,
+            )
+        elif handler_call_details.method == _STREAM_STREAM:
+            return _MethodHandler(
+                True,
+                True,
+                None,
+                None,
+                None,
+                None,
+                None,
+                self._handler.handle_stream_stream,
+            )
+        else:
+            return None
+
+
 class TestOpenTelemetryServerInterceptor(unittest.TestCase):
+
+    # FIXME: test_concurrent_server_spans fails with contextvars context.
+    def setUp(self) -> None:
+        super(TestOpenTelemetryServerInterceptor, self).setUp()
+        self.mock_runtime = patch.object(
+            context, "_RUNTIME_CONTEXT", ThreadLocalRuntimeContext()
+        )
+        self.mock_runtime.start()
+
+    def tearDown(self) -> None:
+        super(TestOpenTelemetryServerInterceptor, self).tearDown()
+        self.mock_runtime.stop()
+
     def test_create_span(self):
         """Check that the interceptor wraps calls with spans server-side."""
 
@@ -56,20 +212,20 @@ class TestOpenTelemetryServerInterceptor(unittest.TestCase):
         # Intercept gRPC calls...
         tracer = mock.Mock(spec=trace.Tracer)
         tracer.start_as_current_span.side_effect = mock_start_as_current_span
-        interceptor = server_interceptor.OpenTelemetryServerInterceptor(
-            tracer=tracer
-        )
+        interceptor = server_interceptor(tracer)
 
         # No-op RPC handler
         def handler(request, context):
             return b""
 
         server = grpc.server(
-            futures.ThreadPoolExecutor(max_workers=1),
-            handlers=(UnaryUnaryRpcHandler(handler),),
-            interceptors=(interceptor,),
-            options=(("grpc.so_reuseport", 0),),
+            futures.ThreadPoolExecutor(), options=(("grpc.so_reuseport", 0),)
         )
+        # FIXME: grpcext interceptor doesn't apply to handlers passed to server
+        # init, should use intercept_service API instead.
+        server = intercept_server(server, interceptor)
+        server.add_generic_rpc_handlers((UnaryUnaryRpcHandler(handler),))
+
         port = server.add_insecure_port("[::]:0")
         channel = grpc.insecure_channel("localhost:{:d}".format(port))
 
@@ -80,7 +236,7 @@ class TestOpenTelemetryServerInterceptor(unittest.TestCase):
             server.stop(None)
 
         tracer.start_as_current_span.assert_called_once_with(
-            "", kind=trace.SpanKind.SERVER
+            name="", kind=trace.SpanKind.SERVER
         )
 
     def test_span_lifetime(self):
@@ -88,9 +244,7 @@ class TestOpenTelemetryServerInterceptor(unittest.TestCase):
 
         tracer_provider = trace_sdk.TracerProvider()
         tracer = tracer_provider.get_tracer(__name__)
-        interceptor = server_interceptor.OpenTelemetryServerInterceptor(
-            tracer=tracer
-        )
+        interceptor = server_interceptor(tracer)
 
         # To capture the current span at the time the handler is called
         active_span_in_handler = None
@@ -101,11 +255,11 @@ class TestOpenTelemetryServerInterceptor(unittest.TestCase):
             return b""
 
         server = grpc.server(
-            futures.ThreadPoolExecutor(max_workers=1),
-            handlers=(UnaryUnaryRpcHandler(handler),),
-            interceptors=(interceptor,),
-            options=(("grpc.so_reuseport", 0),),
+            futures.ThreadPoolExecutor(), options=(("grpc.so_reuseport", 0),)
         )
+        server = intercept_server(server, interceptor)
+        server.add_generic_rpc_handlers((UnaryUnaryRpcHandler(handler),))
+
         port = server.add_insecure_port("[::]:0")
         channel = grpc.insecure_channel("localhost:{:d}".format(port))
 
@@ -122,14 +276,59 @@ class TestOpenTelemetryServerInterceptor(unittest.TestCase):
         self.assertIsInstance(active_span_in_handler, trace_sdk.Span)
         self.assertIsNone(active_span_in_handler.parent)
 
-    def test_concurrent_server_spans(self):
-        """Check that each call gets its own span."""
+    def test_sequential_server_spans(self):
+        """Check that sequential RPCs get separate server spans."""
 
         tracer_provider = trace_sdk.TracerProvider()
         tracer = tracer_provider.get_tracer(__name__)
-        interceptor = server_interceptor.OpenTelemetryServerInterceptor(
-            tracer=tracer
+
+        interceptor = server_interceptor(tracer)
+
+        # Capture the currently active span in each thread
+        active_spans_in_handler = []
+
+        def handler(request, context):
+            active_spans_in_handler.append(tracer.get_current_span())
+            return b""
+
+        server = grpc.server(
+            futures.ThreadPoolExecutor(), options=(("grpc.so_reuseport", 0),)
         )
+        server = intercept_server(server, interceptor)
+        server.add_generic_rpc_handlers((UnaryUnaryRpcHandler(handler),))
+
+        port = server.add_insecure_port("[::]:0")
+        channel = grpc.insecure_channel("localhost:{:d}".format(port))
+
+        try:
+            server.start()
+            channel.unary_unary("")(b"")
+            channel.unary_unary("")(b"")
+        finally:
+            server.stop(None)
+
+        self.assertEqual(len(active_spans_in_handler), 2)
+        span1, span2 = active_spans_in_handler
+        # Spans should belong to separate traces, and each should be a root
+        # span
+        self.assertNotEqual(span1.context.span_id, span2.context.span_id)
+        self.assertNotEqual(span1.context.trace_id, span2.context.trace_id)
+        self.assertIsNone(span1.parent)
+        self.assertIsNone(span1.parent)
+
+    def test_concurrent_server_spans(self):
+        """Check that concurrent RPC calls don't interfere with each other.
+
+        This is the same check as test_sequential_server_spans except that the
+        RPCs are concurrent. Two handlers are invoked at the same time on two
+        separate threads. Each one should see a different active span and
+        context.
+        """
+
+        tracer_provider = trace_sdk.TracerProvider()
+        tracer = tracer_provider.get_tracer(__name__)
+
+        interceptor = server_interceptor(tracer)
 
         # Capture the currently active span in each thread
         active_spans_in_handler = []
@@ -142,10 +341,11 @@ class TestOpenTelemetryServerInterceptor(unittest.TestCase):
 
         server = grpc.server(
             futures.ThreadPoolExecutor(max_workers=2),
-            handlers=(UnaryUnaryRpcHandler(handler),),
-            interceptors=(interceptor,),
             options=(("grpc.so_reuseport", 0),),
         )
+        server = intercept_server(server, interceptor)
+        server.add_generic_rpc_handlers((UnaryUnaryRpcHandler(handler),))
+
         port = server.add_insecure_port("[::]:0")
         channel = grpc.insecure_channel("localhost:{:d}".format(port))
 
