e834de25b19052b5c27ce57cbeb2d59690f67ba3
Chris Kleinknecht <libc@google.com>
2020-03-30 18:04:24 -0700

    gRPC integration (#476)
    This is a port of grpcio-opentracing, and borrows from opencensus-ext-grpc. It
    adds client and server interceptors that wrap each request in a span and use
    the new context API to inject/extract the traceparent header to/from gRPC
    metadata.
    
    Co-authored-by: alrex <alrex.boten@gmail.com>
    Co-authored-by: Mauricio Vásquez <mauricio@kinvolk.io>
    Co-authored-by: Mario Jonke <mario.jonke@dynatrace.com>
    Co-authored-by: Daniel González <danielgonzalezlopes@gmail.com>
    Co-authored-by: Alex Boten <aboten@lightstep.com>
    Co-authored-by: Yusuke Tsutsumi <yusuke@tsutsumi.io>

---
+++ docs/examples/opentelemetry-example-app/src/opentelemetry_example_app/grpc/hello_world_server.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=import-error
+
+"""The Python implementation of the GRPC helloworld.Greeter server.
+
+Note that you need ``opentelemetry-ext-grpc`` and ``protobuf`` to be installed
+to run these examples. To run this script in the context of the example app,
+install ``opentelemetry-example-app``::
+
+    pip install -e ext/opentelemetry-ext-grpc/
+    pip install -e docs/examples/opentelemetry-example-app
+
+Then run the server in one shell::
+
+    python -m opentelemetry_example_app.grpc.hello_world_client
+
+and the client in another::
+
+    python -m opentelemetry_example_app.grpc.hello_world_server
+
+See also:
+https://github.com/grpc/grpc/blob/master/examples/python/helloworld/greeter_server.py
+"""
+
+import logging
+from concurrent import futures
+
+import grpc
+
+from opentelemetry import trace
+from opentelemetry.ext.grpc import server_interceptor
+from opentelemetry.ext.grpc.grpcext import intercept_server
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleExportSpanProcessor,
+)
+
+try:
+    # Relative imports should work in the context of the package, e.g.:
+    # `python -m opentelemetry_example_app.grpc.hello_world_server`.
+    from .gen import helloworld_pb2, helloworld_pb2_grpc
+except ImportError:
+    # This will fail when running the file as a script, e.g.:
+    # `./hello_world_server.py`
+    # fall back to importing from the same directory in this case.
+    from gen import helloworld_pb2, helloworld_pb2_grpc
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    SimpleExportSpanProcessor(ConsoleSpanExporter())
+)
+tracer = trace.get_tracer(__name__)
+
+
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        return helloworld_pb2.HelloReply(message="Hello, %s!" % request.name)
+
+
+def serve():
+
+    server = grpc.server(futures.ThreadPoolExecutor())
+    server = intercept_server(server, server_interceptor(tracer))
+
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
