494d08805c2ce34a879e331770c86d5e421c65ac
Mauricio Vásquez <mauricio@kinvolk.io>
2020-04-16 13:42:09 -0500

    ext/test-docker: Use TestBase

--- ext/opentelemetry-ext-docker-tests/tests/pymongo/test_pymongo_functional.py
+++ ext/opentelemetry-ext-docker-tests/tests/pymongo/test_pymongo_functional.py
@@ -13,18 +13,12 @@
 # limitations under the License.
 
 import os
-import typing
-import unittest
 
 from pymongo import MongoClient
 
 from opentelemetry import trace as trace_api
 from opentelemetry.ext.pymongo import trace_integration
-from opentelemetry.sdk.trace import Span, Tracer, TracerProvider
-from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
-from opentelemetry.sdk.trace.export.in_memory_span_exporter import (
-    InMemorySpanExporter,
-)
+from opentelemetry.test.test_base import TestBase
 
 MONGODB_HOST = os.getenv("MONGODB_HOST ", "localhost")
 MONGODB_PORT = int(os.getenv("MONGODB_PORT ", "27017"))
@@ -32,14 +26,11 @@ MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME ", "opentelemetry-tests")
 MONGODB_COLLECTION_NAME = "test"
 
 
-class TestFunctionalPymongo(unittest.TestCase):
+class TestFunctionalPymongo(TestBase):
     @classmethod
     def setUpClass(cls):
-        cls._tracer_provider = TracerProvider()
-        cls._tracer = Tracer(cls._tracer_provider, None)
-        cls._span_exporter = InMemorySpanExporter()
-        cls._span_processor = SimpleExportSpanProcessor(cls._span_exporter)
-        cls._tracer_provider.add_span_processor(cls._span_processor)
+        super().setUpClass()
+        cls._tracer = cls.tracer_provider.get_tracer(__name__)
         trace_integration(cls._tracer)
         client = MongoClient(
             MONGODB_HOST, MONGODB_PORT, serverSelectionTimeoutMS=2000
@@ -47,11 +38,8 @@ class TestFunctionalPymongo(unittest.TestCase):
         db = client[MONGODB_DB_NAME]
         cls._collection = db[MONGODB_COLLECTION_NAME]
 
-    def setUp(self):
-        self._span_exporter.clear()
-
     def validate_spans(self):
-        spans = self._span_exporter.get_finished_spans()
+        spans = self.memory_exporter.get_finished_spans()
         self.assertEqual(len(spans), 2)
         for span in spans:
             if span.name == "rootSpan":
