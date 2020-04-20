1e48c7da7743fcfa35a7a0ab4b29c6c8488d5e28
Alex Boten <aboten@lightstep.com>
2020-04-20 14:09:46 -0700

    improving documentation

--- ext/opentelemetry-instrumentation-redis/src/opentelemetry/instrumentation/redis/__init__.py
+++ ext/opentelemetry-instrumentation-redis/src/opentelemetry/instrumentation/redis/__init__.py
@@ -12,24 +12,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""Instrument redis to report Redis queries.
+"""
+Instrument `redis`_ to report Redis queries.
 
 There are two options for instrumenting code. The first option is to use
 the `opentelemetry-auto-instrumentation` executable which will automatically
 patch your Redis client. The second is to programmatically enable
 instrumentation via the following code:
 
-::
+.. _redis: https://pypi.org/project/redis/
 
+Usage
+-----
+
+.. code:: python
+
+    from opentelemetry import trace
     from opentelemetry.instrumentation.redis.patch import patch
+    from opentelemetry.sdk.trace import TracerProvider
     import redis
 
+    trace.set_tracer_provider(TracerProvider())
+
     # You can patch redis specifically
-    patch()
+    patch(trace.get_tracer_provider())
 
     # This will report a span with the default settings
     client = redis.StrictRedis(host="localhost", port=6379)
     client.get("my-key")
+
+API
+---
 """
 from opentelemetry.auto_instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.instrumentation.redis.patch import patch, unpatch
