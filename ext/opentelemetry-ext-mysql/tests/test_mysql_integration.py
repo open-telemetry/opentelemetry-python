81add8957de23e8eac55a14d341fe2342c71b1be
Mauricio Vásquez <mauricio@kinvolk.io>
2020-04-21 20:34:16 -0500

    remove non relevant comment

--- ext/opentelemetry-ext-mysql/tests/test_mysql_integration.py
+++ ext/opentelemetry-ext-mysql/tests/test_mysql_integration.py
@@ -34,7 +34,6 @@ class TestMysqlIntegration(TestBase):
         spans_list = self.memory_exporter.get_finished_spans()
         self.assertEqual(len(spans_list), 1)
         span = spans_list[0]
-        # TODO: Add more tests?
 
         # Check version and name in span's instrumentation info
         self.check_span_instrumentation_info(span, opentelemetry.ext.mysql)
