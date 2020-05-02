da8c331c1fa77271c66ae50fab1d4f4dcd05ae69
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-05-01 18:51:12 -0600

    Add more documentation for the instrumentor base class
    Fixes #637

--- opentelemetry-auto-instrumentation/src/opentelemetry/auto_instrumentation/instrumentor.py
+++ opentelemetry-auto-instrumentation/src/opentelemetry/auto_instrumentation/instrumentor.py
@@ -24,7 +24,17 @@ _LOG = getLogger(__name__)
 
 
 class BaseInstrumentor(ABC):
-    """An ABC for instrumentors"""
+    """An ABC for instrumentors
+
+    Child classes of this ABC are implemented to instrument specific third
+    party libraries or frameworks either by using the
+    ``opentelemetry-auto-instrumentation`` command or by calling their methods
+    directly.
+
+    Since every third party library or framework is different and has different
+    instrumentation needs, more methods can be added to the child classes as
+    needed to provide practical instrumentation to the end user.
+    """
 
     _instance = None
     _is_instrumented = False
@@ -38,14 +48,35 @@ class BaseInstrumentor(ABC):
 
     @abstractmethod
     def _instrument(self, **kwargs):
-        """Instrument"""
+        """Instrument the library"""
 
     @abstractmethod
     def _uninstrument(self, **kwargs):
-        """Uninstrument"""
+        """Uninstrument the library"""
 
     def instrument(self, **kwargs):
-        """Instrument"""
+        """Instrument the library
+
+        This method will be called without any optional arguments by the
+        ``opentelemetry-auto-instrumentation`` command. The configuration of
+        the instrumentation when done in this way should be done by previously
+        setting the configuration (using environment variables or any other
+        mechanism) that will be used later by the code in the ``instrument``
+        implementation via the global ``Configuration`` object.
+
+        When this method is to be called directly in the user code, ``kwargs``
+        should be used to pass attributes that will override the configuration
+        values read by the ``Configuration`` object.
+
+        In this way, calling this method directly without passing any optional
+        values should do the very same thing that the
+        ``opentelemetry-auto-instrumentation`` command does. The idea behind
+        this approach is also to keep the ``Configuration`` object immutable.
+
+        This is necessary because this object is used by all the OpenTelemetry
+        components and any change to one of its attributes could break another
+        component, leading to very hard to debug bugs.
+        """
 
         if not self._is_instrumented:
             result = self._instrument(**kwargs)
@@ -57,7 +88,7 @@ class BaseInstrumentor(ABC):
         return None
 
     def uninstrument(self, **kwargs):
-        """Uninstrument"""
+        """Uninstrument the library"""
 
         if self._is_instrumented:
             result = self._uninstrument(**kwargs)
