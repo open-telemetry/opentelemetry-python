4e499159b69e5e13d07f8e3736a34db3c69ea24b
Mauricio Vásquez <mauricio@kinvolk.io>
2020-05-04 12:16:29 -0500

    ext/docker-tests: Fix SQL docker tests
    The executemany test fails in some conditions because the argument used is
    a sequence and not a sequence of sequences as it should be:
    
    https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-executemany.html
    https://pymysql.readthedocs.io/en/latest/modules/cursors.html#pymysql.cursors.Cursor.executemany
    https://www.psycopg.org/docs/cursor.html#cursor.executemany

--- ext/opentelemetry-ext-docker-tests/tests/mysql/test_mysql_functional.py
+++ ext/opentelemetry-ext-docker-tests/tests/mysql/test_mysql_functional.py
@@ -82,7 +82,7 @@ class TestFunctionalMysql(TestBase):
         """Should create a child span for executemany
         """
         with self._tracer.start_as_current_span("rootSpan"):
-            data = ["1", "2", "3"]
+            data = (("1",), ("2",), ("3",))
             stmt = "INSERT INTO test (id) VALUES (%s)"
             self._cursor.executemany(stmt, data)
         self.validate_spans()
