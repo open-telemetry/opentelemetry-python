a3fca93f2e72adcd601d847afb7dd3cc55d71c14
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-05-06 16:27:01 -0600

    Add Django instrumentation (#593)
    Initial Instrumentation
    
    Co-authored-by: Mauricio Vásquez <mauricio@kinvolk.io>
    Co-authored-by: Mathieu Hinderyckx <mathieu.hinderyckx@gmail.com>
    Co-authored-by: alrex <alrex.boten@gmail.com>
    Co-authored-by: Yusuke Tsutsumi <yusuke@tsutsumi.io>

---
+++ docs/examples/django/instrumentation_example/urls.py
@@ -0,0 +1,35 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""instrumentation_example URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path("", views.home, name="home")
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path("", Home.as_view(), name="home")
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("pages.urls")),
+]
