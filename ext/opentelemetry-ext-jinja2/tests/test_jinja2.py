86b60e23f69ac2ea0fe5bdaa16f0db5aae2fe489
Tahir H. Butt <tahir.butt@datadoghq.com>
2020-05-07 15:24:24 -0400

    add parent-child tests

--- ext/opentelemetry-ext-jinja2/tests/test_jinja2.py
+++ ext/opentelemetry-ext-jinja2/tests/test_jinja2.py
@@ -19,6 +19,7 @@ import jinja2
 from opentelemetry import trace as trace_api
 from opentelemetry.ext.jinja2 import Jinja2Instrumentor
 from opentelemetry.test.test_base import TestBase
+from opentelemetry.trace import get_tracer
 
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
 TMPL_DIR = os.path.join(TEST_DIR, "templates")
@@ -32,16 +33,22 @@ class TestJinja2Instrumentor(TestBase):
         # prevent cache effects when using Template('code...')
         # pylint: disable=protected-access
         jinja2.environment._spontaneous_environments.clear()
+        self.tracer = get_tracer(__name__)
 
     def tearDown(self):
         self.instrumentor.uninstrument()
 
     def test_render_inline_template(self):
-        template = jinja2.environment.Template("Hello {{name}}!")
-        self.assertEqual(template.render(name="Jinja"), "Hello Jinja!")
+        with self.tracer.start_as_current_span("test"):
+            template = jinja2.environment.Template("Hello {{name}}!")
+            self.assertEqual(template.render(name="Jinja"), "Hello Jinja!")
 
         spans = self.memory_exporter.get_finished_spans()
-        self.assertEqual(len(spans), 2)
+        self.assertEqual(len(spans), 3)
+
+        self.assertEqual(spans[0].parent, spans[2].get_context())
+        self.assertEqual(spans[1].parent, spans[2].get_context())
+        self.assertIsNone(spans[2].parent)
 
         self.assertEqual(spans[0].name, "jinja2.compile")
         self.assertEqual(spans[0].kind, trace_api.SpanKind.INTERNAL)
@@ -56,13 +63,18 @@ class TestJinja2Instrumentor(TestBase):
         )
 
     def test_generate_inline_template(self):
-        template = jinja2.environment.Template("Hello {{name}}!")
-        self.assertEqual(
-            "".join(template.generate(name="Jinja")), "Hello Jinja!"
-        )
+        with self.tracer.start_as_current_span("test"):
+            template = jinja2.environment.Template("Hello {{name}}!")
+            self.assertEqual(
+                "".join(template.generate(name="Jinja")), "Hello Jinja!"
+            )
 
         spans = self.memory_exporter.get_finished_spans()
-        self.assertEqual(len(spans), 2)
+        self.assertEqual(len(spans), 3)
+
+        self.assertEqual(spans[0].parent, spans[2].get_context())
+        self.assertEqual(spans[1].parent, spans[2].get_context())
+        self.assertIsNone(spans[2].parent)
 
         self.assertEqual(spans[0].name, "jinja2.compile")
         self.assertEqual(spans[0].kind, trace_api.SpanKind.INTERNAL)
@@ -77,15 +89,23 @@ class TestJinja2Instrumentor(TestBase):
         )
 
     def test_file_template(self):
-        loader = jinja2.loaders.FileSystemLoader(TMPL_DIR)
-        env = jinja2.Environment(loader=loader)
-        template = env.get_template("template.html")
-        self.assertEqual(
-            template.render(name="Jinja"), "Message: Hello Jinja!"
-        )
+        with self.tracer.start_as_current_span("test"):
+            loader = jinja2.loaders.FileSystemLoader(TMPL_DIR)
+            env = jinja2.Environment(loader=loader)
+            template = env.get_template("template.html")
+            self.assertEqual(
+                template.render(name="Jinja"), "Message: Hello Jinja!"
+            )
 
         spans = self.memory_exporter.get_finished_spans()
-        self.assertEqual(len(spans), 5)
+        self.assertEqual(len(spans), 6)
+
+        self.assertEqual(spans[0].parent, spans[1].get_context())
+        self.assertEqual(spans[1].parent, spans[5].get_context())
+        self.assertEqual(spans[2].parent, spans[3].get_context())
+        self.assertEqual(spans[3].parent, spans[4].get_context())
+        self.assertEqual(spans[4].parent, spans[5].get_context())
+        self.assertIsNone(spans[5].parent)
 
         self.assertEqual(spans[0].name, "jinja2.compile")
         self.assertEqual(spans[1].name, "jinja2.load")
