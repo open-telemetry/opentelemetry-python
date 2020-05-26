0c6aff3b454728df51b34ec8f8406dd96cd6179f
Leighton <lechen@microsoft.com>
2020-05-25 21:13:43 -0700

    lint

--- ext/opentelemetry-ext-flask/tests/test_programmatic.py
+++ ext/opentelemetry-ext-flask/tests/test_programmatic.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Flask, request
 from unittest.mock import patch
+
+from flask import Flask, request
 
 from opentelemetry import trace
 from opentelemetry.ext.flask import FlaskInstrumentor
