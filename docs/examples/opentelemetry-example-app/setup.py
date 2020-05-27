b14b1cf08f7975f72109530569397ba35062c7c6
Alex Boten <aboten@lightstep.com>
2020-05-27 15:26:52 -0700

    update version for master

--- docs/examples/opentelemetry-example-app/setup.py
+++ docs/examples/opentelemetry-example-app/setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="opentelemetry-example-app",
-    version="0.8b0",
+    version="0.9.dev0",
     author="OpenTelemetry Authors",
     author_email="cncf-opentelemetry-contributors@lists.cncf.io",
     classifiers=[
--- tests/util/src/opentelemetry/test/version.py
+++ tests/util/src/opentelemetry/test/version.py
@@ -1,1 +1,1 @@
-__version__ = "0.8b0"
+__version__ = "0.9.dev0"
