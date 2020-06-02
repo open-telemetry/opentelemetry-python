0853b7d122b9425750bdcbf4a7ad3030833340f1
ericmustin <mustin.eric@gmail.com>
2020-06-02 14:21:08 +0200

    ext/pymemcache: add license info and cleanup formatting

--- ext/opentelemetry-ext-pymemcache/src/opentelemetry/ext/pymemcache/__init__.py
+++ ext/opentelemetry-ext-pymemcache/src/opentelemetry/ext/pymemcache/__init__.py
@@ -1,3 +1,37 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Usage
+-----
+The OpenTelemetry ``jinja2`` integration traces templates loading, compilation
+and rendering.
+Usage
+-----
+.. code-block:: python
+    from opentelemetry import trace
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.ext.pymemcache import PymemcacheInstrumentor
+    trace.set_tracer_provider(TracerProvider())
+    trace.set_tracer_provider(TracerProvider())
+    from pymemcache.client.base import Client
+    client = Client(('localhost', 11211))
+    client.set('some_key', 'some_value')
+API
+---
+"""
+# pylint: disable=no-value-for-parameter
 
 import logging
 
