96c5cefc430f91ce6ff08b1fcf89c011a16d89f3
Seth Maxwell <sethmaxwl@gmail.com>
2020-06-03 21:12:08 +0000

    Add fix for byte type attributes

--- opentelemetry-sdk/src/opentelemetry/sdk/trace/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/trace/__init__.py
@@ -422,6 +422,8 @@ class Span(trace_api.Span):
             # Freeze mutable sequences defensively
             if isinstance(value, MutableSequence):
                 value = tuple(value)
+            if isinstance(value, bytes):
+                value = value.decode()
             with self._lock:
                 self.attributes[key] = value
 
