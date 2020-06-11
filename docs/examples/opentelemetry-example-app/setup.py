8c3de78f569604fc0d4237b5e6b9ace1630ce04c
alrex <aboten@lightstep.com>
2020-06-10 21:35:03 -0700

    release: updating version numbers and changelogs (#812)

--- docs/examples/opentelemetry-example-app/setup.py
+++ docs/examples/opentelemetry-example-app/setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="opentelemetry-example-app",
-    version="0.9.dev0",
+    version="0.9b0",
     author="OpenTelemetry Authors",
     author_email="cncf-opentelemetry-contributors@lists.cncf.io",
     classifiers=[
