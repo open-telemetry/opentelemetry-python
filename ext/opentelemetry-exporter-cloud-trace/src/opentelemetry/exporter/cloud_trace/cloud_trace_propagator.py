0f273c2118b285f83dbed20da64fa2b746152fd5
Andrew Xue <aaxue@google.com>
2020-06-16 17:00:03 -0400

    even more propagator tests bugfixes

--- ext/opentelemetry-exporter-cloud-trace/src/opentelemetry/exporter/cloud_trace/cloud_trace_propagator.py
+++ ext/opentelemetry-exporter-cloud-trace/src/opentelemetry/exporter/cloud_trace/cloud_trace_propagator.py
@@ -20,15 +20,13 @@ import opentelemetry.trace as trace
 from opentelemetry.context.context import Context
 from opentelemetry.trace.propagation import httptextformat
 from opentelemetry.trace.span import (
-    INVALID_SPAN_ID,
-    INVALID_TRACE_ID,
     SpanContext,
     TraceFlags,
     get_hexadecimal_trace_id,
 )
 
 _TRACE_CONTEXT_HEADER_NAME = "X-Cloud-Trace-Context"
-_TRACE_CONTEXT_HEADER_FORMAT = r"^(?P<trace_id>[0-9a-f]{32})\/(?P<span_id>[\d+]{1,20});o=(?P<trace_flags>\d+)$"
+_TRACE_CONTEXT_HEADER_FORMAT = r"^(?P<trace_id>[0-9a-f]{32})\/(?P<span_id>[\d]{1,20});o=(?P<trace_flags>\d+)$"
 _TRACE_CONTEXT_HEADER_RE = re.compile(_TRACE_CONTEXT_HEADER_FORMAT)
 
 
@@ -59,12 +57,7 @@ class CloudTraceFormatPropagator(httptextformat.HTTPTextFormat):
         span_id = match.group("span_id")
         trace_options = match.group("trace_flags")
 
-        if (
-            trace_id == "0" * 32
-            or span_id == "0" * 16
-            or trace_id == str(INVALID_TRACE_ID)
-            or span_id == str(INVALID_SPAN_ID)
-        ):
+        if trace_id == "0" * 32 or int(span_id) == 0:
             return trace.set_span_in_context(trace.INVALID_SPAN, context)
 
         span_context = SpanContext(
