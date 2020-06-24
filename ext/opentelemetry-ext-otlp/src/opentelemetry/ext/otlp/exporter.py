3f34857f354750760ca8d37df1e113a8dc3b47f2
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-06-24 11:52:03 -0600

    Fix docstring

--- ext/opentelemetry-ext-otlp/src/opentelemetry/ext/otlp/exporter.py
+++ ext/opentelemetry-ext-otlp/src/opentelemetry/ext/otlp/exporter.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""OTLP Span Exporter"""
+"""OTLP Exporter"""
 
 import logging
 from abc import ABC, abstractmethod
