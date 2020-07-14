55e1f3d04e4a279484ffecca6d26bf1c30d02d0e
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-07-14 16:54:30 -0600

    Fix a few issues with the Django example
    Fixes #914

--- docs/examples/django/pages/views.py
+++ docs/examples/django/pages/views.py
@@ -19,9 +19,7 @@ from opentelemetry.sdk.trace.export import (
     ConsoleSpanExporter,
     SimpleExportSpanProcessor,
 )
-
 trace.set_tracer_provider(TracerProvider())
-tracer = trace.get_tracer_provider().get_tracer(__name__)
 
 trace.get_tracer_provider().add_span_processor(
     SimpleExportSpanProcessor(ConsoleSpanExporter())
