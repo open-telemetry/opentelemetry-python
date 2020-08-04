2cdba5a9ed525eb2159a2acfa2ee02e154ed30f1
Connor Adams <connoradams@google.com>
2020-08-04 11:04:46 -0400

    stateful batcher fixes

--- opentelemetry-sdk/tests/metrics/test_view.py
+++ opentelemetry-sdk/tests/metrics/test_view.py
@@ -15,10 +15,12 @@
 import unittest
 from unittest import mock
 
+from opentelemetry import metrics as metrics_api
 from opentelemetry.sdk import metrics
-from opentelemetry.sdk.metrics import Counter, view
+from opentelemetry.sdk.metrics import Counter, ValueRecorder, view
 from opentelemetry.sdk.metrics.export import aggregate
 from opentelemetry.sdk.metrics.export.aggregate import (
+    HistogramAggregator,
     MinMaxSumCountAggregator,
     SumAggregator,
 )
@@ -183,6 +185,120 @@ class TestStateless(unittest.TestCase):
         label2 = (("customer_id", 247), ("environment", "production"))
         self.assertTrue((label1, 6) in sum_set)
         self.assertTrue((label2, 5) in sum_set)
+
+
+class TestHistogramView(unittest.TestCase):
+    def test_histogram_stateful(self):
+        # Use the meter type provided by the SDK package
+        metrics_api.set_meter_provider(metrics.MeterProvider())
+        meter = metrics_api.get_meter(__name__)
+        exporter = InMemoryMetricsExporter()
+        controller = PushController(meter, exporter, 5)
+
+        requests_size = meter.create_metric(
+            name="requests_size",
+            description="size of requests",
+            unit="1",
+            value_type=int,
+            metric_type=ValueRecorder,
+        )
+
+        size_view = View(
+            requests_size,
+            HistogramAggregator,
+            aggregator_config={"bounds": [20, 40, 60, 80, 100]},
+            label_keys=["environment"],
+            view_config=ViewConfig.LABEL_KEYS,
+        )
+
+        meter.register_view(size_view)
+
+        # Since this is using the HistogramAggregator, the bucket counts will be reflected
+        # with each record
+        requests_size.record(25, {"environment": "staging", "test": "value"})
+        requests_size.record(1, {"environment": "staging", "test": "value2"})
+        requests_size.record(200, {"environment": "staging", "test": "value3"})
+
+        controller.tick()
+
+        metrics_list = exporter.get_exported_metrics()
+        self.assertEqual(len(metrics_list), 1)
+        checkpoint = metrics_list[0].aggregator.checkpoint
+        self.assertEqual(
+            tuple(checkpoint.items()),
+            ((20, 1), (40, 1), (60, 0), (80, 0), (100, 0), (">", 1)),
+        )
+        exporter.clear()
+
+        requests_size.record(25, {"environment": "staging", "test": "value"})
+        requests_size.record(1, {"environment": "staging", "test": "value2"})
+        requests_size.record(200, {"environment": "staging", "test": "value3"})
+
+        controller.tick()
+
+        metrics_list = exporter.get_exported_metrics()
+        self.assertEqual(len(metrics_list), 1)
+        checkpoint = metrics_list[0].aggregator.checkpoint
+        self.assertEqual(
+            tuple(checkpoint.items()),
+            ((20, 2), (40, 2), (60, 0), (80, 0), (100, 0), (">", 2)),
+        )
+
+    def test_histogram_stateless(self):
+        # Use the meter type provided by the SDK package
+        metrics_api.set_meter_provider(metrics.MeterProvider(stateful=False))
+        meter = metrics_api.get_meter(__name__)
+        exporter = InMemoryMetricsExporter()
+        controller = PushController(meter, exporter, 5)
+
+        requests_size = meter.create_metric(
+            name="requests_size",
+            description="size of requests",
+            unit="1",
+            value_type=int,
+            metric_type=ValueRecorder,
+        )
+
+        size_view = View(
+            requests_size,
+            HistogramAggregator,
+            aggregator_config={"bounds": [20, 40, 60, 80, 100]},
+            label_keys=["environment"],
+            view_config=ViewConfig.LABEL_KEYS,
+        )
+
+        meter.register_view(size_view)
+
+        # Since this is using the HistogramAggregator, the bucket counts will be reflected
+        # with each record
+        requests_size.record(25, {"environment": "staging", "test": "value"})
+        requests_size.record(1, {"environment": "staging", "test": "value2"})
+        requests_size.record(200, {"environment": "staging", "test": "value3"})
+
+        controller.tick()
+
+        metrics_list = exporter.get_exported_metrics()
+        self.assertEqual(len(metrics_list), 1)
+        checkpoint = metrics_list[0].aggregator.checkpoint
+        self.assertEqual(
+            tuple(checkpoint.items()),
+            ((20, 1), (40, 1), (60, 0), (80, 0), (100, 0), (">", 1)),
+        )
+        exporter.clear()
+
+        requests_size.record(25, {"environment": "staging", "test": "value"})
+        requests_size.record(1, {"environment": "staging", "test": "value2"})
+        requests_size.record(200, {"environment": "staging", "test": "value3"})
+
+        controller.tick()
+
+        metrics_list = exporter.get_exported_metrics()
+        self.assertEqual(len(metrics_list), 1)
+        checkpoint = metrics_list[0].aggregator.checkpoint
+        self.assertEqual(
+            tuple(checkpoint.items()),
+            ((20, 1), (40, 1), (60, 0), (80, 0), (100, 0), (">", 1)),
+        )
 
 
 class DummyMetric(metrics.Metric):
