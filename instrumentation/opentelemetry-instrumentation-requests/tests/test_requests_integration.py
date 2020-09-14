b11f9dd764d74045611198edf67dc3bca9aeac5e
Leighton <lechen@microsoft.com>
2020-09-14 13:45:00 -0400

    lint

--- instrumentation/opentelemetry-instrumentation-requests/tests/test_requests_integration.py
+++ instrumentation/opentelemetry-instrumentation-requests/tests/test_requests_integration.py
@@ -162,7 +162,7 @@ class RequestsIntegrationTestBase(abc.ABC):
             self.assertEqual(result.text, "Hello!")
             self.assert_span(None, 0)
             with self.assertRaises(AttributeError):
-                attr = mock_span.attributes
+                mock_span.attributes
             self.assertFalse(mock_span.is_recording())
             self.assertTrue(mock_span.is_recording.called)
             self.assertFalse(mock_span.set_attribute.called)
