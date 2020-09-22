f49939afd41198ac6f3aedb3c33242d115540b7a
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-09-22 16:27:31 -0600

    Add test and expose b3 propagator via entry points

--- opentelemetry-api/tests/propagators/test_propagators.py
+++ opentelemetry-api/tests/propagators/test_propagators.py
@@ -13,10 +13,12 @@
 # limitations under the License.
 
 from importlib import reload
+from os import environ
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from opentelemetry.baggage.propagation import BaggagePropagator
+from opentelemetry.configuration import Configuration
 from opentelemetry.trace.propagation.tracecontext import (
     TraceContextTextMapPropagator,
 )
@@ -33,6 +35,44 @@ class TestPropagators(TestCase):
             self.assertEqual(
                 propagators, {TraceContextTextMapPropagator, BaggagePropagator}
             )
+
+        mock_compositehttppropagator.configure_mock(
+            **{"side_effect": test_propagators}
+        )
+
+        import opentelemetry.propagators
+
+        reload(opentelemetry.propagators)
+
+    @patch.dict(environ, {"OTEL_PROPAGATORS": "a,b,c"})
+    @patch("opentelemetry.propagators.composite.CompositeHTTPPropagator")
+    @patch("pkg_resources.iter_entry_points")
+    def test_non_default_propagators(
+        self, mock_iter_entry_points, mock_compositehttppropagator
+    ):
+
+        Configuration._reset()
+
+        def iter_entry_points_mock(_, propagator):
+            return iter(
+                [
+                    Mock(
+                        **{
+                            "load.side_effect": [
+                                Mock(**{"side_effect": [propagator]})
+                            ]
+                        }
+                    )
+                ]
+            )
+
+        mock_iter_entry_points.configure_mock(
+            **{"side_effect": iter_entry_points_mock}
+        )
+
+        def test_propagators(propagators):
+
+            self.assertEqual(propagators, ["a", "b", "c"])
 
         mock_compositehttppropagator.configure_mock(
             **{"side_effect": test_propagators}
