982b667ab7d32de45ed3b9cd6981f37a9e04975b
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-09-30 15:45:19 -0600

    Use right case (#1187)
    Fixes #1186

--- opentelemetry-api/src/opentelemetry/configuration/__init__.py
+++ opentelemetry-api/src/opentelemetry/configuration/__init__.py
@@ -43,7 +43,7 @@ freely because instantiating it returns always the same object.
 
 For example, if the environment variable
 ``OTEL_PYTHON_METER_PROVIDER`` value is ``my_meter_provider``, then
-``Configuration().meter_provider == "my_meter_provider"`` would be ``True``.
+``Configuration().METER_PROVIDER == "my_meter_provider"`` would be ``True``.
 
 Non defined attributes will always return ``None``. This is intended to make it
 easier to use the ``Configuration`` object in actual code, because it won't be
