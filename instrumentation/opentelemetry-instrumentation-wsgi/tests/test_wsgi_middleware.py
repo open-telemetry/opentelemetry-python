2a77bf95c4465974d216ac3cf10d02bb59f15e3d
Leighton <lechen@microsoft.com>
2020-10-08 15:12:59 -0400

    record

--- instrumentation/opentelemetry-instrumentation-wsgi/tests/test_wsgi_middleware.py
+++ instrumentation/opentelemetry-instrumentation-wsgi/tests/test_wsgi_middleware.py
@@ -131,7 +131,7 @@ class TestWsgiApplication(WsgiTestBase):
         mock_span.is_recording.return_value = False
         mock_tracer.start_span.return_value = mock_span
         mock_tracer.use_span.return_value.__enter__ = mock_span
-        mock_tracer.use_span.return_value.__exit__ = True
+        mock_tracer.use_span.return_value.__exit__ = mock_span
         with mock.patch("opentelemetry.trace.get_tracer") as tracer:
             tracer.return_value = mock_tracer
             app = otel_wsgi.OpenTelemetryMiddleware(simple_wsgi)
