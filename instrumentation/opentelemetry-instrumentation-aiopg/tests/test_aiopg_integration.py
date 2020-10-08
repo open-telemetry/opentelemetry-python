74ba6e76a3e4f7d60865fbc806dd0e22625401dc
Leighton <lechen@microsoft.com>
2020-10-08 15:05:35 -0400

    Update test_aiopg_integration.py

--- instrumentation/opentelemetry-instrumentation-aiopg/tests/test_aiopg_integration.py
+++ instrumentation/opentelemetry-instrumentation-aiopg/tests/test_aiopg_integration.py
@@ -248,7 +248,7 @@ class TestAiopgIntegration(TestBase):
         mock_span.is_recording.return_value = False
         mock_tracer.start_span.return_value = mock_span
         mock_tracer.use_span.return_value.__enter__ = mock_span
-        mock_tracer.use_span.return_value.__exit__ = mock_span
+        mock_tracer.use_span.return_value.__exit__ = True
         db_integration = AiopgIntegration(
             mock_tracer, "testcomponent", "testtype", connection_attributes
         )
