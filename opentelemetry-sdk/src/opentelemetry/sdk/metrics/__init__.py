a50e0efe10c672c4ed47e16ba2fe860c774f7cb2
Leighton <lechen@microsoft.com>
2020-10-19 11:25:23 -0400

    typevar

--- opentelemetry-sdk/src/opentelemetry/sdk/metrics/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/metrics/__init__.py
@@ -15,7 +15,7 @@
 import atexit
 import logging
 import threading
-from typing import Dict, Sequence, Tuple, Type
+from typing import Dict, Sequence, Tuple, Type, TypeVar
 
 from opentelemetry import metrics as metrics_api
 from opentelemetry.sdk.metrics.export import (
