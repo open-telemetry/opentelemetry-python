8f38a6a495c442f20f9e80c030e096032da7f139
Rob Knox <rob.knox.dev@gmail.com>
2020-11-17 19:34:42 -0800

    correcting exporter init logic for precedence of param, env var, defaults

--- exporter/opentelemetry-exporter-zipkin/src/opentelemetry/exporter/zipkin/__init__.py
+++ exporter/opentelemetry-exporter-zipkin/src/opentelemetry/exporter/zipkin/__init__.py
@@ -130,9 +130,9 @@ class ZipkinSpanExporter(SpanExporter):
 
     def __init__(
         self,
-        service_name: str = DEFAULT_SERVICE_NAME,
-        endpoint: str = DEFAULT_ENDPOINT,
-        encoding: Encoding = DEFAULT_ENCODING,
+        service_name: str = None,
+        endpoint: str = None,
+        encoding: Encoding = None,
         encoder: Encoder = None,
         sender: Sender = None,
     ):
@@ -159,16 +159,21 @@ class ZipkinSpanExporter(SpanExporter):
         'endpoint' params, the sender will take precedence and ignore those
         params.
         """
-        env_encoding = Configuration().EXPORTER_ZIPKIN_ENCODING
-        if env_encoding is not None:
-            encoding = Encoding(env_encoding)
+        if encoding is None:
+            env_encoding = Configuration().EXPORTER_ZIPKIN_ENCODING
+            if env_encoding is not None:
+                encoding = Encoding(env_encoding)
+            else:
+                encoding = DEFAULT_ENCODING
 
         if encoder is not None:
             self.encoder = encoder
         else:
-            service_name = (
-                Configuration().EXPORTER_ZIPKIN_SERVICE_NAME or service_name
-            )
+            if service_name is None:
+                service_name = (
+                    Configuration().EXPORTER_ZIPKIN_SERVICE_NAME
+                    or DEFAULT_SERVICE_NAME
+                )
             # TODO: add logic to determine primary ipv4 and ipv6 addresses to
             #  pass into Endpoint constructor.
             local_endpoint = Endpoint(service_name)
@@ -179,9 +184,15 @@ class ZipkinSpanExporter(SpanExporter):
             elif encoding == Encoding.V2_PROTOBUF:
                 self.encoder = ProtobufEncoder(local_endpoint)
 
-        endpoint = Configuration().EXPORTER_ZIPKIN_ENDPOINT or endpoint
-
-        self.sender = sender or HttpSender(endpoint, encoding)
+        if sender is not None:
+            self.sender = sender
+        else:
+            if endpoint is None:
+                endpoint = (
+                    Configuration().EXPORTER_ZIPKIN_ENDPOINT
+                    or DEFAULT_ENDPOINT
+                )
+            self.sender = HttpSender(endpoint, encoding)
 
     def export(self, spans: Sequence[Span]) -> SpanExportResult:
         return self.sender.send(self.encoder.encode(spans))
