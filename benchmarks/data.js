5996b941d9f7d0500029fe493c15a04a8938ce48
github-action-benchmark <github@users.noreply.github.com>
2020-12-14 21:30:12 +0000

    add OpenTelemetry Python Benchmarks - Python 3.9 - core (pytest) benchmark result for 47d6e95057c3a44faaea8562e2875e6d52f3fdab

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1607962566144,
+  "lastUpdate": 1607981412740,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 -": [
@@ -358,6 +358,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000002865030935316055",
             "extra": "mean: 46.774935527901185 usec\nrounds: 7321"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "enowell@amazon.com",
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "47d6e95057c3a44faaea8562e2875e6d52f3fdab",
+          "message": "Do not try to comment on PR after benchmarks (#1478)",
+          "timestamp": "2020-12-14T13:28:12-08:00",
+          "tree_id": "dfe9ff194aba91d6064941b7f51b5d80926e69ed",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/47d6e95057c3a44faaea8562e2875e6d52f3fdab"
+        },
+        "date": 1607981411826,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 31784.94639844907,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000011920796582988882",
+            "extra": "mean: 31.46143421052912 usec\nrounds: 2888"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 24033.46177364314,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000013438538883301935",
+            "extra": "mean: 41.6086541929916 usec\nrounds: 8097"
           }
         ]
       }
