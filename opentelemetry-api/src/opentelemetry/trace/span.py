9e9a5639768d6e5959f9ed96b4896470e0e2d58f
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2020-12-24 14:44:57 +0530

    Update docstring

--- opentelemetry-api/src/opentelemetry/trace/span.py
+++ opentelemetry-api/src/opentelemetry/trace/span.py
@@ -150,6 +150,9 @@ class TraceState(typing.Mapping[str, str]):
     Implementations should conform to the `W3C Trace Context - Tracestate`_
     spec, which describes additional restrictions on valid field values.
 
+    Args:
+        entries: An optional list of key-value pairs to initialize
+
     .. _W3C Trace Context - Tracestate:
         https://www.w3.org/TR/trace-context/#tracestate-field
     """
