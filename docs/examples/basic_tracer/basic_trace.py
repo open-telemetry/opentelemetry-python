fc8a139dde9f82f8c98c392764fb11b7ec73945f
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2021-01-26 10:34:58 +0530

    test

--- docs/examples/basic_tracer/basic_trace.py
+++ docs/examples/basic_tracer/basic_trace.py
@@ -4,6 +4,7 @@ from opentelemetry.sdk.trace.export import (
     ConsoleSpanExporter,
     SimpleExportSpanProcessor,
 )
+
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(
     SimpleExportSpanProcessor(ConsoleSpanExporter())
