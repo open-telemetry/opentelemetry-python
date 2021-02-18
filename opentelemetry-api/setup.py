1ef61aae7720d7a3afd5b321e95077b2a8d43c7a
Diego Hurtado <ocelotl@users.noreply.github.com>
2021-02-17 23:09:25 -0600

    Update warning

--- opentelemetry-api/setup.py
+++ opentelemetry-api/setup.py
@@ -26,16 +26,26 @@ with open(VERSION_FILENAME) as f:
 setuptools.setup(version=PACKAGE_INFO["__version__"],)
 
 sitecustomize_content = """import time
-print("You are using 3.6!")
+from logging import getLogger
 
 def time_ns():
     return int(time.time() * 1e9)
 
 time.time_ns = time_ns
+
+getLogger(__name__).warning(
+    "You are using a Python version previous than 3.7. Your Python version "
+    "does not include a function to get timestamps in nanoseconds. A time_ns "
+    "function has been added to the time module, for the purpose of you being "
+    "able to have a consistent import path with a more recent Python version. "
+    "This time_ns function has not the same resolution as the time_ns "
+    "function introduced in 3.7. Refer to PEP 564 for more information. "
+    "Please upgrade your Python version to 3.7 or newer."
+)
 """
 
 
-if version_info.minor == 6:
+if version_info.minor < 7:
     try:
         with open(
             join(get_python_lib(), "sitecustomize.py"),
