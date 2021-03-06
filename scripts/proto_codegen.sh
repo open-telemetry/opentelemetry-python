afbf2daae7d96f19aaeee8ca1cfb2a170ef6393e
Owais Lone <owais@lone.pw>
2021-03-06 13:58:18 +0530

    Use opentelemetry-proto 0.7.0
    Fixes #1667

--- scripts/proto_codegen.sh
+++ scripts/proto_codegen.sh
@@ -12,7 +12,7 @@
 #   PROTO_REPO_DIR - the path to an existing checkout of the opentelemetry-proto repo
 
 # Pinned commit/branch/tag for the current version used in opentelemetry-proto python package.
-PROTO_REPO_BRANCH_OR_COMMIT="v0.6.0"
+PROTO_REPO_BRANCH_OR_COMMIT="v0.7.0"
 
 set -e
 
