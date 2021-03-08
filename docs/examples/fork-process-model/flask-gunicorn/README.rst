c387aa382cc31e2e2afb777fa93e579ca60776ad
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2021-03-09 01:25:13 +0530

    Document how to work with fork process web server models(Gunicorn, uWSGI etc...) (#1609)

---
+++ docs/examples/fork-process-model/flask-gunicorn/README.rst
@@ -0,0 +1,11 @@
+Installation
+------------
+.. code-block:: sh
+
+    pip install -rrequirements.txt
+
+Run application
+---------------
+.. code-block:: sh
+
+    gunicorn app -c gunicorn.config.py
---
+++ docs/examples/fork-process-model/flask-gunicorn/gunicorn.config.py
@@ -0,0 +1,50 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+bind = "127.0.0.1:8000"
+
+# Sample Worker processes
+workers = 4
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Sample logging
+errorlog = "-"
+loglevel = "info"
+accesslog = "-"
+access_log_format = (
+    '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+)
+
+
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+    resource = Resource.create(attributes={"service.name": "api-service"})
+
+    trace.set_tracer_provider(TracerProvider(resource=resource))
+    # This uses insecure connection for the purpose of example. Please see the
+    # OTLP Exporter documentation for other options.
+    span_processor = BatchSpanProcessor(
+        OTLPSpanExporter(endpoint="localhost:4317", insecure=True)
+    )
+    trace.get_tracer_provider().add_span_processor(span_processor)
