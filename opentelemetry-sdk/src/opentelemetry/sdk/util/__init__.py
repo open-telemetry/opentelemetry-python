a4c4562bd63c00e1ee989e227e6926ef793d0d5c
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2021-03-23 17:13:02 +0900

    Fix regexp string

--- opentelemetry-sdk/src/opentelemetry/sdk/util/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/util/__init__.py
@@ -22,7 +22,7 @@ from typing import Dict, Optional
 
 from opentelemetry import trace as trace_api
 
-_tz_regexp = re.compile("([+-])(\\d{2}):(\d{2})$")
+_tz_regexp = re.compile("([+-])(\\d{2}):(\\d{2})$")
 
 
 def ns_to_iso_str(nanoseconds):
