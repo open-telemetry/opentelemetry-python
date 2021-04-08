d13a7484f1951433bd1d63e3d482d8175b12e0a9
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2021-04-08 18:07:35 +0530

    Uncomment repo clone part

--- scripts/semconv/generate.sh
+++ scripts/semconv/generate.sh
@@ -8,15 +8,15 @@ SPEC_VERSION=v1.1.0
 
 cd ${SCRIPT_DIR}
 
-# rm -rf opentelemetry-specification || true
-# mkdir opentelemetry-specification
-# cd opentelemetry-specification
+rm -rf opentelemetry-specification || true
+mkdir opentelemetry-specification
+cd opentelemetry-specification
 
-# git init
-# git remote add origin https://github.com/open-telemetry/opentelemetry-specification.git
-# git fetch origin "$SPEC_VERSION"
-# git reset --hard FETCH_HEAD
-# cd ${SCRIPT_DIR}
+git init
+git remote add origin https://github.com/open-telemetry/opentelemetry-specification.git
+git fetch origin "$SPEC_VERSION"
+git reset --hard FETCH_HEAD
+cd ${SCRIPT_DIR}
 
 docker run --rm \
   -v ${SCRIPT_DIR}/opentelemetry-specification/semantic_conventions/trace:/source \
