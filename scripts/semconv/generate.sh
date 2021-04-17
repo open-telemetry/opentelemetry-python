6967fe2491f71597c387a66761764e6f61af9410
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2021-04-17 05:54:48 +0530

    Update semconv generate script

--- scripts/semconv/generate.sh
+++ scripts/semconv/generate.sh
@@ -21,21 +21,21 @@ cd ${SCRIPT_DIR}
 docker run --rm \
   -v ${SCRIPT_DIR}/opentelemetry-specification/semantic_conventions/trace:/source \
   -v ${SCRIPT_DIR}/templates:/templates \
-  -v ${ROOT_DIR}/opentelemetry-api/src/opentelemetry/trace/:/output \
+  -v ${ROOT_DIR}/opentelemetry-semantic-conventions/src/opentelemetry/semconv/trace/:/output \
   otel/semconvgen:0.2.1 \
   -f /source code \
   --template /templates/semantic_attributes.j2 \
-  --output /output/attributes.py \
+  --output /output/__init__.py \
   -Dclass=SpanAttributes
 
 docker run --rm \
   -v ${SCRIPT_DIR}/opentelemetry-specification/semantic_conventions/resource:/source \
   -v ${SCRIPT_DIR}/templates:/templates \
-  -v ${ROOT_DIR}/opentelemetry-sdk/src/opentelemetry/sdk/resources/:/output \
+  -v ${ROOT_DIR}/opentelemetry-semantic-conventions/src/opentelemetry/semconv/resources/:/output \
   otel/semconvgen:0.2.1 \
   -f /source code \
   --template /templates/semantic_attributes.j2 \
-  --output /output/attributes.py \
+  --output /output/__init__.py \
   -Dclass=ResourceAttributes
 
 cd "$ROOT_DIR"
