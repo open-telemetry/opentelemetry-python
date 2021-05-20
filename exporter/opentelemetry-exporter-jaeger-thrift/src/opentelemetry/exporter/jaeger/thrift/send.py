77e0d0b4c96520d193f5c9eb64efe52ab437390a
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2021-05-21 00:04:43 +0530

    Remove unused import

--- exporter/opentelemetry-exporter-jaeger-thrift/src/opentelemetry/exporter/jaeger/thrift/send.py
+++ exporter/opentelemetry-exporter-jaeger-thrift/src/opentelemetry/exporter/jaeger/thrift/send.py
@@ -23,8 +23,6 @@ from thrift.transport import THttpClient, TTransport
 from opentelemetry.exporter.jaeger.thrift.gen.agent import Agent as agent
 from opentelemetry.exporter.jaeger.thrift.gen.jaeger import Collector as jaeger
 
-from opentelemetry.sdk.trace.export import SpanExportResult
-
 UDP_PACKET_MAX_LENGTH = 65000
 
 
