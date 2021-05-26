fc07d85e7ccdd823ce8258f58ee1f0e77272a99b
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2021-05-26 18:02:45 +0530

    WIP: Logging SDK

---
+++ opentelemetry-sdk/src/opentelemetry/sdk/logs/__init__.py
@@ -0,0 +1,308 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import abc
+import atexit
+import concurrent.futures
+import logging
+import threading
+from typing import Any, Callable, Optional, Tuple, Union
+
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.util import _time_ns
+from opentelemetry.sdk.util.instrumentation import InstrumentationInfo
+from opentelemetry.trace import get_current_span
+from opentelemetry.trace.span import TraceFlags
+from opentelemetry.util.types import Attributes
+
+
+class LogRecord:
+    """A LogRecord instance represents an event being logged.
+
+    LogRecord instances are created every time something is logged.
+    They contain all the information pertinent to the event being logged.
+    """
+
+    def __init__(
+        self,
+        timestamp: Optional[int] = None,
+        trace_id: Optional[int] = None,
+        span_id: Optional[int] = None,
+        trace_flags: Optional[TraceFlags] = None,
+        severity_text: Optional[str] = None,
+        severity_number: Optional[int] = None,
+        name: Optional[str] = None,
+        body: Optional[str] = None,
+        resource: Optional[Resource] = None,
+        attributes: Optional[Attributes] = None,
+    ):
+        self.timestamp = timestamp
+        self.trace_id = trace_id
+        self.span_id = span_id
+        self.trace_flags = trace_flags
+        self.severity_text = severity_text
+        self.severity_number = severity_number
+        self.name = name
+        self.body = body
+        self.resource = resource
+        self.attributes = attributes
+
+
+class LogData:
+    """Readable LogRecord data plus associated Resource and InstrumentationLibrary."""
+
+    def __init__(
+        self, log_record: LogRecord, instrumentation_info: InstrumentationInfo
+    ):
+        self.log_record = log_record
+        self.instrumentation_info = instrumentation_info
+
+
+class LogProcessor(abc.ABC):
+    @abc.abstractmethod
+    def emit(self, log_data: LogData):
+        """Emits the LogData"""
+
+    @abc.abstractmethod
+    def shutdown(self):
+        """Called when a :class:`opentelemetry.sdk.logs.LogEmitter` is shutdown"""
+
+    @abc.abstractmethod
+    def force_flush(self, timeout_millis: int = 30000):
+        """Export all the received logs to the configured Exporter that have not yet
+        been exported.
+
+        Args:
+            timeout_millis: The maximum amount of time to wait for logs to be
+                exported.
+
+        Returns:
+            False if the timeout is exceeded, True otherwise.
+        """
+
+
+class SynchronousMultiLogProcessor(LogProcessor):
+    def __init__(self):
+        # use a tuple to avoid race conditions when adding a new log and
+        # iterating through it on "emit".
+        self._log_processors = ()  # type: Tuple[LogProcessor, ...]
+        self._lock = threading.Lock()
+
+    def add_log_processor(self, log_processor: LogProcessor) -> None:
+        """Adds a Logprocessor to the list of log processors handled by this instance"""
+        with self._lock:
+            self._log_processors = self._log_processors + (log_processor,)
+
+    def emit(self, log_data: LogData) -> None:
+        for lp in self._log_processors:
+            lp.emit(log_data)
+
+    def shutdown(self) -> None:
+        """Shutdown the log processors one by one"""
+        for lp in self._log_processors:
+            lp.shutdown()
+
+    def force_flush(self, timeout_millis: int = 30000) -> bool:
+        """Force flush the log processors one by one
+
+        Args:
+            timeout_millis: The maximum amount of time to wait for logs to be
+                exported. If the first n log processors exceeded the timeout
+                then remaining log processors will not be flushed.
+
+        Returns:
+            True if all the log processors flushes the logs within timeout,
+            False otherwise.
+        """
+        deadline_ns = _time_ns() + timeout_millis * 1000000
+        for lp in self._log_processors:
+            current_ts = _time_ns()
+            if current_ts >= deadline_ns:
+                return False
+
+            if not lp.force_flush((deadline_ns - current_ts) // 1000000):
+                return False
+
+        return True
+
+
+class ConcurrentMultiLogProcessor(LogProcessor):
+    def __init__(self, max_workers: int = 2):
+        # use a tuple to avoid race conditions when adding a new log and
+        # iterating through it on "emit".
+        self._log_processors = ()  # type: Tuple[LogProcessor, ...]
+        self._lock = threading.Lock()
+        self._executor = concurrent.futures.ThreadPoolExecutor(
+            max_workers=max_workers
+        )
+
+    def add_log_processor(self, log_processor: LogProcessor):
+        with self._lock:
+            self._log_processors = self._log_processors + (log_processor,)
+
+    def _submit_and_wait(
+        self,
+        func: Callable[[LogProcessor], Callable[..., None]],
+        *args: Any,
+        **kwargs: Any,
+    ):
+        futures = []
+        for lp in self._log_processors:
+            future = self._executor.submit(func(lp), *args, **kwargs)
+            futures.append(future)
+        for future in futures:
+            future.result()
+
+    def emit(self, log_data: LogData):
+        self._submit_and_wait(lambda lp: lp.emit, log_data)
+
+    def shutdown(self):
+        self._submit_and_wait(lambda lp: lp.shutdown)
+
+    def force_flush(self, timeout_millis: int = 30000):
+        """Force flush the log processors in parallel.
+
+        Args:
+            timeout_millis: The maximum amount of time to wait for logs to be
+                exported.
+
+        Returns:
+            True if all the log processors flushes the logs within timeout,
+            False otherwise.
+        """
+        futures = []
+        for lp in self._log_processors:
+            future = self._executor.submit(lp.force_flush, timeout_millis)
+            futures.append(future)
+
+        done_futures, not_done_futures = concurrent.futures.wait(
+            futures, timeout_millis / 1e3
+        )
+
+        if not_done_futures:
+            return False
+
+        for future in done_futures:
+            if not future.result():
+                return False
+
+        return True
+
+
+class LogEmitter(logging.Handler):
+    def __init__(
+        self,
+        resource: Resource,
+        multi_log_processor: Union[
+            SynchronousMultiLogProcessor, ConcurrentMultiLogProcessor
+        ],
+        instrumentation_info: InstrumentationInfo,
+    ):
+        super().__init__()
+        self._resource = resource
+        self._multi_log_processor = multi_log_processor
+        self._instrumentation_info = instrumentation_info
+
+    def emit(self, record: logging.LogRecord):
+        """Emits the OTEL :class:`LogRecord` by translating `logging.LogRecord`."""
+        timestamp = int(record.created * 1e9)
+        span_context = get_current_span().get_span_context()
+        attributes: Attributes = {}
+        log_record = LogRecord(
+            timestamp=timestamp,
+            trace_id=span_context.trace_id,
+            span_id=span_context.span_id,
+            trace_flags=span_context.trace_flags,
+            severity_text=record.levelname,
+            severity_number=record.levelno,
+            name=record.name,
+            body=record.msg,
+            resource=self._resource,
+            attributes=attributes,
+        )
+        log_data = LogData(log_record, self._instrumentation_info)
+        self._multi_log_processor.emit(log_data)
+
+    def flush(self):
+        """Ensure all logging output has been flushed."""
+        self._multi_log_processor.force_flush()
+
+
+class LogEmitterProvider:
+    def __init__(
+        self,
+        resource: Resource = Resource.create(),
+        shutdown_on_exit: bool = True,
+        multi_log_processor: Union[
+            SynchronousMultiLogProcessor, ConcurrentMultiLogProcessor
+        ] = None,
+    ):
+        self._resource = resource
+        self._multi_log_processor = (
+            multi_log_processor or SynchronousMultiLogProcessor()
+        )
+        self._at_exit_handler = None
+        if shutdown_on_exit:
+            self._at_exit_handler = atexit.register(self.shutdown)
+
+    @property
+    def resource(self) -> Resource:
+        return self._resource
+
+    def get_log_emitter(
+        self,
+        instrumenting_module_name: str,
+        instrumenting_module_verison: str = "",
+    ) -> LogEmitter:
+        return LogEmitter(
+            self._resource,
+            self._multi_log_processor,
+            InstrumentationInfo(
+                instrumenting_module_name, instrumenting_module_verison
+            ),
+        )
+
+    def add_log_processor(self, log_processor: LogProcessor):
+        """Registers a new :class:`LogProcessor` for this `LogEmitterProvider` instance.
+
+        The log processors are invoked in the same order they are registered.
+        """
+        self._multi_log_processor.add_log_processor(log_processor)
+
+    def shutdown(self):
+        """Shuts down the log processors."""
+        self._multi_log_processor.shutdown()
+        if self._at_exit_handler is not None:
+            atexit.unregister(self._at_exit_handler)
+            self._at_exit_handler = None
+
+    def force_flush(self, timeout_millis: int = 30000) -> bool:
+        """Force flush the log processors.
+
+        By default force flush is called sequentially on all added log
+        processors. This means that log processors further back in the list
+        have less time to flush their logs.
+        To have log processors flush their logs in parallel it is possible to
+        initialize the tracer provider with an instance of
+        `ConcurrentMultiLogProcessor` at the cost of using multiple threads.
+
+        Args:
+            timeout_millis: The maximum amount of time to wait for logs to be
+                exported.
+
+        Returns:
+            True if all the log processors flushes the logs within timeout,
+            False otherwise.
+        """
+        return self._multi_log_processor.force_flush(timeout_millis)
