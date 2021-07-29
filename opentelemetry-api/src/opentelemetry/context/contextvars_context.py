ced39ebecda434c13f7ab8185cd6e1365c6c121b
Diego Hurtado <ocelotl@users.noreply.github.com>
2021-07-29 15:53:02 -0600

    Remove contextvars workaround
    Fixes #2008

--- opentelemetry-api/src/opentelemetry/context/contextvars_context.py
+++ opentelemetry-api/src/opentelemetry/context/contextvars_context.py
@@ -12,19 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from contextvars import ContextVar
-from sys import version_info
 
 from opentelemetry.context.context import Context, _RuntimeContext
-
-if (3, 5, 3) <= version_info < (3, 7):
-    import aiocontextvars  # type: ignore # pylint:disable=import-error
-
-    aiocontextvars  # pylint:disable=pointless-statement
-
-elif (3, 4) < version_info <= (3, 5, 2):
-    import opentelemetry.context.aiocontextvarsfix  # pylint:disable=wrong-import-position
-
-    opentelemetry.context.aiocontextvarsfix  # pylint:disable=pointless-statement
 
 
 class ContextVarsRuntimeContext(_RuntimeContext):
