818742c9822afe5a95f8cf6a9c1d0b2a8c275f27
Diego Hurtado <ocelotl@users.noreply.github.com>
2021-08-02 16:35:43 -0600

    Fix tests

--- opentelemetry-sdk/tests/metrics/test_prototype.py
+++ opentelemetry-sdk/tests/metrics/test_prototype.py
@@ -15,24 +15,10 @@
 
 from unittest import TestCase
 
-from opentelemetry.sdk.metrics.export import Exporter, Result
-
 from opentelemetry.sdk.metrics.meter import MeterProvider
 
 
 meter = MeterProvider().get_meter("meter")
-
-
-class TestExporter(Exporter):
-
-    def __init__(self, records):
-        self.records = records
-
-    def export(self, records):
-
-        self.records.update(records)
-
-        return Result.SUCCESS
 
 
 class Store:
@@ -57,6 +43,11 @@ class Store:
             unit="items",
             description="items sold to a store customer"
         )
+        self.customers_in_store = meter.create_up_down_counter(
+            "customers_in_store",
+            unit="customers",
+            description="customers currently in the store"
+        )
 
     def process_order(self, customer, potatoes=0, tomatoes=0):
 
@@ -75,6 +66,16 @@ class Store:
                 tomatoes, store=self.name, customer=customer, item="tomato"
             )
 
+    def enter_customer(self, account_type):
+        self.customers_in_store.add(
+            1, store=self.name, account_type=account_type
+        )
+
+    def exit_customer(self, account_type):
+        self.customers_in_store.add(
+            -1, store=self.name, account_type=account_type
+        )
+
 
 store = Store("store")
 
@@ -82,17 +83,111 @@ store = Store("store")
 class TestPrototype(TestCase):
     def test_prototype(self):
         self.maxDiff = None
+
+        store.enter_customer("restaurant")
+        store.enter_customer("home_cook")
+        store.enter_customer("home_cook")
+
         store.process_order("customer0", potatoes=2, tomatoes=3)
         store.process_order("customer1", tomatoes=10)
         store.process_order("customer2", potatoes=2)
-        store.process_order("customer0", tomatoes=1)
-
-        records = {}
-        TestExporter(records).export(meter.get_records())
 
         self.assertEqual(
-            records,
+            meter.get_records(),
             {
+                "UpDownCounter": {
+                    "customers_in_store": {
+                        frozenset(
+                            {
+                                ('store', 'store'),
+                                ('account_type', 'restaurant')
+                            }
+                        ): 1,
+                        frozenset(
+                            {('store', 'store'), ('account_type', 'home_cook')}
+                        ): 2,
+                    }
+                },
+                "Counter": {
+                    "number_of_orders": {
+                        frozenset(
+                            {('store', 'store'), ('customer', 'customer0')}
+                        ): 1,
+                        frozenset(
+                            {('store', 'store'), ('customer', 'customer1')}
+                        ): 1,
+                        frozenset(
+                            {('store', 'store'), ('customer', 'customer2')}
+                        ): 1,
+                    },
+                    "amount": {
+                        frozenset(
+                            {('store', 'store'), ('customer', 'customer0')}
+                        ): 11,
+                        frozenset(
+                            {('customer', 'customer1'), ('store', 'store')}
+                        ): 30,
+                        frozenset(
+                            {('store', 'store'), ('customer', 'customer2')}
+                        ): 2,
+                    },
+                    "items_sold": {
+                        frozenset(
+                            {
+                                ('item', 'potato'),
+                                ('store', 'store'),
+                                ('customer', 'customer0')
+                            }
+                        ): 2,
+                        frozenset(
+                            {
+                                ('item', 'tomato'),
+                                ('store', 'store'),
+                                ('customer', 'customer0')
+                            }
+                        ): 3,
+                        frozenset(
+                            {
+                                ('customer', 'customer1'),
+                                ('item', 'tomato'),
+                                ('store', 'store')
+                            }
+                        ): 10,
+                        frozenset(
+                            {
+                                ('item', 'potato'),
+                                ('store', 'store'),
+                                ('customer', 'customer2')
+                            }
+                        ): 2,
+                    }
+                }
+            }
+        )
+
+        store.exit_customer("restaurant")
+        store.exit_customer("home_cook")
+        store.exit_customer("home_cook")
+
+        store.enter_customer("restaurant")
+        store.process_order("customer0", tomatoes=1)
+
+        self.assertEqual(
+            meter.get_records(),
+            {
+                "UpDownCounter": {
+                    "customers_in_store": {
+                        frozenset(
+                            {
+                                ('store', 'store'),
+                                ('account_type', 'restaurant')
+                            }
+                        ): 1,
+                        frozenset(
+                            {('store', 'store'), ('account_type', 'home_cook')}
+                        ): 0,
+                    }
+                },
                 "Counter": {
                     "number_of_orders": {
                         frozenset(
