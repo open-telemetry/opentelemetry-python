e82afea04d7c871bcb1de29166bf79c17710af32
Diego Hurtado <ocelotl@users.noreply.github.com>
2021-08-25 18:57:59 +0200

    Another fix

--- opentelemetry-distro/tests/test_distro.py
+++ opentelemetry-distro/tests/test_distro.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # type: ignore
-# pylint: disable=no-name-in-module
 
 import os
 from unittest import TestCase
