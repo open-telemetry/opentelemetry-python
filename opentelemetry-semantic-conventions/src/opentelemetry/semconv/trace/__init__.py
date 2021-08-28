cc7f79b0fb33426e33a491b71d4ad38a91703e08
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2021-08-28 06:14:40 +0530

    Update to semantic conventions v1.6.1

--- opentelemetry-semantic-conventions/src/opentelemetry/semconv/trace/__init__.py
+++ opentelemetry-semantic-conventions/src/opentelemetry/semconv/trace/__init__.py
@@ -314,6 +314,36 @@ clear whether the exception will escape.
     Local hostname or similar, see note below.
     """
 
+    NET_HOST_CONNECTION_TYPE = "net.host.connection.type"
+    """
+    The internet connection type currently being used by the host.
+    """
+
+    NET_HOST_CONNECTION_SUBTYPE = "net.host.connection.subtype"
+    """
+    This describes more details regarding the connection.type. It may be the type of cell technology connection, but it could be used for describing details about a wifi connection.
+    """
+
+    NET_HOST_CARRIER_NAME = "net.host.carrier.name"
+    """
+    The name of the mobile carrier.
+    """
+
+    NET_HOST_CARRIER_MCC = "net.host.carrier.mcc"
+    """
+    The mobile carrier country code.
+    """
+
+    NET_HOST_CARRIER_MNC = "net.host.carrier.mnc"
+    """
+    The mobile carrier network code.
+    """
+
+    NET_HOST_CARRIER_ICC = "net.host.carrier.icc"
+    """
+    The ISO 3166-1 alpha-2 2-character country code associated with the mobile carrier network.
+    """
+
     MESSAGING_SYSTEM = "messaging.system"
     """
     A string identifying the messaging system.
@@ -608,7 +638,7 @@ clear whether the exception will escape.
     MESSAGING_KAFKA_MESSAGE_KEY = "messaging.kafka.message_key"
     """
     Message keys in Kafka are used for grouping alike messages to ensure they're processed on the same partition. They differ from `messaging.message_id` in that they're not unique. If the key is `null`, the attribute MUST NOT be set.
-    Note: If the key type is not string, its string representation has to be supplied for the attribute. If the key has no unambiguous, canonical string form, don't include its value.
+    Note: If the key type is not string, it's string representation has to be supplied for the attribute. If the key has no unambiguous, canonical string form, don't include its value.
     """
 
     MESSAGING_KAFKA_CONSUMER_GROUP = "messaging.kafka.consumer_group"
@@ -903,6 +933,88 @@ class HttpFlavorValues(Enum):
     """QUIC protocol."""
 
 
+class NetHostConnectionTypeValues(Enum):
+    WIFI = "wifi"
+    """wifi."""
+
+    WIRED = "wired"
+    """wired."""
+
+    CELL = "cell"
+    """cell."""
+
+    UNAVAILABLE = "unavailable"
+    """unavailable."""
+
+    UNKNOWN = "unknown"
+    """unknown."""
+
+
+class NetHostConnectionSubtypeValues(Enum):
+    GPRS = "gprs"
+    """GPRS."""
+
+    EDGE = "edge"
+    """EDGE."""
+
+    UMTS = "umts"
+    """UMTS."""
+
+    CDMA = "cdma"
+    """CDMA."""
+
+    EVDO_0 = "evdo_0"
+    """EVDO Rel. 0."""
+
+    EVDO_A = "evdo_a"
+    """EVDO Rev. A."""
+
+    CDMA2000_1XRTT = "cdma2000_1xrtt"
+    """CDMA2000 1XRTT."""
+
+    HSDPA = "hsdpa"
+    """HSDPA."""
+
+    HSUPA = "hsupa"
+    """HSUPA."""
+
+    HSPA = "hspa"
+    """HSPA."""
+
+    IDEN = "iden"
+    """IDEN."""
+
+    EVDO_B = "evdo_b"
+    """EVDO Rev. B."""
+
+    LTE = "lte"
+    """LTE."""
+
+    EHRPD = "ehrpd"
+    """EHRPD."""
+
+    HSPAP = "hspap"
+    """HSPAP."""
+
+    GSM = "gsm"
+    """GSM."""
+
+    TD_SCDMA = "td_scdma"
+    """TD-SCDMA."""
+
+    IWLAN = "iwlan"
+    """IWLAN."""
+
+    NR = "nr"
+    """5G NR (New Radio)."""
+
+    NRNSA = "nrnsa"
+    """5G NRNSA (New Radio Non-Standalone)."""
+
+    LTE_CA = "lte_ca"
+    """LTE CA."""
+
+
 class MessagingDestinationKindValues(Enum):
     QUEUE = "queue"
     """A message sent to a queue."""
@@ -912,6 +1024,9 @@ class MessagingDestinationKindValues(Enum):
 
 
 class FaasInvokedProviderValues(Enum):
+    ALIBABA_CLOUD = "alibaba_cloud"
+    """Alibaba Cloud."""
+
     AWS = "aws"
     """Amazon Web Services."""
 
