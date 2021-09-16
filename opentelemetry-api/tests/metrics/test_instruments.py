f02f8cfe002fe7d1822e75ebb1633e634066f238
Diego Hurtado <ocelotl@users.noreply.github.com>
2021-09-16 14:21:15 +0200

    Fix fixmes

--- opentelemetry-api/tests/metrics/test_instruments.py
+++ opentelemetry-api/tests/metrics/test_instruments.py
@@ -33,6 +33,8 @@ from opentelemetry.metrics.instrument import (
 # FIXME Test that the instrument methods can be called concurrently safely.
 
 
+# FIXME Test that the instrument methods can be called concurrently safely.
+
 class ChildInstrument(Instrument):
     def __init__(self, name, *args, unit="", description="", **kwargs):
         super().__init__(
