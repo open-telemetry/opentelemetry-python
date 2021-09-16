a0bd78185891eeac5982eb3ff3b1c31ca3e1dbf2
Alex Boten <aboten@lightstep.com>
2021-09-16 15:38:20 -0700

    use timeout in force_flush

--- opentelemetry-sdk/src/opentelemetry/sdk/logs/export/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/logs/export/__init__.py
@@ -266,7 +266,7 @@ class BatchLogProcessor(LogProcessor):
             flush_request = self._get_or_create_flush_request()
             self._condition.notify_all()
 
-        ret = flush_request.event.wait()
+        ret = flush_request.event.wait(flush_request.event.wait(timeout_millis/1e3))
         if not ret:
             _logger.warning("Timeout was exceeded in force_flush().")
         return ret
