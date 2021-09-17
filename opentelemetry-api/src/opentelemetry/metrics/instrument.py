216aba55917bf134eb196337fa1d7ed48ad26dd9
Diego Hurtado <ocelotl@users.noreply.github.com>
2021-09-17 15:48:07 +0200

    Remove blank line as requested.

--- opentelemetry-api/src/opentelemetry/metrics/instrument.py
+++ opentelemetry-api/src/opentelemetry/metrics/instrument.py
@@ -153,7 +153,6 @@ class ObservableCounter(_Monotonic, Asynchronous):
 
     def observe(self):
         with self._lock:
-
             measurement = super().observe()
 
             if isinstance(measurement, Measurement):
