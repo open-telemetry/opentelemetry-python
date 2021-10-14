c39faeae83393343b64f468e6c1e949ace7a73de
Owais Lone <owais@users.noreply.github.com>
2021-10-14 06:25:44 +0530

    Move BaseConfigurator to SDK (#2188)
    * Move _SUPPRESS_INTRUMENTATION key from instrumentation to api
    
    This will continue to be part of the internal API but live in the API
    context package.
    
    * Move BaseConfigurator from isntrumentation to SDK
    
    This allows SDK to not depend on instrumentation package anymore.

--- opentelemetry-sdk/src/opentelemetry/sdk/trace/export/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/trace/export/__init__.py
@@ -21,8 +21,13 @@ from enum import Enum
 from os import environ, linesep
 from typing import Optional
 
-from opentelemetry.context import Context, attach, detach, set_value
-from opentelemetry.instrumentation.utils import _SUPPRESS_INSTRUMENTATION_KEY
+from opentelemetry.context import (
+    _SUPPRESS_INSTRUMENTATION_KEY,
+    Context,
+    attach,
+    detach,
+    set_value,
+)
 from opentelemetry.sdk.environment_variables import (
     OTEL_BSP_EXPORT_TIMEOUT,
     OTEL_BSP_MAX_EXPORT_BATCH_SIZE,
