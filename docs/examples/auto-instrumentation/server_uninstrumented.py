dd0ff73aafe0baf0c46d625c21dbbd65e0ab778b
Leighton Chen <lechen@microsoft.com>
2021-10-27 16:11:04 -0700

    comment

--- docs/examples/auto-instrumentation/server_uninstrumented.py
+++ docs/examples/auto-instrumentation/server_uninstrumented.py
@@ -14,21 +14,7 @@
 
 from flask import Flask, request
 
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    ConsoleSpanExporter,
-)
-
 app = Flask(__name__)
-
-trace.set_tracer_provider(TracerProvider())
-
-trace.get_tracer_provider().add_span_processor(
-    BatchSpanProcessor(ConsoleSpanExporter())
-)
-
 
 @app.route("/server_request")
 def server_request():
