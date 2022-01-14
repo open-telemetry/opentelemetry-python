9d5c1e15c8f18605e75616fd06f3b0b1441d6653
Alex Boten <aboten@lightstep.com>
2022-01-14 09:42:31 -0800

    fix lint

--- docs/examples/metrics/example.py
+++ docs/examples/metrics/example.py
@@ -1,9 +1,8 @@
-from opentelemetry._metrics import set_meter_provider, get_meter_provider
+from opentelemetry._metrics import get_meter_provider, set_meter_provider
 from opentelemetry.exporter.otlp.proto.grpc._metric_exporter import (
     OTLPMetricExporter,
 )
 from opentelemetry.sdk._metrics import MeterProvider
-
 
 provider = MeterProvider()
 exporter = OTLPMetricExporter(insecure=True)
