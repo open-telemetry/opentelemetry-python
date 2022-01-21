83e6e17a4756698084895f07556d2043b4b0e7e8
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-01-21 22:12:53 +0530

    Add periodic exporting metric reader (#2340)
    * Add periodic exporting metric reader
    
    * Fix lint
    
    * Fix typing and env
    
    * lint: black format
    
    * formatting
    
    * lot of changes
    
    * Fix lint
    
    * Lint again
    
    * Pylint fix
    
    * Review suggestions
    
    * fix failures
    
    * Address review suggestions
    
    * Once wrapper, set callback, refactor, and more
    
    * Apply suggestions from code review
    
    Co-authored-by: Aaron Abbott <aaronabbott@google.com>
    
    * Add preferred_temporality to exporter; update doc string
    
    * Fix lint
    
    Co-authored-by: Aaron Abbott <aaronabbott@google.com>

--- opentelemetry-sdk/src/opentelemetry/sdk/_metrics/export/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/_metrics/export/__init__.py
@@ -12,13 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
+import os
 from abc import ABC, abstractmethod
 from enum import Enum
-from os import linesep
+from os import environ, linesep
 from sys import stdout
-from typing import IO, Callable, Sequence
+from threading import Event, Thread
+from typing import IO, Callable, Iterable, Optional, Sequence
 
-from opentelemetry.sdk._metrics.point import Metric
+from opentelemetry.context import (
+    _SUPPRESS_INSTRUMENTATION_KEY,
+    attach,
+    detach,
+    set_value,
+)
+from opentelemetry.sdk._metrics.metric_reader import MetricReader
+from opentelemetry.sdk._metrics.point import AggregationTemporality, Metric
+from opentelemetry.util._once import Once
+
+_logger = logging.getLogger(__name__)
 
 
 class MetricExportResult(Enum):
@@ -32,6 +45,10 @@ class MetricExporter(ABC):
     Interface to be implemented by services that want to export metrics received
     in their own format.
     """
+
+    @property
+    def preferred_temporality(self) -> AggregationTemporality:
+        return AggregationTemporality.CUMULATIVE
 
     @abstractmethod
     def export(self, metrics: Sequence[Metric]) -> "MetricExportResult":
@@ -77,3 +94,82 @@ class ConsoleMetricExporter(MetricExporter):
 
     def shutdown(self) -> None:
         pass
+
+
+class PeriodicExportingMetricReader(MetricReader):
+    """`PeriodicExportingMetricReader` is an implementation of `MetricReader`
+    that collects metrics based on a user-configurable time interval, and passes the
+    metrics to the configured exporter.
+    """
+
+    def __init__(
+        self,
+        exporter: MetricExporter,
+        export_interval_millis: Optional[float] = None,
+        export_timeout_millis: Optional[float] = None,
+    ) -> None:
+        super().__init__(preferred_temporality=exporter.preferred_temporality)
+        self._exporter = exporter
+        if export_interval_millis is None:
+            try:
+                export_interval_millis = float(
+                    environ.get("OTEL_METRIC_EXPORT_INTERVAL", 60000)
+                )
+            except ValueError:
+                _logger.warning(
+                    "Found invalid value for export interval, using default"
+                )
+                export_interval_millis = 60000
+        if export_timeout_millis is None:
+            try:
+                export_timeout_millis = float(
+                    environ.get("OTEL_METRIC_EXPORT_TIMEOUT", 30000)
+                )
+            except ValueError:
+                _logger.warning(
+                    "Found invalid value for export timeout, using default"
+                )
+                export_timeout_millis = 30000
+        self._export_interval_millis = export_interval_millis
+        self._export_timeout_millis = export_timeout_millis
+        self._shutdown = False
+        self._shutdown_event = Event()
+        self._shutdown_once = Once()
+        self._daemon_thread = Thread(target=self._ticker, daemon=True)
+        self._daemon_thread.start()
+        if hasattr(os, "register_at_fork"):
+            os.register_at_fork(
+                after_in_child=self._at_fork_reinit
+            )  # pylint: disable=protected-access
+
+    def _at_fork_reinit(self):
+        self._daemon_thread = Thread(target=self._ticker, daemon=True)
+        self._daemon_thread.start()
+
+    def _ticker(self) -> None:
+        interval_secs = self._export_interval_millis / 1e3
+        while not self._shutdown_event.wait(interval_secs):
+            self.collect()
+        # one last collection below before shutting down completely
+        self.collect()
+
+    def _receive_metrics(self, metrics: Iterable[Metric]) -> None:
+        token = attach(set_value(_SUPPRESS_INSTRUMENTATION_KEY, True))
+        try:
+            self._exporter.export(metrics)
+        except Exception as e:  # pylint: disable=broad-except,invalid-name
+            _logger.exception("Exception while exporting metrics %s", str(e))
+        detach(token)
+
+    def shutdown(self) -> bool:
+        def _shutdown():
+            self._shutdown = True
+
+        did_set = self._shutdown_once.do_once(_shutdown)
+        if not did_set:
+            _logger.warning("Can't shutdown multiple times")
+            return False
+
+        self._shutdown_event.set()
+        self._daemon_thread.join()
+        return self._exporter.shutdown()
