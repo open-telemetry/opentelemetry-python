594083c68a881223e9927dbbfd101bac7c1f06ea
Alex Boten <aboten@lightstep.com>
2022-03-07 11:53:34 -0800

    [docs] updating metrics examples
    Adding different instruments to the example code.

--- docs/getting_started/metrics_example.py
+++ docs/getting_started/metrics_example.py
@@ -21,13 +21,50 @@ from opentelemetry.sdk._metrics.export import (
     ConsoleMetricExporter,
     PeriodicExportingMetricReader,
 )
+from opentelemetry.sdk._metrics.measurement import Measurement
 
 exporter = ConsoleMetricExporter()
 reader = PeriodicExportingMetricReader(exporter)
 provider = MeterProvider(metric_readers=[reader])
 set_meter_provider(provider)
 
+
+def observable_counter_func() -> Measurement:
+    yield Measurement(1, {})
+
+
+def observable_up_down_counter_func() -> Measurement:
+    yield Measurement(-10, {})
+
+
+def observable_gauge_func() -> Measurement:
+    yield Measurement(9, {})
+
+
 meter = get_meter_provider().get_meter("getting-started", "0.1.2")
-counter = meter.create_counter("first_counter")
+
+# Counter
+counter = meter.create_counter("counter")
 counter.add(1)
-# TODO: fill in details for additional metrics
+
+# Async Counter
+observable_counter = meter.create_observable_counter(
+    "observable_counter", observable_counter_func
+)
+
+# UpDownCounter
+updown_counter = meter.create_up_down_counter("updown_counter")
+updown_counter.add(1)
+updown_counter.add(-5)
+
+# Async UpDownCounter
+observable_updown_counter = meter.create_observable_up_down_counter(
+    "observable_updown_counter", observable_up_down_counter_func
+)
+
+# Histogram
+histogram = meter.create_histogram("histogram")
+histogram.record(99.9)
+
+# Async Gauge
+gauge = meter.create_observable_gauge("gauge", observable_gauge_func)
