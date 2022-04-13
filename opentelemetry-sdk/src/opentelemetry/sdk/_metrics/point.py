e41fdbf458c6805521acf85741fb3c5f993e84a5
Leighton Chen <lechen@microsoft.com>
2022-04-13 08:50:18 -0700

    Include min/max information in Histogram point (#2581)
    * hist
    
    * changelog
    
    * tests
    
    * agg
    
    Co-authored-by: Diego Hurtado <ocelotl@users.noreply.github.com>

--- opentelemetry-sdk/src/opentelemetry/sdk/_metrics/point.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/_metrics/point.py
@@ -30,11 +30,11 @@ class AggregationTemporality(IntEnum):
 
 @dataclass(frozen=True)
 class Sum:
+    aggregation_temporality: AggregationTemporality
+    is_monotonic: bool
     start_time_unix_nano: int
     time_unix_nano: int
     value: Union[int, float]
-    aggregation_temporality: AggregationTemporality
-    is_monotonic: bool
 
 
 @dataclass(frozen=True)
@@ -45,12 +45,14 @@ class Gauge:
 
 @dataclass(frozen=True)
 class Histogram:
-    start_time_unix_nano: int
-    time_unix_nano: int
+    aggregation_temporality: AggregationTemporality
     bucket_counts: Sequence[int]
     explicit_bounds: Sequence[float]
+    max: int
+    min: int
+    start_time_unix_nano: int
     sum: Union[int, float]
-    aggregation_temporality: AggregationTemporality
+    time_unix_nano: int
 
 
 PointT = Union[Sum, Gauge, Histogram]
