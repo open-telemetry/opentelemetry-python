dd8b7c010a46ed38f4ec959dae631b081b602038
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-04-27 15:03:45 -0700

    Add prepare-release-branch workflow

---
+++ .github/scripts/set-git-user.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+# TODO update with opentelemetry-python-bot or a shared otel bot
+git config user.name opentelemetry-java-bot
+git config user.email 97938252+opentelemetry-java-bot@users.noreply.github.com
