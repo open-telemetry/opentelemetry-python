18fbb39db40c802921d2bf2f3c77f8440ea5e021
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-06-03 12:25:15 +0530

    lint

--- opentelemetry-sdk/tests/test_configurator.py
+++ opentelemetry-sdk/tests/test_configurator.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # type: ignore
+# pylint: skip-file
 
 import logging
 from os import environ
