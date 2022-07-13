619be776ca57526ba26517df84e4ee5c960bad73
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-07-13 22:33:56 +0530

    Add License and remove obivious comment

--- docs/examples/views/limit_num_of_attrs.py
+++ docs/examples/views/limit_num_of_attrs.py
@@ -1,3 +1,17 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import random
 import time
 from typing import Iterable
@@ -24,10 +38,8 @@ view_with_attributes_limit = View(
     attribute_keys={"k_3", "k_5"},
 )
 
-# Use console exporter for the example
 exporter = ConsoleMetricExporter()
 
-# Create a metric reader with stdout exporter
 reader = PeriodicExportingMetricReader(exporter, export_interval_millis=1_000)
 provider = MeterProvider(
     metric_readers=[
