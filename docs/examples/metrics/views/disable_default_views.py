46480e23a61e1f8adf0377b632c4276a3b5f4ce9
nanfuka <kalungi2k6@gmail.com>
2022-10-23 20:53:57 +0300

    Add disable default views to common view scenarios

---
+++ docs/examples/metrics/views/disable_default_views.py
@@ -0,0 +1,49 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+import time
+
+from opentelemetry.metrics import get_meter_provider, set_meter_provider
+from opentelemetry.sdk.metrics import Counter, MeterProvider
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
+from opentelemetry.sdk.metrics.view import DropAggregation, View
+
+# disable default views 
+# Create a view matching the counter instrument `mycounter`
+disable_default_views= View(instrument_name="*", aggregation=DropAggregation())
+
+exporter = ConsoleMetricExporter()
+
+reader = PeriodicExportingMetricReader(exporter, export_interval_millis=1_000)
+provider = MeterProvider(
+    metric_readers=[
+        reader,
+    ],
+    views=[
+        disable_default_views,
+    ],
+)
+set_meter_provider(provider)
+
+meter = get_meter_provider().get_meter("view-disable-default-views", "0.1.2")
+
+my_counter = meter.create_counter("mycounter")
+
+while 1:
+    my_counter.add(random.randint(1, 10))
+    time.sleep(random.random())
