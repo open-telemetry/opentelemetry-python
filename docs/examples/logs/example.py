b4c3c0cbedd3c0100aaa5fe40804288b65ed7583
Hui Jiang <=>
2022-12-20 17:37:47 -0500

    Fixes #3088 - updated import library for set_logger_provider

--- docs/examples/logs/example.py
+++ docs/examples/logs/example.py
@@ -1,13 +1,13 @@
 import logging
 
 from opentelemetry import trace
+from opentelemetry._logs import set_logger_provider
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
     OTLPLogExporter,
-)
+)`
 from opentelemetry.sdk._logs import (
     LoggerProvider,
     LoggingHandler,
-    set_logger_provider,
 )
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
