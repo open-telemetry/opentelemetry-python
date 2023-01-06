71e0fae3d3be19463e282ef26869e6aad72c4cb9
hannah-jj <hui1021@gmail.com>
2023-01-05 22:23:55 -0500

    Updated import format for LoggerProvider & LoggingHandler to conform to standards on isort tests.

--- docs/examples/logs/example.py
+++ docs/examples/logs/example.py
@@ -5,10 +5,7 @@ from opentelemetry._logs import set_logger_provider
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
     OTLPLogExporter,
 )
-from opentelemetry.sdk._logs import (
-    LoggerProvider,
-    LoggingHandler,
-)
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
