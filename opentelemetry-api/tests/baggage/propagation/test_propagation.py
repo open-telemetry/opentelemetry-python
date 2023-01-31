50a14942e071900a08254dcd22cc60362e98b6f8
Mark Jan van Kampen <m.vankampen@freeday.ai>
2023-01-31 12:59:33 +0100

    Fixes formatting

--- opentelemetry-api/tests/baggage/propagation/test_propagation.py
+++ opentelemetry-api/tests/baggage/propagation/test_propagation.py
@@ -12,12 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from opentelemetry.baggage import (
-    get_baggage,
-    set_baggage,
-)
-from opentelemetry.baggage.propagation import W3CBaggagePropagator
 from unittest import TestCase
+
+from opentelemetry.baggage import get_baggage, set_baggage
+from opentelemetry.baggage.propagation import W3CBaggagePropagator
 
 
 class TestBaggageManager(TestCase):
@@ -25,9 +23,15 @@ class TestBaggageManager(TestCase):
         carrier = {}
         propagator = W3CBaggagePropagator()
 
-        ctx = set_baggage('Test1', 'value1')
-        ctx = set_baggage('test2', 'value2', context=ctx)
+        ctx = set_baggage("Test1", "value1")
+        ctx = set_baggage("test2", "value2", context=ctx)
+
         propagator.inject(carrier, ctx)
         ctx_propagated = propagator.extract(carrier)
-        self.assertEqual(get_baggage("Test1", context=ctx_propagated), "value1")
-        self.assertEqual(get_baggage("test2", context=ctx_propagated), "value2")
+
+        self.assertEqual(
+            get_baggage("Test1", context=ctx_propagated), "value1"
+        )
+        self.assertEqual(
+            get_baggage("test2", context=ctx_propagated), "value2"
+        )
