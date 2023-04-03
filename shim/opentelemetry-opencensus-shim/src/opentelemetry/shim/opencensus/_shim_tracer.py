17aa1e862c450d27b3c138859cd08b1ec6deaa7d
Aaron Abbott <aaronabbott@google.com>
2023-04-03 16:22:41 -0400

    Update OpenCensus shim to update OpenCensus execution context (#3231)
    * Update OpenCensus shim to update OpenCensus execution context
    
    Previously, the shim was only writing to OTel context. This updates it to write to both OC and OTel.
    
    Also updated the code to use the `span_context` provided when instrumentation instantiates the `Tracer`.

--- shim/opentelemetry-opencensus-shim/src/opentelemetry/shim/opencensus/_shim_tracer.py
+++ shim/opentelemetry-opencensus-shim/src/opentelemetry/shim/opencensus/_shim_tracer.py
@@ -15,8 +15,11 @@
 import logging
 
 import wrapt
+from opencensus.trace import execution_context
 from opencensus.trace.blank_span import BlankSpan
+from opencensus.trace.span_context import SpanContext
 from opencensus.trace.tracers.base import Tracer as BaseTracer
+from opencensus.trace.tracestate import Tracestate
 
 from opentelemetry import context, trace
 from opentelemetry.shim.opencensus._shim_span import ShimSpan
@@ -24,6 +27,7 @@ from opentelemetry.shim.opencensus._shim_span import ShimSpan
 _logger = logging.getLogger(__name__)
 
 _SHIM_SPAN_KEY = context.create_key("opencensus-shim-span-key")
+_SAMPLED = trace.TraceFlags(trace.TraceFlags.SAMPLED)
 
 
 def set_shim_span_in_context(
@@ -36,12 +40,57 @@ def get_shim_span_in_context() -> ShimSpan:
     return context.get_value(_SHIM_SPAN_KEY)
 
 
+def set_oc_span_in_context(
+    oc_span_context: SpanContext, ctx: context.Context
+) -> context.Context:
+    """Returns a new OTel context based on ctx with oc_span_context set as the current span"""
+
+    # If no SpanContext is passed to the opencensus.trace.tracer.Tracer, it creates a new one
+    # with a random trace ID and a None span ID to be the parent:
+    # https://github.com/census-instrumentation/opencensus-python/blob/2e08df591b507612b3968be8c2538dedbf8fab37/opencensus/trace/tracer.py#L47.
+    #
+    # OpenTelemetry considers this an invalid SpanContext and will ignore it, so we can just
+    # return early
+    if oc_span_context.span_id is None:
+        return ctx
+
+    trace_id = int(oc_span_context.trace_id, 16)
+    span_id = int(oc_span_context.span_id, 16)
+    is_remote = oc_span_context.from_header
+    trace_flags = (
+        _SAMPLED if oc_span_context.trace_options.get_enabled() else None
+    )
+    trace_state = (
+        trace.TraceState(tuple(oc_span_context.tracestate.items()))
+        # OC SpanContext does not validate this type
+        if isinstance(oc_span_context.tracestate, Tracestate)
+        else None
+    )
+
+    return trace.set_span_in_context(
+        trace.NonRecordingSpan(
+            trace.SpanContext(
+                trace_id=trace_id,
+                span_id=span_id,
+                is_remote=is_remote,
+                trace_flags=trace_flags,
+                trace_state=trace_state,
+            )
+        )
+    )
+
+
 # pylint: disable=abstract-method
 class ShimTracer(wrapt.ObjectProxy):
     def __init__(
-        self, wrapped: BaseTracer, *, otel_tracer: trace.Tracer
+        self,
+        wrapped: BaseTracer,
+        *,
+        oc_span_context: SpanContext,
+        otel_tracer: trace.Tracer
     ) -> None:
         super().__init__(wrapped)
+        self._self_oc_span_context = oc_span_context
         self._self_otel_tracer = otel_tracer
 
     # For now, finish() is not implemented by the shim. It would require keeping a list of all
@@ -53,7 +102,15 @@ class ShimTracer(wrapt.ObjectProxy):
         return self.start_span(name=name)
 
     def start_span(self, name="span"):
-        span = self._self_otel_tracer.start_span(name)
+        parent_ctx = context.get_current()
+        # If there is no current span in context, use the one provided to the OC Tracer at
+        # creation time
+        if trace.get_current_span(parent_ctx) is trace.INVALID_SPAN:
+            parent_ctx = set_oc_span_in_context(
+                self._self_oc_span_context, parent_ctx
+            )
+
+        span = self._self_otel_tracer.start_span(name, context=parent_ctx)
         shim_span = ShimSpan(
             BlankSpan(name=name, context_tracer=self),
             otel_span=span,
@@ -67,11 +124,14 @@ class ShimTracer(wrapt.ObjectProxy):
         # equivalent to the below. This can cause context to leak but is equivalent.
         # pylint: disable=protected-access
         shim_span._self_token = context.attach(ctx)
+        # Also set it in OC's context, equivalent to
+        # https://github.com/census-instrumentation/opencensus-python/blob/2e08df591b507612b3968be8c2538dedbf8fab37/opencensus/trace/tracers/context_tracer.py#L94
+        execution_context.set_current_span(shim_span)
         return shim_span
 
     def end_span(self):
-        """Finishes the current span in the context and pops restores the context from before
-        the span was started.
+        """Finishes the current span in the context and restores the context from before the
+        span was started.
         """
         span = self.current_span()
         if not span:
@@ -79,8 +139,12 @@ class ShimTracer(wrapt.ObjectProxy):
             return
 
         span.finish()
+
         # pylint: disable=protected-access
         context.detach(span._self_token)
+        # Also reset the OC execution_context, equivalent to
+        # https://github.com/census-instrumentation/opencensus-python/blob/2e08df591b507612b3968be8c2538dedbf8fab37/opencensus/trace/tracers/context_tracer.py#L114-L117
+        execution_context.set_current_span(self.current_span())
 
     # pylint: disable=no-self-use
     def current_span(self):
