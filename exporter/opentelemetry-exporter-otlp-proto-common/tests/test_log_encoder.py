3228f14dd609d136e7f1e6ef2d1a75e44844a8b4
Nina Stawski <nstawski@splunk.com>
2023-06-12 16:11:51 -0700

    Lint

--- exporter/opentelemetry-exporter-otlp-proto-common/tests/test_log_encoder.py
+++ exporter/opentelemetry-exporter-otlp-proto-common/tests/test_log_encoder.py
@@ -39,9 +39,8 @@ from opentelemetry.proto.logs.v1.logs_pb2 import ScopeLogs as PB2ScopeLogs
 from opentelemetry.proto.resource.v1.resource_pb2 import (
     Resource as PB2Resource,
 )
-from opentelemetry.sdk._logs import LogData
+from opentelemetry.sdk._logs import LogData, LogLimits
 from opentelemetry.sdk._logs import LogRecord as SDKLogRecord
-from opentelemetry.sdk._logs import LogLimits
 from opentelemetry.sdk.resources import Resource as SDKResource
 from opentelemetry.sdk.util.instrumentation import InstrumentationScope
 from opentelemetry.trace import TraceFlags
