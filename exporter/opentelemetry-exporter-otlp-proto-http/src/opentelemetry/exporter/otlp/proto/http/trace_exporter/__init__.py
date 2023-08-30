91213823fa42b9cd7a83f38bf023371ce57f2f99
Pablo Collins <pablo.collins@gmail.com>
2023-08-30 10:28:38 -0400

    Factor out duplicate backoff code (#3396)
    Co-authored-by: Diego Hurtado <ocelotl@users.noreply.github.com>

--- exporter/opentelemetry-exporter-otlp-proto-http/src/opentelemetry/exporter/otlp/proto/http/trace_exporter/__init__.py
+++ exporter/opentelemetry-exporter-otlp-proto-http/src/opentelemetry/exporter/otlp/proto/http/trace_exporter/__init__.py
@@ -20,9 +20,11 @@ from os import environ
 from typing import Dict, Optional
 from time import sleep
 
-import backoff
 import requests
 
+from opentelemetry.exporter.otlp.proto.common._internal import (
+    _create_exp_backoff_generator,
+)
 from opentelemetry.exporter.otlp.proto.common.trace_encoder import (
     encode_spans,
 )
@@ -53,18 +55,6 @@ DEFAULT_COMPRESSION = Compression.NoCompression
 DEFAULT_ENDPOINT = "http://localhost:4318/"
 DEFAULT_TRACES_EXPORT_PATH = "v1/traces"
 DEFAULT_TIMEOUT = 10  # in seconds
-
-# Work around API change between backoff 1.x and 2.x. Since 2.0.0 the backoff
-# wait generator API requires a first .send(None) before reading the backoff
-# values from the generator.
-_is_backoff_v2 = next(backoff.expo()) is None
-
-
-def _expo(*args, **kwargs):
-    gen = backoff.expo(*args, **kwargs)
-    if _is_backoff_v2:
-        gen.send(None)
-    return gen
 
 
 class OTLPSpanExporter(SpanExporter):
@@ -145,7 +135,9 @@ class OTLPSpanExporter(SpanExporter):
 
         serialized_data = encode_spans(spans).SerializeToString()
 
-        for delay in _expo(max_value=self._MAX_RETRY_TIMEOUT):
+        for delay in _create_exp_backoff_generator(
+            max_value=self._MAX_RETRY_TIMEOUT
+        ):
 
             if delay == self._MAX_RETRY_TIMEOUT:
                 return SpanExportResult.FAILURE
