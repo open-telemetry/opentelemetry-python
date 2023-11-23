1625b35ab55782f1781dd189f5742efab5fdbf8a
Diego Hurtado <ocelotl@users.noreply.github.com>
2023-11-23 17:11:12 -0600

    Remove Jaeger exporter (#3554)

--- opentelemetry-api/src/opentelemetry/_logs/__init__.py
+++ opentelemetry-api/src/opentelemetry/_logs/__init__.py
@@ -14,8 +14,7 @@
 """
 The OpenTelemetry logging API describes the classes used to generate logs and events.
 
-The :class:`.LoggerProvider` provides users access to the :class:`.Logger` which in
-turn is used to create :class:`.Event` and :class:`.Log` objects.
+The :class:`.LoggerProvider` provides users access to the :class:`.Logger`.
 
 This module provides abstract (i.e. unimplemented) classes required for
 logging, and a concrete no-op implementation :class:`.NoOpLogger` that allows applications
