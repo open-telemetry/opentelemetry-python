4836815f751de1ed8bd47c56852e3ca4f1506626
Pablo Collins <pcollins@splunk.com>
2023-11-28 10:15:53 -0500

    revert eachdist change

--- scripts/eachdist.py
+++ scripts/eachdist.py
@@ -208,9 +208,8 @@ def parse_args(args=None):
         editable=True,
         with_dev_deps=True,
         eager_upgrades=True,
-        with_test_deps=False,
+        with_test_deps=True,
     )
-    devparser.add_argument("--with-test-deps", action="store_true")
 
     lintparser = subparsers.add_parser(
         "lint", help="Lint everything, autofixing if possible."
