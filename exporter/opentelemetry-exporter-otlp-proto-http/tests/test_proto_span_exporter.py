90f167d79acbab63f936f5e4e4cc3b68b96fd788
Pablo Collins <pcollins@splunk.com>
2024-02-08 16:46:45 -0500

    replace 'backoff' dependency with a simple implementation

--- exporter/opentelemetry-exporter-otlp-proto-http/tests/test_proto_span_exporter.py
+++ exporter/opentelemetry-exporter-otlp-proto-http/tests/test_proto_span_exporter.py
@@ -14,12 +14,11 @@
 
 import unittest
 from collections import OrderedDict
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import MagicMock, Mock, patch, call
 
 import requests
 import responses
 
-from opentelemetry.exporter.otlp.proto.common._internal import _is_backoff_v2
 from opentelemetry.exporter.otlp.proto.http import Compression
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
     DEFAULT_COMPRESSION,
@@ -205,17 +204,8 @@ class TestOTLPSpanExporter(unittest.TestCase):
 
     # pylint: disable=no-self-use
     @responses.activate
-    @patch("opentelemetry.exporter.otlp.proto.common._internal.backoff")
     @patch("opentelemetry.exporter.otlp.proto.http.trace_exporter.sleep")
-    def test_handles_backoff_v2_api(self, mock_sleep, mock_backoff):
-        # In backoff ~= 2.0.0 the first value yielded from expo is None.
-        def generate_delays(*args, **kwargs):
-            if _is_backoff_v2:
-                yield None
-            yield 1
-
-        mock_backoff.expo.configure_mock(**{"side_effect": generate_delays})
-
+    def test_exponential_backoff(self, mock_sleep):
         # return a retryable error
         responses.add(
             responses.POST,
@@ -239,7 +229,7 @@ class TestOTLPSpanExporter(unittest.TestCase):
         )
 
         exporter.export([span])
-        mock_sleep.assert_called_once_with(1)
+        mock_sleep.assert_has_calls([call(1), call(2), call(4), call(8), call(16), call(32)])
 
     @patch.object(OTLPSpanExporter, "_export", return_value=Mock(ok=True))
     def test_2xx_status_code(self, mock_otlp_metric_exporter):
