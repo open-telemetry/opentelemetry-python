89c62bde74664d53f94a04d38d3fc3da9bd80ea4
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-02-12 17:41:20 -0600

    Add missing license

--- exporter/opentelemetry-exporter-otlp-proto-common/tests/test_backoff.py
+++ exporter/opentelemetry-exporter-otlp-proto-common/tests/test_backoff.py
@@ -1,11 +1,24 @@
-import unittest
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+from unittest import TestCase
 from opentelemetry.exporter.otlp.proto.common._internal import (
     _create_exp_backoff_generator,
 )
 
 
-class TestBackoffGenerator(unittest.TestCase):
+class TestBackoffGenerator(TestCase):
     def test_exp_backoff_generator(self):
         generator = _create_exp_backoff_generator()
         self.assertEqual(next(generator), 1)
