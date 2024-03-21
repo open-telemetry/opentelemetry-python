59e7ce686295db4046970758822407e939c48422
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-03-20 18:11:57 -0600

    Refactor

--- sbom_generator.py
+++ sbom_generator.py
@@ -10,15 +10,21 @@ if sbom_access_token is None:
     raise Exception("SBOM access token is not set")
 
 headers = {
-    'Authorization': f'Bearer {sbom_access_token}'
+    'Authorization': f'Bearer {sbom_access_token}',
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28"
 }
 
 repo_owner = 'ocelotl'
 repo_name = 'opentelemetry-python'
+
 base_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}'
+base_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/dependency-graph/sbom"
+base_url = "https://api.github.com/repos/open-telemetry/opentelemetry-python/dependency-graph/sbom"
 
 contents_url = f'{base_url}/contents'
-result_list = get(contents_url, headers=headers).json()
+# result_list = get(contents_url, headers=headers).json()
+result_list = get(base_url, headers=headers).json()
 
 result_str = dumps(result_list, indent=4)
 
