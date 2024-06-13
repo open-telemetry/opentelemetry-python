3f9f50b075d06f1e6e72839f214d0b6ce8e5f1f5
Emídio Neto <9735060+emdneto@users.noreply.github.com>
2024-06-13 18:21:10 -0300

    Log a warning when an attribute is discarded due to limits (#3946)

--- opentelemetry-sdk/tests/logs/test_log_record.py
+++ opentelemetry-sdk/tests/logs/test_log_record.py
@@ -14,9 +14,14 @@
 
 import json
 import unittest
+import warnings
 
 from opentelemetry.attributes import BoundedAttributes
-from opentelemetry.sdk._logs import LogLimits, LogRecord
+from opentelemetry.sdk._logs import (
+    LogDroppedAttributesWarning,
+    LogLimits,
+    LogRecord,
+)
 
 
 class TestLogRecord(unittest.TestCase):
@@ -97,6 +102,28 @@ class TestLogRecord(unittest.TestCase):
         )
         self.assertTrue(result.dropped_attributes == 1)
         self.assertEqual(expected, result.attributes)
+
+    def test_log_record_dropped_attributes_set_limits_warning_once(self):
+        attr = {"key1": "value1", "key2": "value2"}
+        limits = LogLimits(
+            max_attributes=1,
+            max_attribute_length=1,
+        )
+
+        with warnings.catch_warnings(record=True) as cw:
+            for _ in range(10):
+                LogRecord(
+                    timestamp=0,
+                    body="a log line",
+                    attributes=attr,
+                    limits=limits,
+                )
+        self.assertEqual(len(cw), 1)
+        self.assertIsInstance(cw[-1].message, LogDroppedAttributesWarning)
+        self.assertIn(
+            "Log record attributes were dropped due to limits",
+            str(cw[-1].message),
+        )
 
     def test_log_record_dropped_attributes_unset_limits(self):
         attr = {"key": "value", "key2": "value2"}
