b3907dd7854c178a78a66f039842b76acf3bddb9
emdneto <9735060+emdneto@users.noreply.github.com>
2024-06-14 08:48:51 -0300

    fix json output for LogRecord

--- opentelemetry-sdk/tests/logs/test_log_record.py
+++ opentelemetry-sdk/tests/logs/test_log_record.py
@@ -17,6 +17,7 @@ import unittest
 import warnings
 
 from opentelemetry.attributes import BoundedAttributes
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk._logs import (
     LogDroppedAttributesWarning,
     LogLimits,
@@ -38,7 +39,10 @@ class TestLogRecord(unittest.TestCase):
                 "trace_id": "",
                 "span_id": "",
                 "trace_flags": None,
-                "resource": "",
+                "resource": {
+                    "attributes": {"service.name": "foo"},
+                    "schema_url": "",
+                },
             },
             indent=4,
         )
@@ -46,8 +50,14 @@ class TestLogRecord(unittest.TestCase):
             timestamp=0,
             observed_timestamp=0,
             body="a log line",
-        ).to_json()
-        self.assertEqual(expected, actual)
+            resource=Resource({"service.name": "foo"}),
+        )
+
+        self.assertEqual(expected, actual.to_json(indent=4))
+        self.assertEqual(
+            actual.to_json(indent=None),
+            '{"body": "a log line", "severity_number": "None", "severity_text": null, "attributes": null, "dropped_attributes": 0, "timestamp": "1970-01-01T00:00:00.000000Z", "observed_timestamp": "1970-01-01T00:00:00.000000Z", "trace_id": "", "span_id": "", "trace_flags": null, "resource": {"attributes": {"service.name": "foo"}, "schema_url": ""}}',
+        )
 
     def test_log_record_bounded_attributes(self):
         attr = {"key": "value"}
