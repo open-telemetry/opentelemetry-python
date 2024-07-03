379a2141663378ca859e65a46c60c8a3034bee31
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-07-03 11:51:34 -0600

    Revert "Use flaky instead"
    This reverts commit 4d433d658eb9eab0b9684ed58d7c7ca1e0ad3127.

--- opentelemetry-sdk/tests/trace/export/test_export.py
+++ opentelemetry-sdk/tests/trace/export/test_export.py
@@ -23,7 +23,6 @@ from platform import python_implementation, system
 from unittest import mock
 
 from pytest import mark
-from flaky import flaky
 
 from opentelemetry import trace as trace_api
 from opentelemetry.context import Context
@@ -462,7 +461,11 @@ class TestBatchSpanProcessor(ConcurrencyTestBase):
 
         span_processor.shutdown()
 
-    @flaky(max_runs=3, min_passes=1)
+    @mark.flaky(
+        retries=3,
+        only_on=[AssertionError],
+        condition=(python_implementation == "PyPy" or system() == "Windows"),
+    )
     def test_batch_span_processor_scheduled_delay(self):
         """Test that spans are exported each schedule_delay_millis"""
         spans_names_list = []
