26f6d684b57e51e0a01c34eae67c98b7f90a123f
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-07-03 14:07:51 -0600

    Fix comment

--- opentelemetry-sdk/tests/trace/export/test_export.py
+++ opentelemetry-sdk/tests/trace/export/test_export.py
@@ -463,7 +463,7 @@ class TestBatchSpanProcessor(ConcurrencyTestBase):
 
     @mark.skipif(
         python_implementation() == "PyPy" or system() == "Windows",
-        reason="This test randomly fails with huge delta in Windows with PyPy",
+        reason="This test randomly fails with huge delta in Windows or PyPy",
     )
     def test_batch_span_processor_scheduled_delay(self):
         """Test that spans are exported each schedule_delay_millis"""
