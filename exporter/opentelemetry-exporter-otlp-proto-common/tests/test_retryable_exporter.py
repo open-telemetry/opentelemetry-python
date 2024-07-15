c7b03b769a99f5417ccd427ab9bf1b8a639975b0
Lars Michelsen <lm@checkmk.com>
2024-07-15 13:55:28 +0200

    Add test cases for RetryableExporter
    A few tests are still commented out and will be enabled later, once
    the implementation is updated to implement the desired behavior.
    
    The tests are, like other existing tests, timing sensitive which makes
    them prone to fail on slow systems. It would certainly be better to get
    rid of this issue. I haven't found another approach in the existing test
    cases so I sticked to the current pattern.

---
+++ exporter/opentelemetry-exporter-otlp-proto-common/tests/test_retryable_exporter.py
@@ -0,0 +1,305 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import threading
+import time
+import unittest
+from itertools import repeat
+from logging import WARNING
+from typing import Type
+from unittest.mock import Mock, patch
+
+from opentelemetry.exporter.otlp.proto.common.exporter import (
+    RetryableExportError,
+    RetryingExporter,
+)
+from opentelemetry.exporter.otlp.proto.common.exporter import (
+    logger as exporter_logger,
+)
+
+result_type: Type = Mock()
+
+
+class TestRetryableExporter(unittest.TestCase):
+    def test_export_no_retry(self):
+        export_func = Mock()
+        exporter = RetryingExporter(export_func, result_type, timeout_sec=10.0)
+        with self.subTest("Export success"):
+            export_func.reset_mock()
+            export_func.configure_mock(return_value=result_type.SUCCESS)
+            with self.assertRaises(AssertionError):
+                with self.assertLogs(level=WARNING):
+                    result = exporter.export_with_retry("payload")
+            self.assertIs(result, result_type.SUCCESS)
+            export_func.assert_called_once_with("payload", 10.0)
+
+        with self.subTest("Export Fail"):
+            export_func.reset_mock()
+            export_func.configure_mock(return_value=result_type.FAILURE)
+            with self.assertRaises(AssertionError):
+                with self.assertLogs(exporter_logger, level=WARNING):
+                    result = exporter.export_with_retry("payload")
+            self.assertIs(result, result_type.FAILURE)
+            export_func.assert_called_once_with("payload", 10.0)
+
+    @patch(
+        "opentelemetry.exporter.otlp.proto.common.exporter._create_exp_backoff_generator",
+        return_value=repeat(0),
+    )
+    def test_export_retry(self, mock_backoff):
+        """
+        Test we retry until success/failure.
+        """
+        side_effect = [
+            RetryableExportError(None),
+            RetryableExportError(None),
+            result_type.SUCCESS,
+        ]
+        export_func = Mock(side_effect=side_effect)
+        exporter = RetryingExporter(export_func, result_type, timeout_sec=10.0)
+
+        with self.subTest("Retry until success"):
+            with self.assertLogs(level=WARNING):
+                result = exporter.export_with_retry("")
+            self.assertEqual(export_func.call_count, len(side_effect))
+            self.assertIs(result, result_type.SUCCESS)
+
+        with self.subTest("Retry until failure"):
+            export_func.reset_mock()
+            side_effect.insert(0, RetryableExportError(None))
+            side_effect[-1] = result_type.FAILURE
+            export_func.configure_mock(side_effect=side_effect)
+            with self.assertLogs(level=WARNING):
+                result = exporter.export_with_retry("")
+            self.assertEqual(export_func.call_count, len(side_effect))
+            self.assertIs(result, result_type.FAILURE)
+
+    @patch(
+        "opentelemetry.exporter.otlp.proto.common.exporter._create_exp_backoff_generator",
+        return_value=repeat(0.25),
+    )
+    def test_export_uses_retry_delay(self, mock_backoff):
+        """
+        Test we retry using the delay specified in the RPC error as a lower bound.
+        """
+        side_effects = [
+            RetryableExportError(0.0),
+            RetryableExportError(0.25),
+            RetryableExportError(0.75),
+            RetryableExportError(1.0),
+            result_type.SUCCESS,
+        ]
+        exporter = RetryingExporter(
+            Mock(side_effect=side_effects), result_type, timeout_sec=10.0
+        )
+
+        with patch(
+            "opentelemetry.exporter.otlp.proto.common.exporter.sleep"
+        ) as wait_mock:
+            with self.assertLogs(level=WARNING):
+                result = exporter.export_with_retry("payload")
+        self.assertIs(result, result_type.SUCCESS)
+        self.assertEqual(wait_mock.call_count, len(side_effects) - 1)
+        self.assertEqual(wait_mock.call_args_list[0].args, (0.0,))
+        self.assertEqual(wait_mock.call_args_list[1].args, (0.25,))
+        self.assertEqual(wait_mock.call_args_list[2].args, (0.75,))
+        self.assertEqual(wait_mock.call_args_list[3].args, (1.00,))
+
+    # Does not work like this yet. Will be enabled soon.
+    # @patch(
+    #    "opentelemetry.exporter.otlp.proto.common.exporter._create_exp_backoff_generator",
+    #    return_value=repeat(0.1),
+    # )
+    # def test_retry_delay_exceeds_timeout(self, mock_backoff):
+    #    """
+    #    Test we timeout if we can't respect retry_delay.
+    #    """
+    #    side_effects = [
+    #        RetryableExportError(0.25),
+    #        RetryableExportError(1.0),  # should timeout here
+    #        result_type.SUCCESS,
+    #    ]
+
+    #    mock_export_func = Mock(side_effect=side_effects)
+    #    exporter = RetryingExporter(
+    #        mock_export_func,
+    #        result_type,
+    #        timeout_sec=0.5,
+    #    )
+
+    #    with self.assertLogs(level=WARNING):
+    #        self.assertEqual(
+    #            exporter.export_with_retry("payload"), result_type.FAILURE
+    #        )
+    #    self.assertEqual(mock_export_func.call_count, 2)
+
+    def test_shutdown(self):
+        """Test we refuse to export if shut down."""
+        mock_export_func = Mock(return_value=result_type.SUCCESS)
+        exporter = RetryingExporter(
+            mock_export_func,
+            result_type,
+            timeout_sec=10.0,
+        )
+
+        self.assertEqual(
+            exporter.export_with_retry("payload"), result_type.SUCCESS
+        )
+        mock_export_func.assert_called_once()
+        exporter.shutdown()
+        with self.assertLogs(level=WARNING) as warning:
+            self.assertEqual(
+                exporter.export_with_retry("payload"), result_type.FAILURE
+            )
+        self.assertEqual(
+            warning.records[0].message,
+            "Exporter already shutdown, ignoring batch",
+        )
+        mock_export_func.assert_called_once()
+
+    @patch("opentelemetry.exporter.otlp.proto.common.exporter.sleep")
+    def test_shutdown_wait_last_export(self, mock_sleep):
+        """Test that shutdown waits for ongoing export to complete."""
+
+        timeout_sec = 10.0
+
+        class ExportFunc:
+            is_exporting = threading.Event()
+            ready_to_continue = threading.Event()
+            side_effect = [
+                RetryableExportError(None),
+                RetryableExportError(None),
+                result_type.SUCCESS,
+            ]
+            mock_export_func = Mock(side_effect=side_effect)
+
+            def __call__(self, *args, **kwargs):
+                self.is_exporting.set()
+                self.ready_to_continue.wait()
+                return self.mock_export_func(*args, **kwargs)
+
+        export_func = ExportFunc()
+
+        exporter = RetryingExporter(
+            export_func, result_type, timeout_sec=timeout_sec
+        )
+
+        class ExportWrap:
+            def __init__(self) -> None:
+                self.result = None
+
+            def __call__(self, *args, **kwargs):
+                self.result = exporter.export_with_retry("payload")
+                return self.result
+
+        export_wrapped = ExportWrap()
+
+        export_thread = threading.Thread(
+            name="export_thread", target=export_wrapped
+        )
+        with self.assertLogs(level=WARNING):
+            try:
+                # Simulate shutdown occurring during retry process
+                # Intended execution flow
+                #
+                #   main thread:
+                #       - start export_thread
+                #       - wait for is_exporting
+                #   export_thread:
+                #       - call export_func
+                #       - set is_exporting
+                #       - wait for ready_to_continue
+                #   main thread:
+                #       - start shutdown thread
+                #       - sleep to yield to shutdown thread
+                #   shutdown_thread:
+                #       - block at acquiring lock held by export_thread
+                #       - shutdown is now pending timeout/export completion
+                #   main thread:
+                #       - set ready_to_continue
+                #       - join all threads
+                export_thread.start()
+                export_func.is_exporting.wait()
+                start_time = time.time()
+                shutdown_thread = threading.Thread(
+                    name="shutdown_thread", target=exporter.shutdown
+                )
+                shutdown_thread.start()
+                time.sleep(0.025)
+                export_func.ready_to_continue.set()
+            finally:
+                export_thread.join()
+                shutdown_thread.join()
+
+        duration = time.time() - start_time
+        self.assertLessEqual(duration, timeout_sec)
+        # pylint: disable=protected-access
+        self.assertTrue(exporter._shutdown)
+        self.assertIs(export_wrapped.result, result_type.SUCCESS)
+
+    # Does not work like this yet. Will be enabled soon.
+    # def test_shutdown_timeout_cancels_export_retries(self):
+    #    """Test that shutdown timing out cancels ongoing retries."""
+
+    #    class ExportFunc:
+    #        is_exporting = threading.Event()
+    #        ready_to_continue = threading.Event()
+    #        mock_export_func = Mock(side_effect=RetryableExportError(None))
+
+    #        def __call__(self, *args, **kwargs):
+    #            self.is_exporting.set()
+    #            self.ready_to_continue.wait()
+    #            return self.mock_export_func(*args, **kwargs)
+
+    #    export_func = ExportFunc()
+
+    #    exporter = RetryingExporter(export_func, result_type, timeout_sec=30.0)
+
+    #    class ExportWrap:
+    #        def __init__(self) -> None:
+    #            self.result = None
+
+    #        def __call__(self, *args, **kwargs):
+    #            self.result = exporter.export_with_retry("payload")
+    #            return self.result
+
+    #    export_wrapped = ExportWrap()
+
+    #    shutdown_timeout = 1
+
+    #    export_thread = threading.Thread(target=export_wrapped)
+    #    with self.assertLogs(level=WARNING) as warning:
+    #        try:
+    #            export_thread.start()
+    #            export_func.is_exporting.wait()
+    #            start_time = time.time()
+    #            shutdown_thread = threading.Thread(
+    #                target=exporter.shutdown, args=[shutdown_timeout * 1e3]
+    #            )
+    #            shutdown_thread.start()
+    #            time.sleep(0)
+    #            export_func.ready_to_continue.set()
+    #        finally:
+    #            export_thread.join()
+    #            shutdown_thread.join()
+    #    duration = time.time() - start_time
+    #    self.assertAlmostEqual(duration, shutdown_timeout, places=1)
+    #    # pylint: disable=protected-access
+    #    self.assertTrue(exporter._shutdown)
+    #    self.assertIs(export_wrapped.result, result_type.FAILURE)
+    #    print(warning.records)
+    #    self.assertEqual(
+    #        warning.records[-1].message,
+    #        "Export cancelled due to shutdown timing out",
+    #    )
