c35bafb04f7e249a6bec871af25dd4c10a5b89de
Mauro de Carvalho <mauro.dcg@gmail.com>
2024-07-23 19:26:05 -0300

    fix: removing unnecessary test

--- opentelemetry-sdk/tests/metrics/test_measurement_consumer.py
+++ opentelemetry-sdk/tests/metrics/test_measurement_consumer.py
@@ -180,10 +180,6 @@ class TestSynchronousMeasurementConsumer(TestCase):
             -1
         ].kwargs["timeout_millis"]
 
-        self.assertAlmostEqual(
-            round(10000 - callback_options_time_call), 1000.0, places=1
-        )
-
         self.assertLess(
             callback_options_time_call,
             10000,
