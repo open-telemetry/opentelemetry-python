open-telemetry/opentelemetry-python

commit 9481893f73ad456ae171b93ee3ac81de8b775f94

Add Span.add_link() method to add link after span start. (#3618)
* Add Span.add_link() method to add link after span start.

* Update CHANGELOG.md

* Update CHANGELOG.md

* Unmark Span.add_link() as abstract method and add warning.

* Add Span.add_link() method to add link after span start.

* Update CHANGELOG.md

* Update CHANGELOG.md

* Unmark Span.add_link() as abstract method and add warning.

* Update warning message for Span.add_link().

---------

Co-authored-by: Diego Hurtado <ocelotl@users.noreply.github.com>

opentelemetry-api/src/opentelemetry/trace/__init__.py
@@ -85,7 +85,6 @@ from typing import Iterator, Optional, Sequence, cast
 from deprecated import deprecated
 
 from opentelemetry import context as context_api
-from opentelemetry.attributes import BoundedAttributes  # type: ignore
 from opentelemetry.context.context import Context
 from opentelemetry.environment_variables import OTEL_PYTHON_TRACER_PROVIDER
 from opentelemetry.trace.propagation import (
@@ -144,9 +143,7 @@ class Link(_LinkBase):
         attributes: types.Attributes = None,
     ) -> None:
         super().__init__(context)
-        self._attributes = BoundedAttributes(
-            attributes=attributes
-        )  # type: types.Attributes
+        self._attributes = attributes
 
     @property
     def attributes(self) -> types.Attributes:

opentelemetry-sdk/tests/trace/test_trace.py
@@ -900,6 +900,37 @@ class TestSpan(unittest.TestCase):
             with self.assertRaises(TypeError):
                 root.links[1].attributes["name"] = "new_neighbour"
 
+    def test_add_link(self):
+        id_generator = RandomIdGenerator()
+        other_context = trace_api.SpanContext(
+            trace_id=id_generator.generate_trace_id(),
+            span_id=id_generator.generate_span_id(),
+            is_remote=False,
+        )
+
+        with self.tracer.start_as_current_span("root") as root:
+            root.add_link(other_context, {"name": "neighbor"})
+
+            self.assertEqual(len(root.links), 1)
+            self.assertEqual(
+                root.links[0].context.trace_id, other_context.trace_id
+            )
+            self.assertEqual(
+                root.links[0].context.span_id, other_context.span_id
+            )
+            self.assertEqual(root.links[0].attributes, {"name": "neighbor"})
+
+            with self.assertRaises(TypeError):
+                root.links[0].attributes["name"] = "new_neighbour"
+
+    def test_add_link_with_invalid_span_context(self):
+        other_context = trace_api.INVALID_SPAN_CONTEXT
+
+        with self.tracer.start_as_current_span("root") as root:
+            root.add_link(other_context)
+
+            self.assertEqual(len(root.links), 0)
+
     def test_update_name(self):
         with self.tracer.start_as_current_span("root") as root:
             # name

opentelemetry-sdk/src/opentelemetry/sdk/trace/__init__.py
@@ -850,6 +850,30 @@ class Span(trace_api.Span, ReadableSpan):
             )
         )
 
+    @_check_span_ended
+    def _add_link(self, link: trace_api.Link) -> None:
+        self._links.append(link)
+
+    def add_link(
+        self,
+        context: SpanContext,
+        attributes: types.Attributes = None,
+    ) -> None:
+        if context is None or not context.is_valid:
+            return
+
+        attributes = BoundedAttributes(
+            self._limits.max_link_attributes,
+            attributes,
+            max_value_len=self._limits.max_attribute_length,
+        )
+        self._add_link(
+            trace_api.Link(
+                context=context,
+                attributes=attributes,
+            )
+        )
+
     def _readable_span(self) -> ReadableSpan:
         return ReadableSpan(
             name=self._name,

opentelemetry-api/src/opentelemetry/trace/span.py
@@ -3,6 +3,7 @@ import logging
 import re
 import types as python_types
 import typing
+import warnings
 
 from opentelemetry.trace.status import Status, StatusCode
 from opentelemetry.util import types
@@ -116,6 +117,26 @@ class Span(abc.ABC):
         attributes passed as arguments. Implementations should generate a
         timestamp if the `timestamp` argument is omitted.
         """
+
+    def add_link(  # pylint: disable=no-self-use
+        self,
+        context: "SpanContext",
+        attributes: types.Attributes = None,
+    ) -> None:
+        """Adds a `Link`.
+
+        Adds a single `Link` with the `SpanContext` of the span to link to and,
+        optionally, attributes passed as arguments. Implementations may ignore
+        calls with an invalid span context.
+
+        Note: It is preferred to add links at span creation, instead of calling
+        this method later since samplers can only consider information already
+        present during span creation.
+        """
+        warnings.warn(
+            "Span.add_link() not implemented and will be a no-op. "
+            "Use opentelemetry-sdk >= 1.23 to add links after span creation"
+        )
 
     @abc.abstractmethod
     def update_name(self, name: str) -> None:
@@ -520,6 +541,13 @@ class NonRecordingSpan(Span):
         name: str,
         attributes: types.Attributes = None,
         timestamp: typing.Optional[int] = None,
+    ) -> None:
+        pass
+
+    def add_link(
+        self,
+        context: "SpanContext",
+        attributes: types.Attributes = None,
     ) -> None:
         pass
 

commit 0c4409815767feb18a0a3356bf337269e5c3ef2f

Fix use of `link.attributes.dropped`, which may not exist
A bug was introduced in #3618 as `.dropped` only exists on
`BoundedAttributes`, not the base `Attributes`.

Similar to #3965, add a new `dropped_attributes` property to `Link`.

exporter/opentelemetry-exporter-otlp-proto-common/src/opentelemetry/exporter/otlp/proto/common/_internal/trace_encoder/__init__.py
@@ -157,7 +157,7 @@ def _encode_links(links: Sequence[Link]) -> Sequence[PB2SPan.Link]:
                 trace_id=_encode_trace_id(link.context.trace_id),
                 span_id=_encode_span_id(link.context.span_id),
                 attributes=_encode_attributes(link.attributes),
-                dropped_attributes_count=link.attributes.dropped,
+                dropped_attributes_count=link.dropped_attributes,
                 flags=_span_flags(link.context),
             )
             pb2_links.append(encoded_link)

opentelemetry-api/src/opentelemetry/trace/__init__.py
@@ -84,6 +84,7 @@ from typing import Iterator, Optional, Sequence, cast
 from deprecated import deprecated
 
 from opentelemetry import context as context_api
+from opentelemetry.attributes import BoundedAttributes
 from opentelemetry.context.context import Context
 from opentelemetry.environment_variables import OTEL_PYTHON_TRACER_PROVIDER
 from opentelemetry.trace.propagation import (
@@ -148,6 +149,12 @@ class Link(_LinkBase):
     @property
     def attributes(self) -> types.Attributes:
         return self._attributes
+
+    @property
+    def dropped_attributes(self) -> int:
+        if isinstance(self._attributes, BoundedAttributes):
+            return self._attributes.dropped
+        return 0
 
 
 _Links = Optional[Sequence[Link]]

exporter/opentelemetry-exporter-otlp-proto-grpc/tests/test_otlp_trace_exporter.py
@@ -171,6 +171,7 @@ class TestOTLPSpanExporter(TestCase):
                         "attributes": BoundedAttributes(
                             attributes={"a": 1, "b": False}
                         ),
+                        "dropped_attributes": 0,
                         "kind": OTLPSpan.SpanKind.SPAN_KIND_INTERNAL,  # pylint: disable=no-member
                     }
                 )

opentelemetry-sdk/tests/trace/test_trace.py
@@ -669,6 +669,19 @@ class TestReadableSpan(unittest.TestCase):
         event2 = trace.Event("foo2", {"bar2": "baz2"})
         self.assertEqual(event2.dropped_attributes, 0)
 
+    def test_link_dropped_attributes(self):
+        link1 = trace_api.Link(
+            mock.Mock(spec=trace_api.SpanContext),
+            BoundedAttributes(0, attributes={"bar1": "baz1"}),
+        )
+        self.assertEqual(link1.dropped_attributes, 1)
+
+        link2 = trace_api.Link(
+            mock.Mock(spec=trace_api.SpanContext),
+            {"bar2": "baz2"},
+        )
+        self.assertEqual(link2.dropped_attributes, 0)
+
 
 class DummyError(Exception):
     pass
@@ -1897,7 +1910,7 @@ class TestSpanLimits(unittest.TestCase):
         self.assertEqual(2, span.dropped_attributes)
         self.assertEqual(3, span.dropped_events)
         self.assertEqual(2, span.events[0].dropped_attributes)
-        self.assertEqual(2, span.links[0].attributes.dropped)
+        self.assertEqual(2, span.links[0].dropped_attributes)
 
     def _test_span_limits(
         self,