4a2a9840d97b706df0db2ee119bac7da45c3a08a
Arne Caratti <arne.recknagel@hotmail.com>
2024-08-28 12:15:24 +0200

    chore: ran black

--- opentelemetry-sdk/tests/logs/test_handler.py
+++ opentelemetry-sdk/tests/logs/test_handler.py
@@ -100,7 +100,9 @@ class TestLoggingHandler(unittest.TestCase):
             log_record = processor.get_log_record(0)
 
             self.assertIsNotNone(log_record)
-            self.assertEqual(log_record.trace_id, INVALID_SPAN_CONTEXT.trace_id)
+            self.assertEqual(
+                log_record.trace_id, INVALID_SPAN_CONTEXT.trace_id
+            )
             self.assertEqual(log_record.span_id, INVALID_SPAN_CONTEXT.span_id)
             self.assertEqual(
                 log_record.trace_flags, INVALID_SPAN_CONTEXT.trace_flags
@@ -119,7 +121,9 @@ class TestLoggingHandler(unittest.TestCase):
         with set_up_test_logging(logging.WARNING) as (processor, logger):
             # Assert emit gets called for warning message
             with self.assertLogs(level=logging.WARNING):
-                logger.warning("Warning message", extra={"http.status_code": 200})
+                logger.warning(
+                    "Warning message", extra={"http.status_code": 200}
+                )
 
             log_record = processor.get_log_record(0)
 
@@ -139,8 +143,12 @@ class TestLoggingHandler(unittest.TestCase):
             )
             # The line of the log statement is not a constant (changing tests may change that),
             # so only check that the attribute is present.
-            self.assertTrue(SpanAttributes.CODE_LINENO in log_record.attributes)
-            self.assertTrue(isinstance(log_record.attributes, BoundedAttributes))
+            self.assertTrue(
+                SpanAttributes.CODE_LINENO in log_record.attributes
+            )
+            self.assertTrue(
+                isinstance(log_record.attributes, BoundedAttributes)
+            )
 
     def test_log_record_exception(self):
         """Exception information will be included in attributes"""
@@ -185,7 +193,9 @@ class TestLoggingHandler(unittest.TestCase):
 
             self.assertIsNotNone(log_record)
             self.assertEqual(log_record.body, "Zero Division Error")
-            self.assertNotIn(SpanAttributes.EXCEPTION_TYPE, log_record.attributes)
+            self.assertNotIn(
+                SpanAttributes.EXCEPTION_TYPE, log_record.attributes
+            )
             self.assertNotIn(
                 SpanAttributes.EXCEPTION_MESSAGE, log_record.attributes
             )
@@ -202,13 +212,19 @@ class TestLoggingHandler(unittest.TestCase):
 
                 log_record = processor.get_log_record(0)
 
-                self.assertEqual(log_record.body, "Critical message within span")
+                self.assertEqual(
+                    log_record.body, "Critical message within span"
+                )
                 self.assertEqual(log_record.severity_text, "CRITICAL")
-                self.assertEqual(log_record.severity_number, SeverityNumber.FATAL)
+                self.assertEqual(
+                    log_record.severity_number, SeverityNumber.FATAL
+                )
                 span_context = span.get_span_context()
                 self.assertEqual(log_record.trace_id, span_context.trace_id)
                 self.assertEqual(log_record.span_id, span_context.span_id)
-                self.assertEqual(log_record.trace_flags, span_context.trace_flags)
+                self.assertEqual(
+                    log_record.trace_flags, span_context.trace_flags
+                )
 
     def test_log_record_args_are_translated(self):
         with set_up_test_logging(logging.WARNING) as (processor, logger):
@@ -229,18 +245,19 @@ class TestLoggingHandler(unittest.TestCase):
             logger.warning("Test message")
 
             log_record = processor.get_log_record(0)
-            self.assertEqual(
-                log_record.body, "Test message"
-            )
+            self.assertEqual(log_record.body, "Test message")
 
     def test_custom_format_is_called(self):
-        with set_up_test_logging(logging.WARNING, formatter=logging.Formatter("%(name)s - %(levelname)s - %(message)s")) as (processor, logger):
+        with set_up_test_logging(
+            logging.WARNING,
+            formatter=logging.Formatter(
+                "%(name)s - %(levelname)s - %(message)s"
+            ),
+        ) as (processor, logger):
             logger.warning("Test message")
 
             log_record = processor.get_log_record(0)
-            self.assertEqual(
-                log_record.body, "foo - WARNING - Test message"
-            )
+            self.assertEqual(log_record.body, "foo - WARNING - Test message")
 
     def test_log_body_is_always_string(self):
         with set_up_test_logging(logging.WARNING) as (processor, logger):
