0b898f4c71ea0f6ce549e9760021ae6db9b09764
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-09-04 11:22:59 -0600

    Use importlib_metadata for 3.8 and 3.9 only

--- opentelemetry-api/src/opentelemetry/util/_importlib_metadata.py
+++ opentelemetry-api/src/opentelemetry/util/_importlib_metadata.py
@@ -12,18 +12,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# FIXME: Use importlib.metadata when support for 3.11 is dropped if the rest of
-# the supported versions at that time have the same API.
-from importlib_metadata import (  # type: ignore
-    EntryPoint,
-    EntryPoints,
-    entry_points,
-    version,
-)
-
 # The importlib-metadata library has introduced breaking changes before to its
 # API, this module is kept just to act as a layer between the
 # importlib-metadata library and our project if in any case it is necessary to
 # do so.
+
+from sys import version_info
+
+if version_info.minor < 10:
+
+    # pylint: disable=import-error
+    from importlib_metadata import version  # type: ignore
+    from importlib_metadata import EntryPoint, EntryPoints
+    from importlib_metadata import (
+        entry_points as importlib_metadata_entry_points,
+    )
+
+else:
+
+    from importlib.metadata import EntryPoint, EntryPoints
+    from importlib.metadata import (
+        entry_points as importlib_metadata_entry_points,
+    )
+    from importlib.metadata import version
+
+
+def entry_points(**params) -> EntryPoints:  # type: ignore
+    """
+    Same as entry_points but requires at least one argument
+
+    For Python 3.8 or 3.9:
+
+    isinstance(
+        importlib_metadata.entry_points(), importlib_metadata.EntryPoints
+    )
+
+    evaluates to True.
+
+    For Python 3.10, 3.11:
+
+    isinstance(
+        importlib.metadata.entry_points(), importlib.metadata.SelectableGroups
+    )
+
+    evaluates to True.
+
+    For Python 3.12:
+
+    isinstance(
+        importlib.metadata.entry_points(), importlib.metadata.EntryPoints
+    )
+
+    evaluates to True.
+
+    So, when called with no arguments, entry_points returns objects of
+    different types depending on the Python version that is being used. This is
+    obviously very problematic. Nevertheless, in our code we don't ever call
+    entry_points without arguments, so the approach here is to redefine
+    entry_points so that it requires at least one argument.
+    """
+
+    if not params:  # type: ignore
+        raise ValueError("entry_points requires at least one argument")
+    return importlib_metadata_entry_points(**params)  # type: ignore
+
 
 __all__ = ["entry_points", "version", "EntryPoint", "EntryPoints"]
--- opentelemetry-api/tests/util/test__importlib_metadata.py
+++ opentelemetry-api/tests/util/test__importlib_metadata.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from sys import version_info
 from unittest import TestCase
 
 from opentelemetry.metrics import MeterProvider
@@ -19,6 +20,38 @@ from opentelemetry.util._importlib_metadata import EntryPoint, EntryPoints
 from opentelemetry.util._importlib_metadata import (
     entry_points as importlib_metadata_entry_points,
 )
+from opentelemetry.util._importlib_metadata import version
+
+
+class TestDependencies(TestCase):
+
+    def test_dependencies(self):
+
+        # pylint: disable=import-outside-toplevel
+        # pylint: disable=unused-import
+        # pylint: disable=import-error
+        if version_info.minor < 10:
+            try:
+                import importlib_metadata  # type: ignore
+
+            except ImportError:
+                self.fail(
+                    "importlib_metadata not installed when testing with "
+                    f"{version_info.major}.{version_info.minor}"
+                )
+
+        else:
+            try:
+                import importlib_metadata  # noqa
+
+            except ImportError:
+                pass
+
+            else:
+                self.fail(
+                    "importlib_metadata installed when testing with "
+                    f"{version_info.major}.{version_info.minor}"
+                )
 
 
 class TestEntryPoints(TestCase):
@@ -40,27 +73,29 @@ class TestEntryPoints(TestCase):
         """
         Test that entry_points behaves the same regardless of the Python
         version.
+
+        importlib.metadata was introduced in 3.8 as a replacement for
+        pkg_resources. The problem is that the API of importlib.metadata
+        changed in subsequent versions.
+
+        For example, in 3.8 or 3.9 importlib.metadata.entry_points does not
+        support the keyword arguments group or name, but those keyword
+        arguments are supported in 3.10, 3.11 and 3.12.
+
+        There exists a package named importlib-metadata that has an API that
+        includes a function named importlib_metadata.entry_points which
+        supports the keyword arguments group and name, so we use
+        importlib_metadata.entry_points when running in 3.8 or 3.9.
+
+        importlib_metadata.entry_points and importlib.metadata.entry_points do
+        not return objects of the same type when called without any arguments.
+        That is why the implementation of the
+        opentelemetry.util._importlib_metadata redefines entry_points so that
+        it is mandatory to use an argument.
         """
 
-        entry_points = importlib_metadata_entry_points()
-
-        self.assertIsInstance(entry_points, EntryPoints)
-
-        entry_points = entry_points.select(group="opentelemetry_propagator")
-        self.assertIsInstance(entry_points, EntryPoints)
-
-        entry_points = entry_points.select(name="baggage")
-        self.assertIsInstance(entry_points, EntryPoints)
-
-        entry_point = next(iter(entry_points))
-        self.assertIsInstance(entry_point, EntryPoint)
-
-        self.assertEqual(entry_point.name, "baggage")
-        self.assertEqual(entry_point.group, "opentelemetry_propagator")
-        self.assertEqual(
-            entry_point.value,
-            "opentelemetry.baggage.propagation:W3CBaggagePropagator",
-        )
+        with self.assertRaises(ValueError):
+            importlib_metadata_entry_points()
 
         entry_points = importlib_metadata_entry_points(
             group="opentelemetry_propagator"
@@ -106,3 +141,5 @@ class TestEntryPoints(TestCase):
         entry_points = importlib_metadata_entry_points(group="abc", name="abc")
         self.assertIsInstance(entry_points, EntryPoints)
         self.assertEqual(len(entry_points), 0)
+
+        self.assertIsInstance(version("opentelemetry-api"), str)
