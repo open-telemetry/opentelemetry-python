489c1707abf6271fd9e8c2b92083e660ec487147
Leighton Chen <lechen@microsoft.com>
2024-11-07 15:54:25 -0800

    Remove generate contrib workflow from core
    Including the generate contrib workflow in core will block changes in core if contrib workflows are in an erroneous state. While we do want to fix those erroneous workflows, we do not want to block the development of core and fix them in parallel.

--- .github/workflows/generate_workflows.py
+++ .github/workflows/generate_workflows.py
@@ -17,5 +17,4 @@ generate_test_workflow(
     "windows-latest",
 )
 generate_lint_workflow(tox_ini_path, workflows_directory_path)
-generate_contrib_workflow(workflows_directory_path)
 generate_misc_workflow(tox_ini_path, workflows_directory_path)
