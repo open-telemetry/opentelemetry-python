2c75e8e1ecee2855a070e93255daef7e809c50b5
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2024-12-23 17:32:33 +0100

    Update opentelemetry-sdk/src/opentelemetry/sdk/resources/__init__.py
    Co-authored-by: Aaron Abbott <aaronabbott@google.com>

--- opentelemetry-sdk/src/opentelemetry/sdk/resources/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/resources/__init__.py
@@ -23,8 +23,8 @@ This package implements `OpenTelemetry Resources
     these attributes can be included in the Resource.*
 
 Resource objects are created with `Resource.create`, which accepts attributes
-(key-values). Resources should NOT be created via constructor yet ResourceDetector
-instance can't use `Resource.create` to avoid infinite loops. Working with
+(key-values). Resources should NOT be created via constructor except by `ResourceDetector`
+instances which can't use `Resource.create` to avoid infinite loops. Working with
 `Resource` objects should only be done via the Resource API methods. Resource
 attributes can also be passed at process invocation in the
 :envvar:`OTEL_RESOURCE_ATTRIBUTES` environment variable. You should register
