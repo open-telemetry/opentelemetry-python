232a364b38d04c7b0bf5c5c01255a4e07139e8c4
Liudmila Molkova <limolkova@microsoft.com>
2025-01-24 15:11:23 -0800

    up

--- scripts/semconv/generate.sh
+++ scripts/semconv/generate.sh
@@ -6,7 +6,7 @@ ROOT_DIR="${SCRIPT_DIR}/../.."
 
 # freeze the spec version to make SemanticAttributes generation reproducible
 SEMCONV_VERSION=1.30.0
-SEMCONV_VERSION_TAG=v1.30.0
+SEMCONV_VERSION_TAG=v$SEMCONV_VERSION
 OTEL_WEAVER_IMG_VERSION=v0.12.0
 INCUBATING_DIR=_incubating
 cd ${SCRIPT_DIR}
