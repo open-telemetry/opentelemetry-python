791bff399ba84bb855a37f437e9879f810fdce10
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-02-28 17:24:10 +0100

    Apply suggestions from code review
    Co-authored-by: Emídio Neto <9735060+emdneto@users.noreply.github.com>

--- opentelemetry-api/src/opentelemetry/context/context.py
+++ opentelemetry-api/src/opentelemetry/context/context.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import typing
 from abc import ABC, abstractmethod
 from contextvars import Token
--- opentelemetry-api/src/opentelemetry/context/contextvars_context.py
+++ opentelemetry-api/src/opentelemetry/context/contextvars_context.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from __future__ import annotations
+
 from contextvars import ContextVar, Token
 
 from opentelemetry.context.context import Context, _RuntimeContext
