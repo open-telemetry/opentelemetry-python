95ccfea3f9af5d10c6220b9788e9825d4958eec4
Dylan Russell <dylanrussell@google.com>
2025-05-09 17:43:25 +0000

    Respond to review comments..

--- exporter/opentelemetry-exporter-otlp-proto-grpc/src/opentelemetry/exporter/otlp/proto/grpc/exporter.py
+++ exporter/opentelemetry-exporter-otlp-proto-grpc/src/opentelemetry/exporter/otlp/proto/grpc/exporter.py
@@ -78,9 +78,14 @@ _JSON_CONFIG = json.dumps(
             {
                 "name": [dict()],
                 "retryPolicy": {
+                    # 5 is the maximum allowable attempts allowed by grpc retry policy.
+                    # This policy results in backoffs of 1s, 2s, 4s, and then 8s after the initial failed attempt.
+                    # Timeout set on the RPC call encompasses the retry backoffs AND time spent waiting
+                    # for a response. DEADLINE_EXCEEDED is returned if all the attempts cannot complete within the
+                    # timeout. See https://grpc.io/docs/guides/retry/ for more details.
                     "maxAttempts": 5,
                     "initialBackoff": "1s",
-                    "maxBackoff": "64s",
+                    "maxBackoff": "9s",
                     "backoffMultiplier": 2,
                     "retryableStatusCodes": [
                         "UNAVAILABLE",
