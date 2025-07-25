c967cef740476d671e815791a12cfb74e854645e
Jourdan Rodrigues <thiagojourdan@gmail.com>
2025-07-25 15:11:10 -0300

    :umbrella: Assert exception properties

--- opentelemetry-sdk/tests/logs/test_handler.py
+++ opentelemetry-sdk/tests/logs/test_handler.py
@@ -15,6 +15,7 @@
 import logging
 import os
 import sys
+import traceback
 import unittest
 from unittest.mock import Mock, patch
 
@@ -51,17 +52,32 @@ class TestLoggingHandler(unittest.TestCase):
 
     def test_handler_error_exc_info(self):
         processor, logger = set_up_test_logging(logging.NOTSET)
+
+        class CustomException(Exception):
+            pass
+
+        try:
+            raise CustomException("Custom exception")
+        except CustomException as exception:
+            exc_info = (type(exception), exception, exception.__traceback__)
+            traceback_str = "".join(traceback.format_exception(*exc_info))
+
         exc_info_values = [
             # Don't know what caused it in my context, so I'm relying on mocks to replicate the behavior.
             # First the `record.exc_info` becomes a string somehow, then `sys.exc_info` brings the tuple.
             "Stringified exception",
-            (None, None, None),
+            exc_info,
         ]
 
         with patch.object(sys, "exc_info", side_effect=exc_info_values):
             logger.exception("Exception message")  # Should not raise exception
 
         assert processor.emit_count() == 1
+
+        attributes = processor.log_data_emitted[0].log_record.attributes._dict
+        assert attributes["exception.type"] == "CustomException"
+        assert attributes["exception.message"] == str(exc_info[1])
+        assert attributes["exception.stacktrace"] == traceback_str
 
     def test_handler_custom_log_level(self):
         processor, logger = set_up_test_logging(logging.ERROR)
