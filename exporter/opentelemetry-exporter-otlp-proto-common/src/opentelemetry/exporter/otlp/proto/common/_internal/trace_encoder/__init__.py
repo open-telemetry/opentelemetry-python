a1d43e8d4851cd5ba394c4466232542e50fbcdf8
nikhilmantri0902 <nikhil.mantri1999@gmail.com>
2025-10-02 13:24:57 +0530

    chore: added full trace flags implementation and tests

--- exporter/opentelemetry-exporter-otlp-proto-common/src/opentelemetry/exporter/otlp/proto/common/_internal/trace_encoder/__init__.py
+++ exporter/opentelemetry-exporter-otlp-proto-common/src/opentelemetry/exporter/otlp/proto/common/_internal/trace_encoder/__init__.py
@@ -105,10 +105,14 @@ def _encode_resource_spans(
     return pb2_resource_spans
 
 
-def _span_flags(parent_span_context: Optional[SpanContext]) -> int:
-    flags = PB2SpanFlags.SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK
-    if parent_span_context and parent_span_context.is_remote:
-        flags |= PB2SpanFlags.SPAN_FLAGS_CONTEXT_IS_REMOTE_MASK
+def _span_flags(child_trace_flags: int, parent_span_context: Optional[SpanContext]) -> int:
+    # Lower 8 bits: W3C TraceFlags
+    flags = int(child_trace_flags) & int(PB2SpanFlags.SPAN_FLAGS_TRACE_FLAGS_MASK)
+    # Always indicate whether parent remote information is known
+    flags |= int(PB2SpanFlags.SPAN_FLAGS_CONTEXT_HAS_IS_REMOTE_MASK)
+    # Set remote bit when applicable
+    if parent_span_context and getattr(parent_span_context, "is_remote", False):
+        flags |= int(PB2SpanFlags.SPAN_FLAGS_CONTEXT_IS_REMOTE_MASK)
     return flags
 
 
@@ -130,7 +134,7 @@ def _encode_span(sdk_span: ReadableSpan) -> PB2SPan:
         dropped_attributes_count=sdk_span.dropped_attributes,
         dropped_events_count=sdk_span.dropped_events,
         dropped_links_count=sdk_span.dropped_links,
-        flags=_span_flags(sdk_span.parent),
+        flags=_span_flags(getattr(span_context, "trace_flags", 0), sdk_span.parent),
     )
 
 
@@ -161,7 +165,7 @@ def _encode_links(links: Sequence[Link]) -> Sequence[PB2SPan.Link]:
                 span_id=_encode_span_id(link.context.span_id),
                 attributes=_encode_attributes(link.attributes),
                 dropped_attributes_count=link.dropped_attributes,
-                flags=_span_flags(link.context),
+                flags=_span_flags(getattr(link.context, "trace_flags", 0), link.context),
             )
             pb2_links.append(encoded_link)
     return pb2_links
