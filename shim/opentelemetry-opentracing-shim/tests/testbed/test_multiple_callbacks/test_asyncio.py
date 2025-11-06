54ecf4a466538c6d526183ae0fae0fa2b35c2369
emdneto <9735060+emdneto@users.noreply.github.com>
2025-11-06 10:50:45 -0300

    fix shim

--- shim/opentelemetry-opentracing-shim/tests/testbed/test_multiple_callbacks/test_asyncio.py
+++ shim/opentelemetry-opentracing-shim/tests/testbed/test_multiple_callbacks/test_asyncio.py
@@ -25,9 +25,14 @@ logger = get_logger(__name__)
 
 
 class TestAsyncio(OpenTelemetryTestCase):
-    def setUp(self):  # pylint: disable=invalid-name
+    def setUp(self):
         self.tracer = MockTracer()
-        self.loop = asyncio.get_event_loop()
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+    def tearDown(self):
+        self.loop.close()
+        super().tearDown()
 
     def test_main(self):
         # Need to run within a Task, as the scope manager depends
