78dbefdbdd3c25343dc795c5df61d78c4689febd
Paul Fischer <paul_fischer94@hotmail.com>
2025-11-17 14:27:25 +0100

    feat(http_exporter): only retry on connection error

--- exporter/opentelemetry-exporter-otlp-proto-http/src/opentelemetry/exporter/otlp/proto/http/metric_exporter/__init__.py
+++ exporter/opentelemetry-exporter-otlp-proto-http/src/opentelemetry/exporter/otlp/proto/http/metric_exporter/__init__.py
@@ -239,7 +239,10 @@ class OTLPMetricExporter(MetricExporter, OTLPMetricExporterMixin):
                     return MetricExportResult.SUCCESS
             except requests.exceptions.RequestException as error:
                 reason = str(error)
-                retryable = True
+                if isinstance(error, ConnectionError):
+                    retryable = True
+                else:
+                    retryable = False
                 status_code = None
             else:
                 reason = resp.reason
