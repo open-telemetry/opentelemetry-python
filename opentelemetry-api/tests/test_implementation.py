3a51dee88f7874be244f8fd94c762de3f46da646
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2026-01-16 11:37:13 +0100

    More correct nooptracer and more test coverage

--- opentelemetry-api/tests/test_implementation.py
+++ opentelemetry-api/tests/test_implementation.py
@@ -17,6 +17,63 @@ import unittest
 from opentelemetry import trace
 
 
+class RecordingSpan(trace.Span):
+    def __init__(self, context: trace.SpanContext) -> None:
+        self._context = context
+
+    def get_span_context(self) -> trace.SpanContext:
+        return self._context
+
+    def is_recording(self) -> bool:
+        return True
+
+    def end(self, end_time=None) -> None:
+        pass
+
+    def set_attributes(self, attributes) -> None:
+        pass
+
+    def set_attribute(self, key, value) -> None:
+        pass
+
+    def add_event(
+        self,
+        name: str,
+        attributes=None,
+        timestamp=None,
+    ) -> None:
+        pass
+
+    def add_link(
+        self,
+        context,
+        attributes=None,
+    ) -> None:
+        pass
+
+    def update_name(self, name) -> None:
+        pass
+
+    def set_status(
+        self,
+        status,
+        description=None,
+    ) -> None:
+        pass
+
+    def record_exception(
+        self,
+        exception,
+        attributes=None,
+        timestamp=None,
+        escaped=False,
+    ) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return f"RecordingSpan({self._context!r})"
+
+
 class TestAPIOnlyImplementation(unittest.TestCase):
     """
     This test is in place to ensure the API is returning values that
@@ -42,22 +99,37 @@ class TestAPIOnlyImplementation(unittest.TestCase):
                 self.assertFalse(span2.get_span_context().is_valid)
                 self.assertIs(span2.is_recording(), False)
 
-    def test_default_tracer_context_propagation(self):
+    def test_default_tracer_context_propagation_recording_span(self):
+        tracer_provider = trace.NoOpTracerProvider()
+        tracer = tracer_provider.get_tracer(__name__)
+        span_context = trace.SpanContext(
+            2604504634922341076776623263868986797,
+            5213367945872657620,
+            False,
+            trace.TraceFlags(0x01),
+        )
+        ctx = trace.set_span_in_context(RecordingSpan(context=span_context))
+        with tracer.start_span("test", context=ctx) as span:
+            self.assertTrue(span.get_span_context().is_valid)
+            self.assertEqual(span.get_span_context(), span_context)
+            self.assertIs(span.is_recording(), False)
+
+    def test_default_tracer_context_propagation_non_recording_span(self):
+        tracer_provider = trace.NoOpTracerProvider()
+        tracer = tracer_provider.get_tracer(__name__)
+        ctx = trace.set_span_in_context(trace.INVALID_SPAN)
+        with tracer.start_span("test", context=ctx) as span:
+            self.assertFalse(span.get_span_context().is_valid)
+            self.assertIs(span, trace.INVALID_SPAN)
+
+    def test_default_tracer_context_propagation_with_invalid_context(self):
         tracer_provider = trace.NoOpTracerProvider()
         tracer = tracer_provider.get_tracer(__name__)
         ctx = trace.set_span_in_context(
-            trace.NonRecordingSpan(
-                trace.SpanContext(
-                    2604504634922341076776623263868986797,
-                    5213367945872657620,
-                    False,
-                    trace.TraceFlags(0x01),
-                )
-            )
+            RecordingSpan(context="invalid_context")
         )
-        with tracer.start_span("test", context=ctx) as span:
-            self.assertTrue(span.get_span_context().is_valid)
-            self.assertIs(span.is_recording(), False)
+        with self.assertRaises(TypeError):
+            tracer.start_span("test", context=ctx)
 
     def test_span(self):
         with self.assertRaises(TypeError):
