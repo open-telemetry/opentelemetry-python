acc97553a1d11eca042ed72bf4b01637875fa81f
Mike Goldsmith <goldsmith.mike@gmail.com>
2026-02-18 12:14:05 +0000

    add pylint excludes to test

--- opentelemetry-sdk/tests/_configuration/test_models.py
+++ opentelemetry-sdk/tests/_configuration/test_models.py
@@ -16,8 +16,8 @@ import unittest
 
 
 class TestModelsImport(unittest.TestCase):
-    def test_models_import(self):
+    def test_models_import(self):  # pylint: disable=no-self-use
         """Verify generated models import successfully across all Python versions"""
-        from opentelemetry.sdk._configuration import (
+        from opentelemetry.sdk._configuration import (  # pylint: disable=import-outside-toplevel,unused-import
             models,  # noqa: F401  # type: ignore
         )
