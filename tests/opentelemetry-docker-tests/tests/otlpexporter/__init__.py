2f96d452adc8725ee6582e13875fce6320d6d2e4
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2026-04-01 09:23:53 -0700

    Add docker-tests metrics export

--- tests/opentelemetry-docker-tests/tests/otlpexporter/__init__.py
+++ tests/opentelemetry-docker-tests/tests/otlpexporter/__init__.py
@@ -12,9 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
 from abc import ABC, abstractmethod
 
 from opentelemetry.context import attach, detach, set_value
+from opentelemetry.sdk.metrics._internal.export import (
+    MetricExportResult,
+    PeriodicExportingMetricReader,
+)
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 
 
@@ -27,6 +32,25 @@ class ExportStatusSpanProcessor(SimpleSpanProcessor):
         token = attach(set_value("suppress_instrumentation", True))
         self.export_status.append(self.span_exporter.export((span,)))
         detach(token)
+
+
+class ExportStatusMetricReader(PeriodicExportingMetricReader):
+    def __init__(self, exporter, **kwargs):
+        # Very short export interval for testing
+        super().__init__(exporter, export_interval_millis=1, **kwargs)
+        self.export_status = []
+
+    def _receive_metrics(self, metrics_data, timeout_millis=10_000, **kwargs):
+        token = attach(set_value("suppress_instrumentation", True))
+        try:
+            export_result = self._exporter.export(
+                metrics_data, timeout_millis=timeout_millis
+            )
+            self.export_status.append(export_result)
+        except Exception:
+            self.export_status.append(MetricExportResult.FAILURE)
+        finally:
+            detach(token)
 
 
 class BaseTestOTLPExporter(ABC):
@@ -44,5 +68,29 @@ class BaseTestOTLPExporter(ABC):
         self.assertTrue(len(self.span_processor.export_status), 3)
 
         for export_status in self.span_processor.export_status:
+            self.assertEqual(export_status.name, "SUCCESS")
+            self.assertEqual(export_status.value, 0)
+
+    def test_metrics_export(self):
+        counter = self.meter.create_counter("test_counter")
+        histogram = self.meter.create_histogram("test_histogram")
+        up_down_counter = self.meter.create_up_down_counter(
+            "test_up_down_counter"
+        )
+
+        counter.add(1, {"key1": "value1"})
+        counter.add(2, {"key2": "value2"})
+        histogram.record(1.5, {"key3": "value3"})
+        histogram.record(2.5, {"key4": "value4"})
+        up_down_counter.add(3, {"key5": "value5"})
+        up_down_counter.add(-1, {"key6": "value6"})
+        self.metric_reader.force_flush(timeout_millis=5000)
+        time.sleep(0.1)
+
+        # Verify at least one export happened
+        self.assertTrue(len(self.metric_reader.export_status) >= 1)
+
+        # Verify all exports succeeded
+        for export_status in self.metric_reader.export_status:
             self.assertEqual(export_status.name, "SUCCESS")
             self.assertEqual(export_status.value, 0)
