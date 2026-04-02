d25e09a5815a00ee80a09c02a80eb46d0433ba12
Lukas Hering <40302054+herin049@users.noreply.github.com>
2026-04-02 05:35:02 -0400

    opentelemetry-sdk: Implement meter configurator  (#4966)
    * Initial changes to add support for MeterConfigurator
    
    * fix lint errors
    
    * update SDK configuration to utilize Meter configurator
    
    * add basic Meter configurator benchmark
    
    * fix imports
    
    * Make meter configurator application more robust.
    
    * add update_rules method to _RuleBasedMeterConfigurator
    
    * fix lint errors
    
    * Update instrument.py: fix mismerge
    
    ---------
    
    Co-authored-by: Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>

--- opentelemetry-sdk/src/opentelemetry/sdk/trace/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/trace/__init__.py
@@ -16,7 +16,6 @@
 import abc
 import atexit
 import concurrent.futures
-import fnmatch
 import json
 import logging
 import os
@@ -69,6 +68,7 @@ from opentelemetry.sdk.util import BoundedList
 from opentelemetry.sdk.util.instrumentation import (
     InstrumentationInfo,
     InstrumentationScope,
+    _InstrumentationScopePredicateT,
 )
 from opentelemetry.semconv.attributes.exception_attributes import (
     EXCEPTION_ESCAPED,
@@ -1262,20 +1262,9 @@ class Tracer(trace_api.Tracer):
 
 
 _TracerConfiguratorT = Callable[[InstrumentationScope], _TracerConfig]
-_InstrumentationScopePredicateT = Callable[[InstrumentationScope], bool]
 _TracerConfiguratorRulesT = Sequence[
     tuple[_InstrumentationScopePredicateT, _TracerConfig]
 ]
-
-
-# TODO: share this with configurators for other signals
-def _scope_name_matches_glob(
-    glob_pattern: str,
-) -> _InstrumentationScopePredicateT:
-    def inner(scope: InstrumentationScope) -> bool:
-        return fnmatch.fnmatch(scope.name, glob_pattern)
-
-    return inner
 
 
 class _RuleBasedTracerConfigurator:
