1f1bb9c97ae65a0d5b1dfbb081053c46debbddaf
Ogulcan Aydogan <ogulcanaydogan@hotmail.com>
2026-05-14 11:47:16 +0100

    chore: apply ruff format to fix precommit CI
    Signed-off-by: Ogulcan Aydogan <ogulcanaydogan@hotmail.com>

--- opentelemetry-api/tests/propagators/test_propagators.py
+++ opentelemetry-api/tests/propagators/test_propagators.py
@@ -90,34 +90,12 @@ class TestPropagators(TestCase):
     ):
         mock_entry_points.configure_mock(
             side_effect=[
-                    [
-                        Mock(
-                            **{
-                                "load.return_value": Mock(
-                                    return_value="a"
-                                )
-                            }
-                        ),
-                    ],
-                    [
-                        Mock(
-                            **{
-                                "load.return_value": Mock(
-                                    return_value="b"
-                                )
-                            }
-                        )
-                    ],
-                    [
-                        Mock(
-                            **{
-                                "load.return_value": Mock(
-                                    return_value="c"
-                                )
-                            }
-                        )
-                    ],
-                ]
+                [
+                    Mock(**{"load.return_value": Mock(return_value="a")}),
+                ],
+                [Mock(**{"load.return_value": Mock(return_value="b")})],
+                [Mock(**{"load.return_value": Mock(return_value="c")})],
+            ]
         )
 
         def test_propagators(propagators):
