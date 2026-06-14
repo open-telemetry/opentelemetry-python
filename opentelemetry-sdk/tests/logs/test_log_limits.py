423bc7bc23ea748e804764f86233128104a5fb8d
Lukas Hering <lukasheringb@gmail.com>
2026-06-14 16:58:07 -0400

    opentelemetry-sdk: add log record limits environment variables

--- opentelemetry-sdk/tests/logs/test_log_limits.py
+++ opentelemetry-sdk/tests/logs/test_log_limits.py
@@ -22,12 +22,20 @@ from opentelemetry.sdk._logs._internal import (
 from opentelemetry.sdk.environment_variables import (
     OTEL_ATTRIBUTE_COUNT_LIMIT,
     OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT,
+    OTEL_LOGRECORD_ATTRIBUTE_COUNT_LIMIT,
+    OTEL_LOGRECORD_ATTRIBUTE_VALUE_LENGTH_LIMIT,
 )
 
 
 class TestLogLimits(unittest.TestCase):
     def test_log_limits_repr_unset(self):
-        expected = f"LogRecordLimits(max_attributes={_DEFAULT_OTEL_ATTRIBUTE_COUNT_LIMIT}, max_attribute_length=None)"
+        expected = (
+            f"LogRecordLimits("
+            f"max_attributes={_DEFAULT_OTEL_ATTRIBUTE_COUNT_LIMIT}, "
+            f"max_attribute_length=None, "
+            f"max_log_record_attributes={_DEFAULT_OTEL_ATTRIBUTE_COUNT_LIMIT}, "
+            f"max_log_record_attribute_length=None)"
+        )
         limits = str(LogRecordLimits())
 
         self.assertEqual(expected, limits)
@@ -44,10 +52,82 @@ class TestLogLimits(unittest.TestCase):
 
         self.assertEqual(expected, limits.max_attribute_length)
 
+    def test_log_limits_max_log_record_attributes(self):
+        limits = LogRecordLimits(max_log_record_attributes=5)
+
+        self.assertEqual(5, limits.max_log_record_attributes)
+
+    def test_log_limits_max_log_record_attribute_length(self):
+        limits = LogRecordLimits(max_log_record_attribute_length=10)
+
+        self.assertEqual(10, limits.max_log_record_attribute_length)
+
+    @patch.dict("os.environ", {OTEL_LOGRECORD_ATTRIBUTE_COUNT_LIMIT: "7"})
+    def test_logrecord_count_env_var(self):
+        limits = LogRecordLimits()
+
+        self.assertEqual(7, limits.max_log_record_attributes)
+        self.assertEqual(
+            _DEFAULT_OTEL_ATTRIBUTE_COUNT_LIMIT, limits.max_attributes
+        )
+
+    @patch.dict(
+        "os.environ", {OTEL_LOGRECORD_ATTRIBUTE_VALUE_LENGTH_LIMIT: "20"}
+    )
+    def test_logrecord_length_env_var(self):
+        limits = LogRecordLimits()
+
+        self.assertEqual(20, limits.max_log_record_attribute_length)
+        self.assertIsNone(limits.max_attribute_length)
+
+    @patch.dict(
+        "os.environ",
+        {
+            OTEL_ATTRIBUTE_COUNT_LIMIT: "50",
+            OTEL_LOGRECORD_ATTRIBUTE_COUNT_LIMIT: "3",
+        },
+    )
+    def test_logrecord_count_env_takes_precedence(self):
+        limits = LogRecordLimits()
+
+        self.assertEqual(50, limits.max_attributes)
+        self.assertEqual(3, limits.max_log_record_attributes)
+
+    @patch.dict(
+        "os.environ",
+        {
+            OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT: "100",
+            OTEL_LOGRECORD_ATTRIBUTE_VALUE_LENGTH_LIMIT: "25",
+        },
+    )
+    def test_logrecord_length_env_takes_precedence(self):
+        limits = LogRecordLimits()
+
+        self.assertEqual(100, limits.max_attribute_length)
+        self.assertEqual(25, limits.max_log_record_attribute_length)
+
+    @patch.dict("os.environ", {OTEL_ATTRIBUTE_COUNT_LIMIT: "42"}, clear=True)
+    def test_global_count_env_applies_as_fallback(self):
+        limits = LogRecordLimits()
+
+        self.assertEqual(42, limits.max_attributes)
+        self.assertEqual(42, limits.max_log_record_attributes)
+
+    @patch.dict(
+        "os.environ", {OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT: "60"}, clear=True
+    )
+    def test_global_length_env_applies_as_fallback(self):
+        limits = LogRecordLimits()
+
+        self.assertEqual(60, limits.max_attribute_length)
+        self.assertEqual(60, limits.max_log_record_attribute_length)
+
     def test_invalid_env_vars_raise(self):
         env_vars = [
             OTEL_ATTRIBUTE_COUNT_LIMIT,
             OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT,
+            OTEL_LOGRECORD_ATTRIBUTE_COUNT_LIMIT,
+            OTEL_LOGRECORD_ATTRIBUTE_VALUE_LENGTH_LIMIT,
         ]
 
         bad_values = ["bad", "-1"]
