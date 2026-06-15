6858898e3bf18d647ef8177b55660f23d92a4714
Dylan Russell <dylanrus@gmail.com>
2026-06-15 18:45:23 +0000

    Minor changes to changelog and envvar wording.

--- opentelemetry-sdk/src/opentelemetry/sdk/environment_variables/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/environment_variables/__init__.py
@@ -141,7 +141,7 @@ OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT = "OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT"
 """
 .. envvar:: OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT
 
-The :envvar:`OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT` represents the maximum allowed attribute value length for string/byte attribute values.
+The :envvar:`OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT` represents the maximum allowed attribute value length for string attribute values.
 """
 
 OTEL_EVENT_ATTRIBUTE_COUNT_LIMIT = "OTEL_EVENT_ATTRIBUTE_COUNT_LIMIT"
@@ -174,7 +174,7 @@ OTEL_SPAN_ATTRIBUTE_VALUE_LENGTH_LIMIT = (
 """
 .. envvar:: OTEL_SPAN_ATTRIBUTE_VALUE_LENGTH_LIMIT
 
-The :envvar:`OTEL_SPAN_ATTRIBUTE_VALUE_LENGTH_LIMIT` represents the maximum allowed attribute value length for string/byte span attribute values. This takes precedence over :envvar:`OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT`.
+The :envvar:`OTEL_SPAN_ATTRIBUTE_VALUE_LENGTH_LIMIT` represents the maximum allowed attribute value length for string span attribute values. This takes precedence over :envvar:`OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT`.
 """
 
 OTEL_SPAN_EVENT_COUNT_LIMIT = "OTEL_SPAN_EVENT_COUNT_LIMIT"
