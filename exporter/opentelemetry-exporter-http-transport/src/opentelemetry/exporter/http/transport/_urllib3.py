fa5c8aab123812edd4344c3844d3328326d62403
Lukas Hering <40302054+herin049@users.noreply.github.com>
2026-06-19 12:15:05 -0400

    Refactor kwargs type hint to Any for urllib3

--- exporter/opentelemetry-exporter-http-transport/src/opentelemetry/exporter/http/transport/_urllib3.py
+++ exporter/opentelemetry-exporter-http-transport/src/opentelemetry/exporter/http/transport/_urllib3.py
@@ -73,7 +73,7 @@ class Urllib3HTTPTransport(BaseHTTPTransport):
         cls,
         verify: bool | str,
         cert: str | tuple[str, str] | None,
-        **kwargs: dict[str, Any],
+        **kwargs: Any,
     ) -> Self:
         return cls(verify=verify, cert=cert, **kwargs)
 
@@ -82,7 +82,7 @@ class Urllib3HTTPTransport(BaseHTTPTransport):
         *,
         verify: bool | str = True,
         cert: str | tuple[str, str] | None = None,
-        **kwargs: dict[str, Any],
+        **kwargs: Any,
     ) -> None:
         # pylint: disable-next=import-outside-toplevel
         import urllib3  # noqa: PLC0415
