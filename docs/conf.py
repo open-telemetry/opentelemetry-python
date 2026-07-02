319213b8361227749441e8be95c72bbf6dbba3bf
Lukas Hering <lukasheringb@gmail.com>
2026-07-02 16:13:16 -0500

    fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -194,10 +194,7 @@ nitpick_ignore = [
     # ``from os import PathLike`` renders as the bare name ``PathLike`` in the
     # file exporter type hints, which sphinx cannot resolve to os.PathLike.
     ("py:class", "PathLike"),
-    # ``Compression``/``BaseHTTPTransport`` resolve to their private defining
-    # modules in the json-http exporter's type hints; those modules aren't
-    # documented on their own.
-    ("py:class", "opentelemetry.exporter.otlp.common._http.Compression"),
+    ("py:class", "opentelemetry.exporter.otlp.common.http.Compression"),
     (
         "py:class",
         "opentelemetry.exporter.http.transport._base.BaseHTTPTransport",
