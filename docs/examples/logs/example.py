c6f9145140ae698885ae10744a288fbc3dc2f13f
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2026-07-10 11:59:02 +0200

    Apply suggestion from @xrmx

--- docs/examples/logs/example.py
+++ docs/examples/logs/example.py
@@ -54,7 +54,6 @@ logging.getLogger().addHandler(handler)
 logger1 = logging.getLogger("myapp.area1")
 logger2 = logging.getLogger("myapp.area2")
 
-
 logger1.debug("Quick zephyrs blow, vexing daft Jim.")
 logger1.info("How quickly daft jumping zebras vex.")
 logger2.warning("Jail zesty vixen who grabbed pay from quack.")
