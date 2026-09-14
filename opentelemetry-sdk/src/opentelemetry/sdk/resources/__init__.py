406ebb31ddcb0056bde0313314633d804e6fcbe4
Radhika Gupta <guptaradhika@microsoft.com>
2026-09-14 09:33:01 -0700

    Fix overwritting of the service.instance.id which has been populated from the user provided values from the resource detectors

--- opentelemetry-sdk/src/opentelemetry/sdk/resources/__init__.py
+++ opentelemetry-sdk/src/opentelemetry/sdk/resources/__init__.py
@@ -633,6 +633,12 @@ def get_aggregated_resources(
             detected_resource: Resource = _EMPTY_RESOURCE
             try:
                 detected_resource = future.result(timeout=timeout)
+
+                if (
+                    isinstance(detector, ServiceInstanceIdResourceDetector)
+                    and SERVICE_INSTANCE_ID in detectors_merged_resource.attributes
+                ):
+                    continue
             except concurrent.futures.TimeoutError as ex:
                 if detector.raise_on_error:
                     raise ex
@@ -646,8 +652,7 @@ def get_aggregated_resources(
                 if detector.raise_on_error:
                     raise ex
                 logger.warning("Exception %s in detector %s, ignoring", ex, detector)
-            finally:
-                detectors_merged_resource = detectors_merged_resource.merge(detected_resource)
+            detectors_merged_resource = detectors_merged_resource.merge(detected_resource)
     finally:
         executor.shutdown(wait=False, cancel_futures=True)
 
