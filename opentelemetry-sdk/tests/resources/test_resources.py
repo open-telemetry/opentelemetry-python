4e082b2900c20411cafc3374382b5b54e7d50490
Radhika Gupta <guptaradhika@microsoft.com>
2026-09-21 13:01:59 -0700

    Fix lint

--- opentelemetry-sdk/tests/resources/test_resources.py
+++ opentelemetry-sdk/tests/resources/test_resources.py
@@ -1076,9 +1076,7 @@ class TestServiceInstanceIdResourceDetector(unittest.TestCase):
                         "custom.detector": "value",
                     }
                 )
-                entry_point = Mock(
-                    **{"load.return_value": Mock(return_value=custom_detector)}
-                )
+                entry_point = Mock(**{"load.return_value": Mock(return_value=custom_detector)})
 
                 def side_effect(*args, **kwargs):
                     if kwargs.get("name") == "mock":
